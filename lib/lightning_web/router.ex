361c9e546987ad8a0e4ec351dc800320f89448eb
Stuart Corbishley <corbish@gmail.com>
2022-03-23 10:24:50 +0200

    Satisfy high priority security warnings

--- lib/lightning_web/router.ex
+++ lib/lightning_web/router.ex
@@ -8,7 +8,7 @@ defmodule LightningWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {LightningWeb.LayoutView, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
