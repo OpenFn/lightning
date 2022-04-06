d025fd91711bafbed7515445d0ecfe95ffaee9f7
Stuart Corbishley <corbish@gmail.com>
2022-04-06 14:48:19 +0200

    Fix failing test for User edit patch/redirect

--- lib/lightning_web/live/user_live/index.ex
+++ lib/lightning_web/live/user_live/index.ex
@@ -6,7 +6,7 @@ defmodule LightningWeb.UserLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :users, list_users())}
+    {:ok, assign(socket, :users, list_users()) |> assign(:active_menu_item, :users)}
   end
 
   @impl true
--- test/lightning_web/live/user_live_test.exs
+++ test/lightning_web/live/user_live_test.exs
@@ -58,40 +58,21 @@ defmodule LightningWeb.UserLiveTest do
     test "updates user in listing", %{conn: conn, user: user} do
       {:ok, index_live, _html} = live(conn, Routes.user_index_path(conn, :index))
 
-      assert index_live |> element("#user-#{user.id} a", "Edit") |> render_click() =~
-               "Edit"
+      {:ok, form_live, _} =
+        index_live
+        |> element("#user-#{user.id} a", "Edit")
+        |> render_click()
+        |> follow_redirect(conn, Routes.user_edit_path(conn, :edit, user))
 
-      assert_patch(index_live, Routes.user_edit_path(conn, :edit, user))
-
-      assert index_live
+      assert form_live
              |> form("#user-form", user: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
       {:ok, _, html} =
-        index_live
+        form_live
         |> form("#user-form", user: @update_attrs)
         |> render_submit()
         |> follow_redirect(conn, Routes.user_index_path(conn, :index))
-
-      # assert html =~ "Dataclip updated successfully"
-
-      # {:ok, index_live, _html} = live(conn, Routes.user_index_path(conn, :index))
-
-      # {:ok, form_live, _} =
-      #   index_live
-      #   |> element("#user-#{user.id} a", "Edit")
-      #   |> render_click()
-      #   |> follow_redirect(conn, Routes.user_edit_path(conn, :edit, user))
-
-      # assert form_live
-      #        |> form("#user-form", user: @invalid_attrs)
-      #        |> render_change() =~ "can&#39;t be blank"
-
-      # {:ok, _, html} =
-      #   form_live
-      #   |> form("#user-form", user: @update_attrs)
-      #   |> render_submit()
-      #   |> follow_redirect(conn, Routes.user_index_path(conn, :index))
 
       assert html =~ "User updated successfully"
     end
@@ -99,41 +80,11 @@ defmodule LightningWeb.UserLiveTest do
     test "deletes user in listing", %{conn: conn, user: user} do
       {:ok, index_live, _html} = live(conn, Routes.user_index_path(conn, :index))
 
-      assert index_live |> element("#user-#{user.id} a", "Delete") |> render_click()
+      assert index_live
+             |> element("#user-#{user.id} a", "Delete")
+             |> render_click()
+
       refute has_element?(index_live, "#user-#{user.id}")
     end
   end
-
-  # describe "Show" do
-  #   setup [:create_user]
-
-  #   test "displays user", %{conn: conn, user: user} do
-  #     {:ok, _show_live, html} = live(conn, Routes.user_show_path(conn, :show, user))
-
-  #     assert html =~ "Show User"
-  #     assert html =~ user.email
-  #   end
-
-  #   test "updates user within modal", %{conn: conn, user: user} do
-  #     {:ok, show_live, _html} = live(conn, Routes.user_show_path(conn, :show, user))
-
-  #     assert show_live |> element("a", "Edit") |> render_click() =~
-  #              "Edit User"
-
-  #     assert_patch(show_live, Routes.user_show_path(conn, :edit, user))
-
-  #     assert show_live
-  #            |> form("#user-form", user: @invalid_attrs)
-  #            |> render_change() =~ "can&#39;t be blank"
-
-  #     {:ok, _, html} =
-  #       show_live
-  #       |> form("#user-form", user: @update_attrs)
-  #       |> render_submit()
-  #       |> follow_redirect(conn, Routes.user_show_path(conn, :show, user))
-
-  #     assert html =~ "User updated successfully"
-  #     assert html =~ "some updated email"
-  #   end
-  # end
 end
