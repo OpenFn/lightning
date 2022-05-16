eec071674481179fdba4b1a290eff52784789080
Stuart Corbishley <corbish@gmail.com>
2022-05-16 08:43:16 +0200

    Use assign_new when setting current_user in liveview

--- lib/lightning_web/init_assigns.ex
+++ lib/lightning_web/init_assigns.ex
@@ -5,13 +5,10 @@ defmodule LightningWeb.InitAssigns do
   import Phoenix.LiveView
   alias Lightning.Accounts
 
-  defp assign_current_user_from_session(socket, session) do
-    user_token = session["user_token"]
-    user = user_token && Accounts.get_user_by_session_token(user_token)
-    assign(socket, :current_user, user)
-  end
-
   def on_mount(:default, _params, session, socket) do
-    {:cont, assign_current_user_from_session(socket, session)}
+    {:cont,
+     assign_new(socket, :current_user, fn ->
+       Accounts.get_user_by_session_token(session["user_token"])
+     end)}
   end
 end
