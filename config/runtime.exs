d3cf16169697c6f48c0d92b2f5501311795a8989
Taylor Downs <taylor@openfn.org>
2022-05-24 17:57:11 +0100

    don't set ip

--- config/runtime.exs
+++ config/runtime.exs
@@ -64,7 +64,7 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      # ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base,
