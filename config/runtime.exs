5fe23594391b7e8b1abf2908670009654b87b729
Taylor Downs <downs.taylor@gmail.com>
2022-05-25 12:07:18 +0100

    add ssl to extra apps

--- config/runtime.exs
+++ config/runtime.exs
@@ -70,7 +70,8 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      # ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      # ip: listen_address,
       port: port
     ],
     secret_key_base: secret_key_base,
--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Lightning.MixProject do
   def application do
     [
       mod: {Lightning.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
