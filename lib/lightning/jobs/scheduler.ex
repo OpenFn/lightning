776e2a2f51b9d63f5b04288c95d2dbe0b844b4e4
Taylor Downs <taylor@openfn.org>
2022-06-24 08:49:31 +0100

    refactor scheduler per credo depth reccomendation

--- lib/lightning/jobs/scheduler.ex
+++ lib/lightning/jobs/scheduler.ex
@@ -28,39 +28,42 @@ defmodule Lightning.Jobs.Scheduler do
     |> DateTime.to_unix()
     |> Jobs.find_cron_triggers()
     |> Enum.each(fn %Jobs.Job{id: id, project_id: project_id} ->
-      {:ok, %{event: event, run: run}} =
-        case last_state_for_job(id) do
-          nil ->
-            Logger.debug(fn ->
-              "Starting cronjob #{id} for the first time. (No previous final state.)"
-            end)
-
-            Invocation.create(
-              %{job_id: id, project_id: project_id, type: :cron},
-              # Add a facility to specify _which_ global state should be use as
-              # the first initial state for a cron-triggered job.
-              %{type: :global, body: %{}}
-              # The implementation would look like:
-              # default_state_for_job(id)
-              # which returns %{id: uuid, type: :global, body: %{arbitrary: true}}
-            )
-
-          state ->
-            Logger.debug(fn ->
-              "Starting cronjob #{id} using the final state of its last successful run."
-            end)
-
-            Invocation.create(
-              %{job_id: id, project_id: project_id, type: :cron},
-              %{type: :run_result, body: Map.get(state, :body)}
-            )
-        end
+      {:ok, %{event: event, run: run}} = invoke_cronjob(id, project_id)
 
       Pipeline.new(%{event_id: event.id, run_id: run.id})
       |> Oban.insert()
     end)
 
     :ok
+  end
+
+  defp invoke_cronjob(id, project_id) do
+    case last_state_for_job(id) do
+      nil ->
+        Logger.debug(fn ->
+          "Starting cronjob #{id} for the first time. (No previous final state.)"
+        end)
+
+        Invocation.create(
+          %{job_id: id, project_id: project_id, type: :cron},
+          # Add a facility to specify _which_ global state should be use as
+          # the first initial state for a cron-triggered job.
+          %{type: :global, body: %{}}
+          # The implementation would look like:
+          # default_state_for_job(id)
+          # which returns %{id: uuid, type: :global, body: %{arbitrary: true}}
+        )
+
+      state ->
+        Logger.debug(fn ->
+          "Starting cronjob #{id} using the final state of its last successful run."
+        end)
+
+        Invocation.create(
+          %{job_id: id, project_id: project_id, type: :cron},
+          %{type: :run_result, body: Map.get(state, :body)}
+        )
+    end
   end
 
   defp last_state_for_job(id) do
