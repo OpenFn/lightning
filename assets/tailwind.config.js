1dcf396a276d3d6d7c4e5068fc0131cd33012435
Taylor Downs <taylor@openfn.org>
2022-07-13 16:29:06 +0700

    fix tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,20 +1,18 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    './js/**/*.js',
-    '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
-  ],
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
   theme: {
     extend: {},
     colors: {
-      'primary': '#111827',
-      'primary-light': '#334155',
-      'white': '#ffffff'
-    }
+      slate: colors.slate,
+      indigo: colors.indigo,
+      primary: "#111827",
+      "primary-light": "#334155",
+      white: "#ffffff",
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
-}
+  plugins: [require("@tailwindcss/forms")],
+};
