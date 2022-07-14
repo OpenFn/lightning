9d1a7761ab0954c05c0eeaa73215aca0fea28d59
Taylor Downs <taylor@openfn.org>
2022-07-14 22:17:48 +0700

    needs testing

--- lib/lightning_web.ex
+++ lib/lightning_web.ex
@@ -102,6 +102,7 @@ defmodule LightningWeb do
       alias LightningWeb.Router.Helpers, as: Routes
 
       import PetalComponents.Table
+      import PetalComponents.Dropdown
       alias PetalComponents.Heroicons
 
       alias LightningWeb.Components
