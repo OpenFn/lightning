7f4ddf84e8dd6c6496e28376bcbb8c81e59d4d6f
Taylor Downs <taylor@openfn.org>
2022-07-14 13:03:20 +0700

    install petal, use table

--- mix.exs
+++ mix.exs
@@ -60,6 +60,7 @@ defmodule Lightning.MixProject do
       {:mimic, "~> 1.7.2", only: :test},
       {:mix_test_watch, "~> 1.0", only: [:test, :dev], runtime: false},
       {:oban, "~> 2.12"},
+      {:petal_components, "~> 0.17"},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 3.2"},
       {:phoenix_live_dashboard, "~> 0.6.5"},
