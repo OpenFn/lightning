d3d1f54eb22d756d2b97e0a451cadf645bb16f32
Stuart Corbishley <corbish@gmail.com>
2022-07-26 10:05:12 +0200

    Upgrade credo, dialyxir and excoveralls

--- mix.exs
+++ mix.exs
@@ -13,7 +13,13 @@ defmodule Lightning.MixProject do
       deps: deps(),
       dialyzer: [plt_add_apps: [:mix]],
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [verify: :test],
+      preferred_cli_env: [
+        verify: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
 
       # Docs
       name: "Lightning",
@@ -48,7 +54,7 @@ defmodule Lightning.MixProject do
       {:dialyxir, "~> 1.1", only: [:test, :dev], runtime: false},
       {:ecto_enum, "~> 1.4"},
       {:ecto_sql, "~> 3.6"},
-      {:engine, github: "OpenFn/engine", tag: "v0.7.0"},
+      {:engine, github: "OpenFn/engine", tag: "v0.7.1"},
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:excoveralls, "~> 0.14.4", only: [:test, :dev]},
