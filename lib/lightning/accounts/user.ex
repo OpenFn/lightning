85ad864d5d17fe5155358d88d1954f714132a02b
Elias W. BA <eliaswalyba@gmail.com>
2022-08-16 13:04:35 +0000

    Add scheduled_deletion to user model

--- lib/lightning/accounts/user.ex
+++ lib/lightning/accounts/user.ex
@@ -77,6 +77,7 @@ defmodule Lightning.Accounts.User do
       :disabled,
       :scheduled_deletion
     ])
+    |> cast(attrs, [:first_name, :last_name, :email, :password, :disabled, :scheduled_deletion])
     |> validate_email()
     |> validate_password(opts)
   end
