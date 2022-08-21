25d8dd7eeb03ee967935c2c6b8df71e28607cd0c
Elias W. BA <eliaswalyba@gmail.com>
2022-08-21 20:21:05 +0000

    Test user.ex

---
+++ test/lightning/accounts/user_test.exs
@@ -0,0 +1,31 @@
+defmodule Lightning.Credentials.UserTest do
+  use Lightning.DataCase, async: true
+
+  alias Lightning.Accounts.User
+
+  describe "scheduled deletion changeset" do
+    test "email doesn't match current users email" do
+      errors =
+        User.scheduled_deletion_changeset(%User{email: "real@email.com"}, %{
+          "id" => "86201fff-a699-4eca-bb53-8736228ff187",
+          "scheduled_deletion_email" => "user@gmail.com"
+        })
+        |> errors_on()
+
+      assert errors[:scheduled_deletion_email] == [
+               "This email doesn't match your current email"
+             ]
+    end
+
+    test "email does match current users email" do
+      errors =
+        User.scheduled_deletion_changeset(%User{email: "real@email.com"}, %{
+          "id" => "86201fff-a699-4eca-bb53-8736228ff187",
+          "scheduled_deletion_email" => "real@email.com"
+        })
+        |> errors_on()
+
+      assert errors[:scheduled_deletion_email] == nil
+    end
+  end
+end
