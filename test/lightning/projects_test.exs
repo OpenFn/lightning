587338428d0ed690987204803158d5ba86cba78a
Mamadou WANE <wanecode@gmail.com>
2022-09-10 17:06:37 +0000

    add export_project + test

--- test/lightning/projects_test.exs
+++ test/lightning/projects_test.exs
@@ -7,6 +7,7 @@ defmodule Lightning.ProjectsTest do
   import Lightning.ProjectsFixtures
   import Lightning.AccountsFixtures
   import Lightning.CredentialsFixtures
+  import Lightning.FullProjectsFixtures
 
   describe "projects" do
     @invalid_attrs %{name: nil}
@@ -127,5 +128,16 @@ defmodule Lightning.ProjectsTest do
       assert [project_1, project_2] == Projects.get_projects_for_user(user)
       assert [project_1] == Projects.get_projects_for_user(other_user)
     end
+
+    test "export_project/2 as yaml" do
+      %{project: project} = full_project_fixture();
+      expected_yaml = File.read!("test/fixtures/canonical_project.yaml")
+      {:ok, generated_yaml} = Projects.export_project(:yaml, project.id)
+      File.write!("test/fixtures/generated_project.yaml", generated_yaml)
+
+      assert generated_yaml == expected_yaml
+    end
+
+
   end
 end
---
+++ test/support/fixtures/full_project_fixtures.ex
@@ -0,0 +1,73 @@
+defmodule Lightning.FullProjectsFixtures do
+  @moduledoc """
+  This module generates full projects for testing purpose
+  """
+
+  @doc """
+  Generate a project with 2 workflows with all kind of jobs
+  """
+
+  import Lightning.ProjectsFixtures
+  import Lightning.CredentialsFixtures
+  import Lightning.WorkflowsFixtures
+  import Lightning.JobsFixtures
+
+  def full_project_fixture() do
+    project = project_fixture()
+      w1 = workflow_fixture(project_id: project.id, name: "workflow 1")
+      w2 = workflow_fixture(project_id: project.id, name: "workflow 2")
+
+      project_credential =
+        project_credential_fixture(
+          name: "new credential",
+          body: %{"foo" => "manchu"}
+        )
+
+      w1_job =
+        job_fixture(
+          name: "webhook job",
+          project_id: project.id,
+          workflow_id: w1.id,
+          project_credential_id: project_credential.id,
+          trigger: %{type: :webhook},
+          body: "console.log('webhook job')\nfn(state => state)"
+        )
+
+      job_fixture(
+        name: "on fail",
+        project_id: project.id,
+        workflow_id: w1.id,
+        trigger: %{type: :on_job_failure, upstream_job_id: w1_job.id},
+        body: "console.log('on fail')\nfn(state => state)"
+      )
+
+      job_fixture(
+        name: "on success",
+        project_id: project.id,
+        workflow_id: w1.id,
+        trigger: %{type: :on_job_success, upstream_job_id: w1_job.id}
+      )
+
+      w2_job =
+        job_fixture(
+          name: "other workflow",
+          project_id: project.id,
+          workflow_id: w2.id,
+          trigger: %{type: :webhook}
+        )
+
+      job_fixture(
+        name: "on fail",
+        project_id: project.id,
+        workflow_id: w2.id,
+        trigger: %{type: :on_job_failure, upstream_job_id: w2_job.id}
+      )
+
+      job_fixture(
+        name: "unrelated job",
+        trigger: %{type: :webhook}
+      )
+
+      %{project: project, w1: w1, w2: w2}
+  end
+end
