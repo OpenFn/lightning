17ecd5e2b44c56d5bdd2efda6ea8cbe21798f11c
mtuchi <mtuchidev@gmail.com>
2022-09-19 11:24:05 +0300

    wip: session with stuart

--- lib/lightning_web/live/run_live/index.ex
+++ lib/lightning_web/live/run_live/index.ex
@@ -110,7 +110,9 @@ defmodule LightningWeb.RunLive.Index do
     time |> Timex.from_now(Timex.now(), "en")
   end
 
-  def run_time(%{run: run} = assigns) do
+  def run_time(assigns) do
+    run = assigns[:run]
+
     if run.finished_at do
       time_taken = Timex.diff(run.finished_at, run.started_at, :milliseconds)
 
@@ -129,5 +131,134 @@ defmodule LightningWeb.RunLive.Index do
 
       """
     end
+  end
+
+  # Expanded
+  def first_example(assigns) do
+
+    workflows = [%{
+      name: "first workflow",
+      last_run: "yesterday",
+      dataclip_id: Ecto.UUID.generate() |> String.slice(0..5),
+      status: "success"
+
+      # list of workflow job statuses
+      job_status: [
+        %{
+          status: "success", run_at: "right now", jobs: [
+            %{name: "Kobo Submissions", status: "success"},
+            %{name: "Upload to Googlesheets", status: "success"},
+            %{name: "Upload to DHIS2", status: "success"}
+          ]
+        }
+      ]
+    }]
+
+    # now we know that we can use `@expanded`
+    assigns =
+      assigns
+      |> assign_new(:expanded, fn -> false end)
+      |> assign(:workflows, workflows)
+
+    ~H"""
+    <tr class="my-4 grid grid-cols-4 gap-4 rounded-lg bg-white">
+      <th
+        scope="row"
+        class="my-auto whitespace-nowrap p-6 font-medium text-gray-900 dark:text-white"
+      >
+        workFlowName
+      </th>
+      <td class="my-auto p-6">⭐ my-test-a</td>
+      <td class="my-auto p-6">UTC 24:20:60</td>
+      <td class="my-auto p-6">
+        <div class="flex content-center justify-between">
+          <span class="my-auto whitespace-nowrap rounded-full bg-green-200 py-2 px-4 text-center align-baseline text-xs font-medium leading-none text-green-800">
+            Success
+          </span>
+
+          <button class="w-auto rounded-full bg-gray-50 p-3">
+            <Heroicons.Outline.chevron_down class="h-5 w-5" />
+          </button>
+        </div>
+      </td>
+      <%= if @expanded do %>
+      ghgfhgfhgfh
+    <% end %>
+      <td class="col-span-4 mx-3 mb-3 rounded-lg bg-gray-100 p-6">
+        <ul class="list-inside list-none space-y-4 text-gray-500 dark:text-gray-400">
+          <li>
+            <span class="flex items-center">
+              <Heroicons.Solid.clock class="mr-1 h-5 w-5" />
+
+              <span>
+                Re-run at 15 June 14:20:42
+                <span class="my-auto ml-2 whitespace-nowrap rounded-full bg-green-200 py-2 px-4 text-center align-baseline text-xs font-medium leading-none text-green-800">
+                  Success
+                </span>
+              </span>
+            </span>
+            <ol class="mt-2 list-none space-y-4">
+              <%= if @expanded do %>
+                ghgfhgfhgfh
+              <% end %>
+              <li>
+                <span class="my-4 flex">
+                  &vdash;
+                  <span class="mx-2 flex">
+                    <Heroicons.Solid.check_circle class="mr-1.5 h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400" />
+                    You might feel like you are being
+                  </span>
+                </span>
+                <ol class="space-y-4 pl-5">
+                  <li>
+                    <span class="mx-1 flex">
+                      &vdash;
+                      <span class="ml-1">
+                        are being really "organized" o
+                      </span>
+                    </span>
+                  </li>
+                </ol>
+              </li>
+              <li>
+                <span class="flex">
+                  &vdash;
+                  <span class="mx-2 flex">
+                    <Heroicons.Solid.check_circle class="mr-1.5 h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400" />
+                    You might feel like you are being
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span class="flex">
+                  &vdash;
+                  <span class="mx-2 flex">
+                    <Heroicons.Solid.check_circle class="mr-1.5 h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400" />
+                    You might feel like you are being
+                  </span>
+                </span>
+              </li>
+            </ol>
+          </li>
+        </ul>
+      </td>
+      <td class="col-span-4 mx-3 mb-3 rounded-lg bg-gray-100 p-6">
+        <ul>
+          <li>
+            <span class="flex items-center">
+              <Heroicons.Solid.clock class="mr-1 h-5 w-5" />
+
+              <span>
+                Run at 15 June 14:20:42
+                <span class="my-auto ml-2 whitespace-nowrap rounded-full bg-red-200 py-2 px-4 text-center align-baseline text-xs font-medium leading-none text-red-800">
+                  Failure
+                </span>
+              </span>
+            </span>
+          </li>
+        </ul>
+      </td>
+    </tr>
+    """
   end
 end
