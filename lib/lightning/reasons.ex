7195b7167e71dd6522bd7eed88293a40ba2951aa
Mamadou WANE <wanecode@gmail.com>
2022-10-14 10:57:46 +0000

    remove update, delete, change ops on reason

--- lib/lightning/reasons.ex
+++ lib/lightning/reasons.ex
@@ -26,40 +26,6 @@ defmodule Lightning.InvocationReasons do
   end
 
   @doc """
-  Updates a reason.
-
-  ## Examples
-
-      iex> update_reason(reason, %{field: new_value})
-      {:ok, %InvocationReason{}}
-
-      iex> update_reason(reason, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_reason(%InvocationReason{} = reason, attrs) do
-    reason
-    |> InvocationReason.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a reason.
-
-  ## Examples
-
-      iex> delete_reason(reason)
-      {:ok, %InvocationReason{}}
-
-      iex> delete_reason(reason)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_reason(%InvocationReason{} = reason) do
-    Repo.delete(reason)
-  end
-
-  @doc """
   Returns an `%Ecto.Changeset{}` for tracking reason changes.
 
   ## Examples
