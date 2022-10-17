299111fb486b4ba4ff82ee0bfe8c07355e83ecf1
Mamadou WANE <wanecode@gmail.com>
2022-10-17 08:11:24 +0000

    add attempt_runs table migration

--- priv/repo/migrations/20221017071303_add_attempts_table.exs
+++ priv/repo/migrations/20221017071303_add_attempts_table.exs
@@ -4,7 +4,9 @@ defmodule Lightning.Repo.Migrations.AddAttemptsTable do
   def change do
     create table(:attempt_runs, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :reason_id, references(:invocation_reasons, on_delete: :nothing, type: :binary_id), null: false
+
+      add :reason_id, references(:invocation_reasons, on_delete: :nothing, type: :binary_id),
+        null: false
 
       timestamps()
     end
