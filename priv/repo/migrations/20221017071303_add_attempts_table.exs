ffbccc794bdc7d3988b380f5a376fe44bb8f4b90
Mamadou WANE <wanecode@gmail.com>
2022-10-17 08:48:54 +0000

    fix attempts table name in migration

--- priv/repo/migrations/20221017071303_add_attempts_table.exs
+++ priv/repo/migrations/20221017071303_add_attempts_table.exs
@@ -2,7 +2,7 @@ defmodule Lightning.Repo.Migrations.AddAttemptsTable do
   use Ecto.Migration
 
   def change do
-    create table(:attempt_runs, primary_key: false) do
+    create table(:attempts, primary_key: false) do
       add :id, :binary_id, primary_key: true
 
       add :reason_id, references(:invocation_reasons, on_delete: :nothing, type: :binary_id),
