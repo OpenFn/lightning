c98873412cc9d6919bb2c2a36a640b4b97d57159
Mamadou WANE <wanecode@gmail.com>
2022-10-20 08:11:12 +0000

    add attempt_service

---
+++ lib/lightning/attempt_service.ex
@@ -0,0 +1,56 @@
+defmodule Lightning.AttemptService do
+  @moduledoc """
+  The Attempts context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Lightning.Repo
+  alias Lightning.Attempt
+  alias Lightning.Invocation.{Event, Run}
+
+  @doc """
+  Creates a reason.
+
+  ## Examples
+
+      iex> create_attempt(%{field: value})
+      {:ok, %Attempt{}}
+
+      iex> create_attempt(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+
+  def create_attempt(_workorder, job, reason) do
+    project_id = job.project_id
+    dataclip_id = reason.dataclip_id
+
+    Ecto.Multi.new()
+    |> Ecto.Multi.insert(
+      :event,
+      Event.changeset(%Event{}, %{
+        type: :webhook,
+        job_id: job.id,
+        project_id: project_id,
+        dataclip_id: dataclip_id
+      })
+    )
+    |> Ecto.Multi.insert(:run, fn %{event: %Event{id: event_id}} ->
+      Run.changeset(%Run{}, %{
+        event_id: event_id,
+        project_id: project_id,
+        job_id: job.id,
+        input_dataclip_id: dataclip_id,
+        output_dataclip: nil
+      })
+    end)
+    |> Ecto.Multi.insert(:attempt, fn %{run: run} ->
+      Attempt.changeset(%Attempt{}, %{
+        reason_id: reason.id,
+        runs: [Map.from_struct(run)]
+        # why we set manually timestamps  https://github.com/elixir-ecto/ecto/issues/1260
+      })
+    end)
+    |> Repo.transaction()
+  end
+end
