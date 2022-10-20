2c591f0a4108be5b9f211b2ca87170a15d2919db
Mamadou WANE <wanecode@gmail.com>
2022-10-20 08:12:31 +0000

    add attempt_service_test

---
+++ test/lightning/attempt_service_test.exs
@@ -0,0 +1,44 @@
+defmodule Lightning.AttemptServiceTest do
+  use Lightning.DataCase, async: true
+
+  describe "attempts" do
+    import Lightning.JobsFixtures
+    import Lightning.InvocationFixtures
+    alias Lightning.AttemptService
+
+    alias Lightning.Invocation.{Event, Run}
+    alias Lightning.Attempt
+
+    test "create/3 returns an Event, a Run and an Attempt" do
+      job = job_fixture()
+      reason = reason_fixture(trigger_id: job.trigger.id)
+
+      job_id = job.id
+      reason_id = reason.id
+      data_clip_id = reason.dataclip_id
+
+      IO.inspect(
+        AttemptService.create_attempt(
+          nil,
+          job,
+          reason
+        )
+      )
+
+      assert {:ok,
+              %{
+                attempt: %Attempt{
+                  reason_id: ^reason_id,
+                  runs: [%Run{job_id: ^job_id, input_dataclip_id: ^data_clip_id}]
+                },
+                event: %Event{},
+                run: %Run{}
+              }} =
+               AttemptService.create_attempt(
+                 nil,
+                 job,
+                 reason
+               )
+    end
+  end
+end
