9dd753549786d5ccf45d715abd41c25657f4142a
Mamadou WANE <wanecode@gmail.com>
2022-10-20 08:12:13 +0000

    add attempt_test

---
+++ test/lightning/invocation/attempt_test.exs
@@ -0,0 +1,13 @@
+defmodule Lightning.Invocation.AttemptTest do
+  use Lightning.DataCase, async: true
+
+  alias Lightning.Attempt
+
+  describe "changeset/2" do
+    test "must have a reason" do
+      errors = Attempt.changeset(%Attempt{}, %{}) |> errors_on()
+
+      assert errors[:reason_id] == ["can't be blank"]
+    end
+  end
+end
