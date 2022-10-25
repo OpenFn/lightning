1b428e190a10f7f7bd15a0f3fb8bc2f2b3bc2c57
Mamadou WANE <wanecode@gmail.com>
2022-10-25 10:58:14 +0000

    refactor migrations files (runs, workorder, attempt)

--- priv/repo/migrations/20221017071303_add_attempts_table.exs
+++ priv/repo/migrations/20221017071303_add_attempts_table.exs
@@ -8,7 +8,12 @@ defmodule Lightning.Repo.Migrations.AddAttemptsTable do
       add :reason_id, references(:invocation_reasons, on_delete: :nothing, type: :binary_id),
         null: false
 
+      add :work_order_id, references(:work_orders, on_delete: :delete_all, type: :binary_id),
+        null: false
+
       timestamps()
     end
+
+    create index(:attempts, [:work_order_id])
   end
 end
--- priv/repo/migrations/20221025102818_refactor_runs_for_workorders.exs
+++ priv/repo/migrations/20221025102818_refactor_runs_for_workorders.exs
@@ -1,8 +1,13 @@
-defmodule Lightning.Repo.Migrations.RemoveEventFromRuns do
+defmodule Lightning.Repo.Migrations.RefactorRunsForWorkorders do
   use Ecto.Migration
 
   def change do
     alter table(:runs) do
+      add :previous_id, references(:runs, on_delete: :delete_all, type: :binary_id), null: true
+
+      remove :project_id, references(:projects, on_delete: :delete_all, type: :binary_id),
+        null: false
+
       remove :event_id, references(:invocation_events, on_delete: :nothing, type: :binary_id),
         null: false
     end
@@ -12,5 +17,7 @@ defmodule Lightning.Repo.Migrations.RemoveEventFromRuns do
              references(:invocation_events, on_delete: :delete_all, type: :binary_id),
              null: true
     end
+
+    create index(:runs, [:previous_id])
   end
 end
