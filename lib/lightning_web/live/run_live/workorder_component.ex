258a72714a38a6949f1ae9d5b46a57fe733b5997
Mamadou WANE <wanecode@gmail.com>
2022-10-28 16:31:37 +0000

    add toggle workorder details

--- lib/lightning_web/live/run_live/workorder_component.ex
+++ lib/lightning_web/live/run_live/workorder_component.ex
@@ -2,6 +2,7 @@ defmodule LightningWeb.RunLive.Components.WorkOrder do
   @moduledoc """
   Workorder component
   """
+  use Phoenix.Component
   use LightningWeb, :live_component
   import LightningWeb.RunLive.Components
 
@@ -11,12 +12,14 @@ defmodule LightningWeb.RunLive.Components.WorkOrder do
   end
 
   @impl true
-  def handle_event("todo", %{}, socket) do
-    {:noreply, socket}
+  def handle_event("toggle-details", %{}, socket) do
+    {:noreply, assign(socket, :show_details, !socket.assigns[:show_details])}
   end
 
   @impl true
   def render(assigns) do
+    assigns = assigns |> assign_new(:show_details, fn -> false end)
+
     ~H"""
     <tr class="my-4 grid grid-cols-5 gap-4 rounded-lg bg-white">
       <th
@@ -26,7 +29,11 @@ defmodule LightningWeb.RunLive.Components.WorkOrder do
         <%= @work_order.workflow.name %>
       </th>
       <td class="my-auto p-6"><%= @work_order.reason.dataclip_id %></td>
-      <td class="my-auto p-6">12i78iy</td>
+      <td class="my-auto p-6">
+        <%= live_redirect to: Routes.project_dataclip_edit_path(@socket, :edit, @work_order.workflow.project_id, @work_order.reason.dataclip_id) do %>
+        <div><%= @work_order.reason.dataclip_id %></div>
+        <% end %>
+      </td>
       <td class="my-auto p-6">
         <%= @work_order.last_attempt.last_run.finished_at |> Calendar.strftime("%c") %>
       </td>
@@ -40,17 +47,27 @@ defmodule LightningWeb.RunLive.Components.WorkOrder do
             <% _ -> %>
           <% end %>
 
-          <button class="w-auto rounded-full bg-gray-50 p-3">
-            <Heroicons.Outline.chevron_down class="h-5 w-5" />
+          <button
+            class="w-auto rounded-full bg-gray-50 p-3 hover:bg-gray-100"
+            phx-click="toggle-details"
+            phx-target={@myself}
+          >
+            <%= if @show_details do %>
+              <Heroicons.Outline.chevron_up class="h-5 w-5" />
+            <% else %>
+              <Heroicons.Outline.chevron_down class="h-5 w-5" />
+            <% end %>
           </button>
         </div>
       </td>
-      <%= for attempt <- @work_order.attempts do %>
-        <.live_component
-          module={LightningWeb.RunLive.Components.Attempt}
-          id={attempt.id}
-          attempt={attempt}
-        />
+      <%= if @show_details do %>
+        <%= for attempt <- @work_order.attempts do %>
+          <.live_component
+            module={LightningWeb.RunLive.Components.Attempt}
+            id={attempt.id}
+            attempt={attempt}
+          />
+        <% end %>
       <% end %>
     </tr>
     """
