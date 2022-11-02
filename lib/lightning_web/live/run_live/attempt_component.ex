8ee29efb7d07c9e17e38550f2189030fbf1baf52
Mamadou WANE <wanecode@gmail.com>
2022-11-02 02:04:17 +0000

    move variable last_run from render to assign

--- lib/lightning_web/live/run_live/attempt_component.ex
+++ lib/lightning_web/live/run_live/attempt_component.ex
@@ -6,6 +6,13 @@ defmodule LightningWeb.RunLive.Components.Attempt do
 
   @impl true
   def update(assigns, socket) do
+    last_run = Enum.at(assigns.attempt.runs, 0)
+
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign(:last_run, last_run)
+
     {:ok, socket |> assign(assigns)}
   end
 
@@ -16,8 +23,6 @@ defmodule LightningWeb.RunLive.Components.Attempt do
 
   @impl true
   def render(assigns) do
-    last_run = Enum.at(assigns.attempt.runs, 0)
-
     ~H"""
     <td class="col-span-5 mx-3 mb-3 rounded-lg bg-gray-100 p-6">
       <ul class="list-inside list-none space-y-4 text-gray-500 dark:text-gray-400">
@@ -25,10 +30,10 @@ defmodule LightningWeb.RunLive.Components.Attempt do
           <span class="flex items-center">
             <Heroicons.clock solid class="mr-1 h-5 w-5" />
             <span>
-              Re-run at <%= last_run.finished_at
+              Re-run at <%= @last_run.finished_at
               |> Calendar.strftime("%c") %>
 
-              <%= case last_run.exit_code do %>
+              <%= case @last_run.exit_code do %>
                 <% val when val > 0-> %>
                   <span class="my-auto ml-2 whitespace-nowrap rounded-full bg-red-200 py-2 px-4 text-center align-baseline text-xs font-medium leading-none text-red-800">
                     Failure
