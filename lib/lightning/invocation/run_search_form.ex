4d5cdf00f7cd1a863d6482326bd3bbb614cb1012
Mamadou WANE <wanecode@gmail.com>
2022-11-10 20:21:26 +0000

    enrich search form embedded schema

--- lib/lightning/invocation/run_search_form.ex
+++ lib/lightning/invocation/run_search_form.ex
@@ -2,6 +2,9 @@ defmodule Lightning.RunSearchForm do
   use Ecto.Schema
 
   embedded_schema do
+    field :before, :utc_datetime_usec
+    field :after, :utc_datetime_usec
+    embeds_one :workflow, Lightning.Workflows.Workflow
     embeds_many :options, Lightning.RunSearchForm.RunStatusOption
   end
 
