f04247733d3c8c27bd0c3b095c9d452d10f02779
Mamadou WANE <wanecode@gmail.com>
2022-11-14 07:57:02 +0000

    add @module docs

--- lib/lightning/invocation/run_search_form.ex
+++ lib/lightning/invocation/run_search_form.ex
@@ -1,16 +1,19 @@
 defmodule Lightning.RunSearchForm do
+  @moduledoc """
+  Run filtering serach form.
+  """
   use Ecto.Schema
 
   embedded_schema do
     field :before, :utc_datetime_usec
     field :after, :utc_datetime_usec
     field :workflow_id, :string
-    # we'll need this one
     embeds_one :workflow, Lightning.Workflows.Workflow
     embeds_many :options, Lightning.RunSearchForm.RunStatusOption
   end
 
   defmodule RunStatusOption do
+    @moduledoc false
     use Ecto.Schema
 
     embedded_schema do
