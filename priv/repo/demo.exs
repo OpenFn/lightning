fa0eb81318de0faa4731153468a14e0013186dd2
Taylor Downs <downs.taylor@gmail.com>
2022-11-17 05:51:38 -0500

    fixes the issue on #419

--- priv/repo/demo.exs
+++ priv/repo/demo.exs
@@ -10,5 +10,5 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-Lightning.Demo.setup(create_super: true)
 Lightning.Demo.tear_down(destroy_super: true)
+Lightning.Demo.setup(create_super: true)
