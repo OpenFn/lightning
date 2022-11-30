75a6fe84447319680ebecdc6c1558b0d87ce2359
Elias W. BA <eliaswalyba@gmail.com>
2022-11-30 08:38:10 +0000

    Create flash components

--- lib/lightning_web/live/components/common.ex
+++ lib/lightning_web/live/components/common.ex
@@ -2,6 +2,8 @@ defmodule LightningWeb.Components.Common do
   @moduledoc false
   use LightningWeb, :component
 
+  alias Phoenix.LiveView.JS
+
   def button(assigns) do
     class =
       button_classes(
@@ -115,6 +117,90 @@ defmodule LightningWeb.Components.Common do
     <div class={@class} id={@id}>
       <%= render_slot(@inner_block) %>
     </div>
+    """
+  end
+
+  def flash(%{kind: :error} = assigns) do
+    ~H"""
+    <div
+      :if={msg = Phoenix.Flash.get(@flash, @kind)}
+      id="flash"
+      class="rounded-md bg-red-50 p-4 fixed top-1 right-1 w-96 fade-in-scale z-50"
+      phx-click={
+        JS.push("lv:clear-flash")
+        |> JS.remove_class("fade-in-scale", to: "#flash")
+        |> hide("#flash")
+      }
+      phx-hook="Flash"
+    >
+      <div class="flex justify-between items-center space-x-3 text-red-700">
+        <.icon name={:exclamation_circle} class="w-5 w-5" />
+        <p class="flex-1 text-sm font-medium" role="alert">
+          <%= msg %>
+        </p>
+        <button
+          type="button"
+          class="inline-flex bg-red-50 rounded-md p-1.5 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-50 focus:ring-red-600"
+        >
+          <.icon name={:x} class="w-4 h-4" />
+        </button>
+      </div>
+    </div>
+    """
+  end
+
+  def flash(%{kind: :info} = assigns) do
+    ~H"""
+    <div
+      :if={msg = Phoenix.Flash.get(@flash, @kind)}
+      id="flash"
+      class="rounded-md bg-green-50 p-4 fixed top-1 right-1 w-96 fade-in-scale z-50"
+      phx-click={
+        JS.push("lv:clear-flash")
+        |> JS.remove_class("fade-in-scale")
+        |> hide("#flash")
+      }
+      phx-value-key="info"
+      phx-hook="Flash"
+    >
+      <div class="flex justify-between items-center space-x-3 text-green-700">
+        <.icon name={:check_circle} class="w-5 h-5" />
+        <p class="flex-1 text-sm font-medium" role="alert">
+          <%= msg %>
+        </p>
+        <button
+          type="button"
+          class="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
+        >
+          <.icon name={:x} class="w-4 h-4" />
+        </button>
+      </div>
+    </div>
+    """
+  end
+
+  defp hide(js \\ %JS{}, selector) do
+    JS.hide(js,
+      to: selector,
+      time: 300,
+      transition:
+        {"transition ease-in duration-300", "transform opacity-100 scale-100",
+         "transform opacity-0 scale-95"}
+    )
+  end
+
+  defp icon(assigns) do
+    assigns =
+      assign_new(assigns, :"aria-hidden", fn ->
+        !Map.has_key?(assigns, :"aria-label")
+      end)
+
+    ~H"""
+    <%= if @outlined do %>
+      <%= apply(Heroicons.Outline, @name, [Map.to_list(@rest)]) %>
+    <% else %>
+      <%= apply(Heroicons.Solid, @name, [Map.to_list(@rest)]) %>
+    <% end %>
     """
   end
 end
