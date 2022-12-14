ffd0cc7397b5e8c270f1aea06ce15c3f6257b18d
Elias W. BA <eliaswalyba@gmail.com>
2022-12-14 08:54:25 +0000

    Oban manager marks run as finished, Unit test Oban manager

--- lib/lightning/oban_manager.ex
+++ lib/lightning/oban_manager.ex
@@ -1,8 +1,12 @@
-defmodule ObanManager do
+defmodule Lightning.ObanManager do
   @moduledoc """
   The Oban Manager
   """
   require Logger
+
+  alias Lightning.Repo
+  alias Lightning.AttemptRun
+  alias Lightning.Invocation
 
   @doc """
   handles oban events
@@ -13,9 +17,7 @@ defmodule ObanManager do
   def handle_event([:oban, :circuit, :trip], _measure, meta, _pid) do
     Logger.error("Circuit tripped with #{inspect(meta, pretty: true)}")
 
-    context =
-      Map.take(meta, [:name])
-      |> scrub_context
+    context = Map.take(meta, [:name])
 
     Sentry.capture_exception(meta.error,
       stacktrace: meta.stacktrace,
@@ -26,6 +28,13 @@ defmodule ObanManager do
   end
 
   def handle_event([:oban, :job, :exception], measure, meta, _pid) do
+    Repo.get!(AttemptRun, meta.args["attempt_run_id"])
+    |> Ecto.assoc(:run)
+    |> Repo.one!()
+    |> Invocation.update_run(%{
+      finished_at: DateTime.utc_now()
+    })
+
     Logger.error(~s"""
     Oban exception:
     #{inspect(meta.error)}
@@ -39,13 +48,8 @@ defmodule ObanManager do
       meta
       |> Map.take([:id, :args, :queue, :worker])
       |> Map.merge(measure)
-      |> scrub_context
 
-    # args = meta.args
-    # queue = meta.queue
     error = meta.error
-
-    # dead? = meta.attempt >= meta.max_attempts
     timeout? = Map.get(error, :reason) == :timeout
 
     if timeout? do
@@ -64,140 +68,5 @@ defmodule ObanManager do
         tags: %{type: "oban"}
       )
     end
-
-    # Note: if this is a failed run, we need to ensure that it gets an exit code
-    # and that any subsequent "catch" jobs are initialized. This is done via a
-    # task because—at this point—the best we can do is "try" and we don't want
-    # to tie up the success of this Oban event handler with the success of our
-    # "rescue effort".
-    # if dead? && String.contains?(queue, "runs"),
-    #   do:
-    #     Task.start(__MODULE__, :handle_crashed_run, [
-    #       get_in(args, ["run", "id"]),
-    #       get_in(args, ["state"]),
-    #       error,
-    #       timeout?
-    #     ])
-
-    # handle_crashed_run(
-    #     get_in(args, ["run", "id"]),
-    #     get_in(args, ["state"]),
-    #     error,
-    #     timeout?
-    #   )
-  end
-
-  # @doc """
-  # Attempt to create a record of the crash, updating an existing run in the db.
-  # """
-
-  # def handle_crashed_run(run_id, state, error, timeout?) do
-  #   run = Repo.get(Run, run_id)
-
-  #   result =
-  #     if timeout? do
-  #       %Engine.Result{
-  #         exit_code: 4,
-  #         log: [
-  #           "==== TIMEOUT && UNRESPONSIVE ===================================================",
-  #           "",
-  #           "We had to shut down this run because it timed out and became unresponsive.",
-  #           "Here's what to do:",
-  #           "",
-  #           " - Check your destination system to ensure it's working and responding properly",
-  #           "   to API requests.",
-  #           " - Check your job expression to make sure you haven't created any infinite loops",
-  #           "   or long sleep/wait commands.",
-  #           "",
-  #           "Only enterprise plans support runs lasting more than 100 seconds.",
-  #           "Contact enterprise@openfn.org to enable long-running jobs."
-  #         ]
-  #       }
-  #     else
-  #       case error do
-  #         %Dispatcher.AdaptorError{message: message, code: nil} ->
-  #           %Engine.Result{exit_code: 7, log: [message]}
-
-  #         %Dispatcher.AdaptorError{message: message, code: code, log: log} ->
-  #           %Engine.Result{
-  #             exit_code: 7,
-  #             log:
-  #               ~s"""
-  #               #{message}:
-  #               #{log}
-  #               Exited with #{code}
-  #               """
-  #               |> String.split("\n")
-  #           }
-
-  #         _other ->
-  #           %Engine.Result{
-  #             exit_code: 5,
-  #             log: [
-  #               "OpenFn encountered an unexpected error during the execution of this job:",
-  #               inspect(error, pretty: true)
-  #             ]
-  #           }
-  #       end
-  #     end
-
-  #   result
-  #   |> RunService.ensure_result_handled(run)
-  #   |> FlowService.ensure_flow_controlled(state)
-  #   |> clean_timeouts_for_run()
-  # end
-
-  # @doc """
-  # Attempt to create a record of the crash, updating an existing run in the db.
-  # """
-
-  # def handle_orphaned_run(run_id, state) do
-  #   run = Repo.get(Run, run_id)
-
-  #   %Engine.Result{
-  #     exit_code: 4,
-  #     log: [
-  #       "==== LOST CONNECTION TO NODE ===================================================",
-  #       "",
-  #       "We lost connection to the node that was handling this run. It may have taken",
-  #       "longer than the grace period. Here's what to do:",
-  #       "",
-  #       " - Check your destination system to ensure it's working and responding properly",
-  #       "   to API requests.",
-  #       " - Check your job expression to make sure you haven't created any infinite loops",
-  #       "   or long sleep/wait commands.",
-  #       "",
-  #       "Only enterprise plans support runs lasting more than 100 seconds.",
-  #       "Contact enterprise@openfn.org to enable long-running jobs."
-  #     ]
-  #   }
-  #   |> RunService.ensure_result_handled(run)
-  #   |> FlowService.ensure_flow_controlled(state)
-  # end
-
-  # @doc """
-  # Given a run which we are certain has been handled properly (i.e., exit_code
-  # has been set, FlowService has been called), clean_timeouts_for_run will remove
-  # the 'discarded' Oban jobs related to that run from the DB. Normally, discarded
-  # Oban Jobs stay in the DB to be manually removed during daily reviews, but exit
-  # code 4 (an unresponsive NodeVM) happens frequently enough that we should
-  # automate this process. A record of unresponsive VMs can (and should) be
-  # reviewed to ensure that it happens infrequently, but this decouples that
-  # process from the normal operation of the platform which, sadly, includes and
-  # handles unresponsive NodeVMs as part of its standard operating procedure.
-  # """
-
-  # def clean_timeouts_for_run(%Run{id: id}) do
-  #   Logger.warn(
-  #     "Deleting discarded ObanJob(s) for Run ##{id}; it failed with Oban.TimeoutError and was handled."
-  #   )
-
-  #   ObanQuery.timeouts_for_run(id)
-  #   |> Repo.delete_all()
-  # end
-
-  defp scrub_context(context) do
-    {_private_state, safe_context} = pop_in(context, [:args, "state"])
-    safe_context
   end
 end
--- test/lightning/crash_test.exs
+++ test/lightning/crash_test.exs
@@ -1,11 +1,16 @@
 defmodule Lightning.CrashTest do
   use Lightning.DataCase, async: true
-  use Oban.Testing, repo: Lightning.Repo
+  # use Oban.Testing, repo: Lightning.Repo
 
   alias Lightning.Pipeline
+  alias Lightning.ObanManager
+  alias Lightning.AttemptRun
+  alias Lightning.Attempt
+  alias Lightning.Invocation.{Run}
+  alias Lightning.Repo
+
   import Lightning.JobsFixtures
   import Lightning.InvocationFixtures
-  import Lightning.CredentialsFixtures
   import Lightning.ProjectsFixtures
 
   describe "Crash test" do
@@ -13,38 +18,28 @@ defmodule Lightning.CrashTest do
       Application.put_env(:lightning, :max_run_duration, 5 * 1000)
       project = project_fixture()
 
-      project_credential =
-        project_credential_fixture(
-          name: "test credential",
-          body: %{"username" => "foo", "password" => "bar"},
-          project_id: project.id
+      run =
+        run_fixture(
+          job_id:
+            workflow_job_fixture(
+              body: ~s[fn(state => {
+                return new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    console.log('wait, and then resolve');
+                    resolve(state);
+                  }, 10 * 1000);
+                });
+              });],
+              project_id: project.id
+            ).id,
+          input_dataclip_id:
+            dataclip_fixture(
+              body: %{"foo" => "bar"},
+              project_id: project.id,
+              type: :http_request
+            ).id
         )
 
-      job =
-        workflow_job_fixture(
-          adaptor: "@openfn/language-common",
-          body: """
-          fn(state => {
-            return new Promise((resolve, reject) => {
-              setTimeout(() => {
-                console.log('wait, and then resolve');
-                resolve(state);
-              }, 10 * 1000);
-            });
-          });
-          """,
-          project_id: project.id,
-          project_credential_id: project_credential.id
-        )
-
-      dataclip =
-        dataclip_fixture(
-          body: %{"foo" => "bar"},
-          project_id: project.id,
-          type: :http_request
-        )
-
-      run = run_fixture(job_id: job.id, input_dataclip_id: dataclip.id)
       result = %Engine.Result{} = Pipeline.Runner.start(run)
 
       assert result.exit_reason == :killed
@@ -61,6 +56,61 @@ defmodule Lightning.CrashTest do
       refute is_nil(run.started_at)
       refute is_nil(run.finished_at)
       assert run.exit_code == nil
+    end
+  end
+
+  describe "Oban Manager" do
+    test "handle_event/4 marks a job as finished" do
+      job = workflow_job_fixture(body: ~s[fn(state => {
+            return new Promise((resolve, reject) => {
+              setTimeout(() => {
+                console.log('wait, and then resolve');
+                resolve(state);
+              }, 10 * 1000);
+            });
+          });])
+
+      dataclip = dataclip_fixture()
+
+      {:ok, attempt_run} =
+        AttemptRun.new()
+        |> Ecto.Changeset.put_assoc(
+          :attempt,
+          Attempt.changeset(%Attempt{}, %{
+            work_order_id: work_order_fixture(workflow_id: job.workflow_id).id,
+            reason_id:
+              reason_fixture(
+                trigger_id: job.trigger.id,
+                dataclip_id: dataclip.id
+              ).id
+          })
+        )
+        |> Ecto.Changeset.put_assoc(
+          :run,
+          Run.changeset(%Run{}, %{
+            project_id: job.workflow.project_id,
+            job_id: job.id,
+            input_dataclip_id: dataclip.id
+          })
+        )
+        |> Repo.insert()
+
+      assert attempt_run.run.finished_at |> Kernel.is_nil()
+
+      ObanManager.handle_event(
+        [:oban, :job, :exception],
+        %{duration: 5_096_921_850, queue_time: 106_015_000},
+        %{
+          args: %{"attempt_run_id" => attempt_run.id},
+          error: %CaseClauseError{term: :killed},
+          stacktrace: []
+        },
+        nil
+      )
+
+      run = Repo.get!(Run, attempt_run.run.id)
+
+      assert run.finished_at |> Kernel.is_nil() |> Kernel.not()
     end
   end
 end
