e854bbc4722ce327eef7ac9c086232338f1835f9
Mamadou WANE <wanecode@gmail.com>
2022-12-19 16:24:48 +0000

    handle webhook post on disabled job

--- lib/lightning_web/controllers/webhooks_controller.ex
+++ lib/lightning_web/controllers/webhooks_controller.ex
@@ -13,6 +13,13 @@ defmodule LightningWeb.WebhooksController do
         put_status(conn, :not_found)
         |> json(%{})
 
+      %Jobs.Job{enabled: false} ->
+        put_status(conn, :forbidden)
+        |> json(%{
+          message:
+            "Unable to process request, trigger is disabled. Enable it on OpenFn to allow requests to this endpoint."
+        })
+
       job ->
         {:ok, %{work_order: work_order, attempt_run: attempt_run}} =
           WorkOrderService.create_webhook_workorder(job, conn.body_params)
