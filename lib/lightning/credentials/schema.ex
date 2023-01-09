fcbe19af2c11e4d5fe52bf2877dd2d44e76b1809
Stuart Corbishley <corbish@gmail.com>
2023-01-09 14:31:14 +0200

    Update schema.ex

--- lib/lightning/credentials/schema.ex
+++ lib/lightning/credentials/schema.ex
@@ -64,9 +64,6 @@ defmodule Lightning.Credentials.Schema do
       %{actual: 0, expected: _} ->
         add_error(changeset, field, "can't be blank")
 
-      %{actual: "null", expected: ["string"]} ->
-        add_error(changeset, field, "can't be blank")
-
       %{actual: "null", expected: expected} when is_list(expected) ->
         add_error(changeset, field, "can't be blank")
     end
