d781e8ce01ed36e8c4d5f90680fdfad8f2cdd5eb
Mamadou WANE <wanecode@gmail.com>
2023-01-09 06:23:19 +0000

    add install_schemas failure tests

--- test/lightning/install_schemas_test.exs
+++ test/lightning/install_schemas_test.exs
@@ -4,6 +4,10 @@ defmodule Lightning.InstallSchemasTest do
 
   alias Mix.Tasks.Lightning.InstallSchemas
 
+  @request_options [recv_timeout: 15_000, pool: :default]
+  @ok_200 {:ok, 200, "headers", :client}
+  @ok_400 {:ok, 400, "headers", :client}
+
   describe "install_schemas mix task" do
     setup do
       stub(:hackney)
@@ -11,14 +15,14 @@ defmodule Lightning.InstallSchemasTest do
       :ok
     end
 
-    test "run" do
+    test "run success" do
       expect(:hackney, :request, fn
         :get,
         "https://registry.npmjs.org/-/user/openfn/package",
         [],
         "",
-        [recv_timeout: 15_000, pool: :default] ->
-          {:ok, 200, "headers", :client}
+        @request_options ->
+          @ok_200
       end)
 
       expect(:hackney, :body, fn :client, _timeout ->
@@ -31,8 +35,8 @@ defmodule Lightning.InstallSchemasTest do
         "https://cdn.jsdelivr.net/npm/@openfn/language-asana/configuration-schema.json",
         [],
         "",
-        [recv_timeout: 15_000, pool: :default] ->
-          {:ok, 200, "headers", :client}
+        @request_options ->
+          @ok_200
       end)
 
       expect(:hackney, :body, fn :client, _timeout ->
@@ -44,8 +48,8 @@ defmodule Lightning.InstallSchemasTest do
         "https://cdn.jsdelivr.net/npm/@openfn/language-primero/configuration-schema.json",
         [],
         "",
-        [recv_timeout: 15_000, pool: :default] ->
-          {:ok, 200, "headers", :client}
+        @request_options ->
+          @ok_200
       end)
 
       expect(:hackney, :body, fn :client, _timeout ->
@@ -53,6 +57,7 @@ defmodule Lightning.InstallSchemasTest do
       end)
 
       File
+      |> expect(:rm_rf, fn _ -> nil end)
       |> expect(:mkdir_p, fn _ -> nil end)
       |> expect(:open!, fn "priv/schemas/asana.json", [:write] -> nil end)
       |> expect(:open!, fn "priv/schemas/primero.json", [:write] -> nil end)
@@ -65,6 +70,105 @@ defmodule Lightning.InstallSchemasTest do
       # |> expect(:binwrite, fn _, ~s({"name": "language-common"}) -> nil end)
 
       InstallSchemas.run([])
+    end
+
+    test "run fail" do
+      expect(File, :rm_rf, fn _ -> {:error, "error occured"} end)
+      expect(File, :mkdir_p, fn _ -> {:error, "error occured"} end)
+
+      assert_raise RuntimeError,
+                   "Couldn't create the schemas directory: priv/schemas/, got :error occured.",
+                   fn ->
+                     InstallSchemas.run([])
+                   end
+    end
+
+    test "persist_schema fail 1" do
+      expect(:hackney, :request, fn
+        :get,
+        "https://cdn.jsdelivr.net/npm/@openfn/language-asana/configuration-schema.json",
+        [],
+        "",
+        @request_options ->
+          @ok_200
+      end)
+
+      expect(:hackney, :body, fn :client, _timeout ->
+        {:error, %HTTPoison.Error{}}
+      end)
+
+      assert_raise RuntimeError, "Unable to access @openfn/language-asana", fn ->
+        InstallSchemas.persist_schema("@openfn/language-asana")
+      end
+    end
+
+    test "persist_schema fail 2" do
+      expect(:hackney, :request, fn
+        :get,
+        "https://cdn.jsdelivr.net/npm/@openfn/language-asana/configuration-schema.json",
+        [],
+        "",
+        @request_options ->
+          @ok_400
+      end)
+
+      expect(:hackney, :body, fn :client, _timeout ->
+        {:ok, %HTTPoison.Response{status_code: 400}}
+      end)
+
+      assert_raise RuntimeError, fn ->
+        InstallSchemas.persist_schema("@openfn/language-asana")
+      end
+    end
+
+    test "fetch_schemas fail 1" do
+      expect(:hackney, :request, fn
+        :get,
+        "https://registry.npmjs.org/-/user/openfn/package",
+        [],
+        "",
+        @request_options ->
+          @ok_200
+      end)
+
+      expect(:hackney, :body, fn :client, _timeout ->
+        {:error, %HTTPoison.Error{}}
+      end)
+
+      assert_raise RuntimeError,
+                   "Unable to connect to NPM; no adaptors fetched.",
+                   fn ->
+                     InstallSchemas.fetch_schemas([])
+                   end
+    end
+
+    test "fetch_schemas fail 2" do
+      expect(:hackney, :request, fn
+        :get,
+        "https://registry.npmjs.org/-/user/openfn/package",
+        [],
+        "",
+        @request_options ->
+          @ok_400
+      end)
+
+      expect(:hackney, :body, fn :client, _timeout ->
+        {:ok, %HTTPoison.Response{status_code: 400}}
+      end)
+
+      assert_raise RuntimeError,
+                   "Unable to access openfn user packages. status=400",
+                   fn ->
+                     InstallSchemas.fetch_schemas([])
+                   end
+    end
+
+    test "parse_excluded" do
+      assert ["pack1", "pack2", "language-common", "language-devtools"] ==
+               InstallSchemas.parse_excluded(["--exclude", "pack1", "pack2"])
+
+      assert ["language-common", "language-devtools"] ==
+               InstallSchemas.parse_excluded([])
     end
   end
 end
