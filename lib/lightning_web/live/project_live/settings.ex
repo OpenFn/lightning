c3a69ab02556701285d3d37631d83bf1a57d2795
Elias W. BA <eliaswalyba@gmail.com>
2023-01-10 18:28:41 +0000

    Layout project settings page

---
+++ lib/lightning_web/live/project_live/settings.ex
@@ -0,0 +1,24 @@
+defmodule LightningWeb.ProjectLive.Settings do
+  @moduledoc """
+  Index Liveview for Runs
+  """
+  use LightningWeb, :live_view
+
+  on_mount {LightningWeb.Hooks, :project_scope}
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(active_menu_item: :settings)}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, socket |> apply_action(socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket |> assign(:page_title, "Projects settings")
+  end
+end
