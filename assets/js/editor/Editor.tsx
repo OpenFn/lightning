7c7bb431c38ceee52dfad4d4d542ec1fb70650d5
Joe Clark <josephjclark@gmail.com>
2023-01-17 17:37:23 +0000

    editor: add a loading indicator

--- assets/js/editor/Editor.tsx
+++ assets/js/editor/Editor.tsx
@@ -12,6 +12,34 @@ type EditorProps = {
   onChange?: (newSource: string) => void;
 }
 
+const spinner = (<svg
+  className="animate-spin h-5 w-5 inline-block"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  >
+  <circle
+    className="opacity-25"
+    cx="12"
+    cy="12"
+    r="10"
+    stroke="currentColor"
+    stroke-width="4"
+  >
+  </circle>
+  <path
+    className="opacity-75"
+    fill="currentColor"
+    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+  >
+  </path>
+</svg>);
+
+const loadingIndicator = (<div className="inline-block bg-vs-dark p-2">
+  <span className="mr-2">Loading</span>
+  {spinner}
+</div>);
+
 // https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.IStandaloneEditorConstructionOptions.html
 const defaultOptions: MonacoProps['options'] = {
   dragAndDrop: false,
@@ -68,6 +96,7 @@ async function loadDTS(specifier: string, type: 'namespace' | 'module' = 'namesp
 
 export default function Editor({ source, adaptor, onChange }: EditorProps) {
   const [lib, setLib] = useState<Lib[]>();
+  const [loading, setLoading] = useState(false);
   const [monaco, setMonaco] = useState<typeof Monaco>();
   const [options, setOptions] = useState(defaultOptions);
   const listeners = useRef<{ insertSnippet?: EventListenerOrEventListenerObject}>({});
@@ -142,8 +171,12 @@ export default function Editor({ source, adaptor, onChange }: EditorProps) {
   
   useEffect(() => {
     if (adaptor) {
+      setLoading(true)
       setLib([]); // instantly clear intelligence
-      loadDTS(adaptor).then(l => setLib(l));
+      loadDTS(adaptor).then(l => {
+        setLib(l)
+        setLoading(false)
+      });
     }
   }, [adaptor])
 
@@ -153,13 +186,19 @@ export default function Editor({ source, adaptor, onChange }: EditorProps) {
     }
   }, [monaco, lib]);
   
-  return (<Monaco
-    defaultLanguage="javascript"
-    loading=""
-    theme="vs-dark"
-    value={source || DEFAULT_TEXT}
-    options={options}
-    onMount={handleEditorDidMount}
-    onChange={handleSourceChange}
-  />)
+  return (
+    <>
+      <div className="text-xs text-white text-right pr-2 h-0 z-10 overflow-visible relative">
+        {loading && loadingIndicator}
+      </div>
+      <Monaco
+        defaultLanguage="javascript"
+        theme="vs-dark"
+        value={source || DEFAULT_TEXT}
+        options={options}
+        onMount={handleEditorDidMount}
+        onChange={handleSourceChange}
+      />
+    </>
+  );
 }
