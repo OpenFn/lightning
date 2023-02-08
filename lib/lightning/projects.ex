610303973892e24a52c8df0cf8f29a3a2df94e3b
Mamadou WANE <wanecode@gmail.com>
2023-02-08 15:51:50 +0200

    complete tests

--- lib/lightning/projects.ex
+++ lib/lightning/projects.ex
@@ -254,6 +254,10 @@ defmodule Lightning.Projects do
 end
 
 defmodule ImportUtils do
+  @moduledoc """
+  Module that expose a function building a multi for importing a project yaml (via a map)
+  """
+
   import Ecto.Changeset
 
   alias Ecto.Multi
@@ -267,9 +271,7 @@ defmodule ImportUtils do
     Multi.new()
     |> put_project(project_data, user)
     |> put_credentials(project_data, user)
-    # |> put_project_credentials(project_data, user)
-
-    # |> put_workflows(project_data)
+    |> put_workflows(project_data)
   end
 
   defp put_project(multi, project_data, user) do
@@ -314,16 +316,30 @@ defmodule ImportUtils do
   end
 
   def import_job_changeset(job, attrs, workflow_id, import_transaction) do
-    credential = import_transaction["credential::#{attrs[:credential]}"]
-    %{project_credentials: [%{id: project_credential_id}]} = credential
-    attrs = Map.put(attrs, :project_credential_id, project_credential_id)
+    credential_key = attrs[:credential]
 
-    job
-    |> Job.changeset(attrs, workflow_id)
+    if credential_key do
+      credential = import_transaction["credential::#{credential_key}"]
+
+      if credential do
+        %{project_credentials: [%{id: project_credential_id}]} = credential
+        attrs = Map.put(attrs, :project_credential_id, project_credential_id)
+
+        job
+        |> Job.changeset(attrs, workflow_id)
+      else
+        job
+        |> Job.changeset(attrs, workflow_id)
+        |> add_error(:credential, "not found in project input")
+      end
+    else
+      job
+      |> Job.changeset(attrs, workflow_id)
+    end
   end
 
   defp put_workflows(multi, project_data) do
-    workflows = project_data[:workflows]
+    workflows = project_data[:workflows] || []
 
     workflows
     |> Enum.reduce(multi, fn workflow, m ->
@@ -340,42 +356,14 @@ defmodule ImportUtils do
     end)
   end
 
-  # defp put_credentials(multi, project_data, user) do
-  #   credentials = project_data[:credentials]
-
-  #   credentials
-  #   |> Enum.reduce(multi, fn credential, m ->
-  #     Multi.insert(m, "credential::#{credential.key}", fn %{
-  #                                                           project: project
-  #                                                         } ->
-  #       id = Ecto.UUID.generate()
-
-  #       attrs =
-  #         credential
-  #         |> Map.put(:id, id)
-  #         |> Map.put(:user_id, user.id)
-  #         |> Map.put(:project_credentials, [
-  #           %{
-  #             project_id: project.id,
-  #             credential_id: id
-  #           }
-  #         ])
-
-  #       %Credential{}
-  #       |> Credential.changeset(attrs)
-  #     end)
-  #   end)
-  # end
-
   defp put_credentials(multi, project_data, user) do
-    credentials = project_data[:credentials]
+    credentials = project_data[:credentials] || []
 
-    multi
-    |> Ecto.Multi.insert_all(:insert_all, Credential, fn %{project: project} ->
-      # Others validations
-
-      credentials
-      |> Enum.map(fn credential ->
+    credentials
+    |> Enum.reduce(multi, fn credential, m ->
+      Multi.insert(m, "credential::#{credential.key}", fn %{
+                                                            project: project
+                                                          } ->
         id = Ecto.UUID.generate()
 
         attrs =
@@ -391,31 +379,7 @@ defmodule ImportUtils do
 
         %Credential{}
         |> Credential.changeset(attrs)
-        |> apply_changes()
-        |> IO.inspect()
-        |> Map.from_struct()
-        # or something similar
-        |> Stream.reject(fn
-          {_key, nil} ->
-            true
-
-          {key, %_struct{}} ->
-            # rejects __meta__: #Ecto.Schema.Metadata<:built, "items">
-            # and association: #Ecto.Association.NotLoaded<association :association is not loaded>
-            true
-
-          _other ->
-            false
-        end)
       end)
     end)
   end
-
-
-  # defp put_project_credentials(_multi, project_data, _user) do
-  #   # credentials = project_data[:credentials]
-
-  #   IO.inspect(%{project_data: project_data})
-
-  # end
 end
