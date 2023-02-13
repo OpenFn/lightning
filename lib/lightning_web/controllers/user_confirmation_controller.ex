ca3f43049977ffe17ab0b3073ff39e376998fb2c
mtuchi <mtuchidev@gmail.com>
2023-02-13 17:18:57 +0200

    wip: email confirmation

--- lib/lightning_web/controllers/user_confirmation_controller.ex
+++ lib/lightning_web/controllers/user_confirmation_controller.ex
@@ -28,6 +28,10 @@ defmodule LightningWeb.UserConfirmationController do
     render(conn, "edit.html", token: token)
   end
 
+  def confirm_email(conn, %{"token" => token}) do
+    render(conn, "confirm_email.html", token: token)
+  end
+
   # Do not log in the user after confirmation to avoid a
   # leaked token giving the user access to the account.
   def update(conn, %{"token" => token}) do
@@ -44,6 +48,34 @@ defmodule LightningWeb.UserConfirmationController do
         # a warning message.
         case conn.assigns do
           %{current_user: %{confirmed_at: confirmed_at}}
+          when not is_nil(confirmed_at) ->
+            redirect(conn, to: "/")
+
+          %{} ->
+            conn
+            |> put_flash(
+              :error,
+              "User confirmation link is invalid or it has expired."
+            )
+            |> redirect(to: "/")
+        end
+    end
+  end
+
+  def update_email(conn, %{"user" => user, "token" => token}) do
+    case Accounts.update_user_email(user, token) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "User confirmed successfully.")
+        |> redirect(to: "/")
+
+      :error ->
+        # If there is a current user and the account was already confirmed,
+        # then odds are that the confirmation link was already visited, either
+        # by some automation or by the user themselves, so we redirect without
+        # a warning message.
+        case conn.assigns do
+          %{user: %{confirmed_at: confirmed_at}}
           when not is_nil(confirmed_at) ->
             redirect(conn, to: "/")
 
