965b2deac9e65fabb2a19f41e67b4f70ea7f660e
Stuart Corbishley <corbish@gmail.com>
2023-02-22 11:08:54 +0200

    Fix credential modal when in JobBuilder

--- lib/lightning_web/live/workflow_live.ex
+++ lib/lightning_web/live/workflow_live.ex
@@ -276,6 +276,15 @@ defmodule LightningWeb.WorkflowLive do
      )}
   end
 
+  @doc """
+  A generic handler for forwarding updates from PubSub
+  """
+  @impl true
+  def handle_info({:forward, mod, opts}, socket) do
+    send_update(mod, opts)
+    {:noreply, socket}
+  end
+
   @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
