8afa1ee1ac869346734c1679bf7585f1ec9dec76
Joe Clark <jclark@openfn.org>
2023-03-03 14:57:37 +0000

    job-editor: nested tab controller

--- assets/js/job-editor/JobEditor.tsx
+++ assets/js/job-editor/JobEditor.tsx
@@ -1,13 +1,38 @@
 import React, { useState, useCallback, useMemo } from 'react';
 import { ViewColumnsIcon, ChevronLeftIcon, ChevronRightIcon, ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/outline'
 
+import Docs from '../adaptor-docs/Docs';
 import Editor from '../editor/Editor';
+import Metadata from '../metadata-explorer/Explorer';
 
 const iconStyle = "cursor-pointer h-6 w-6"
 
+const Tabs = ({ options, onSelectionChange }: { options: string[], onSelectionChange?: (newName: string) =>void }) => {
+  const [selected, setSelected ] = useState(options[0]);
+
+  const handleSelectionChange = (name: string) => {
+    if (name !== selected) {
+      setSelected(name);
+      onSelectionChange?.(name);
+    }
+  }
+
+  return (
+    <nav className="flex space-x-4 w-full" aria-label="Tabs">
+       {options.map((name) => {
+          const style = name === selected ? 
+            'bg-gray-100 text-gray-700' : 'text-gray-500 hover:text-gray-700'
+          return <div onClick={() => handleSelectionChange(name)} className={`${style} rounded-md px-3 py-2 text-sm font-medium cursor-pointer`}>{name}</div>
+        })
+      }
+    </nav>
+  )
+}
+
 export default ({ adaptor, source }) => {
-  const [vertical, setVertical] = useState(true);
+  const [vertical, setVertical] = useState(false);
   const [showPanel, setShowPanel] = useState(true);
+  const [selectedTab, setSelectedTab] = useState('Docs');
 
   const toggleOrientiation = useCallback(() => {
     setVertical(!vertical)
@@ -33,22 +58,25 @@ export default ({ adaptor, source }) => {
     }
   }, [vertical, showPanel])
 
+  // TODO too many complex style rules embedded in this - is there a better approach?
   return (<>
   <div className="cursor-pointer" >
   </div>
   <div className={`flex h-full v-full flex-${vertical ? 'col' : 'row'}`}>
     <div className="flex flex-1 rounded-md border border-secondary-300 shadow-sm bg-vs-dark">
-      {/* TODO need this to resize nicely dynamically */}
       <Editor source={source} adaptor={adaptor} />
     </div>
-    <div className={`${showPanel ? 'flex flex-col flex-1' : ''} bg-white`}>
-      <div className={`flex flex-${!vertical && !showPanel ? 'col' : 'row'} text-right`}>
+    <div className={`${showPanel ? 'flex flex-col flex-1' : ''} bg-white ${vertical && 'overflow-auto'}`}>
+      <div className={`flex flex-${!vertical && !showPanel ? 'col' : 'row'} w-full justify-items-end sticky`}>
+        <Tabs options={['Docs', 'Metadata']} onSelectionChange={setSelectedTab}/>
         <ViewColumnsIcon className={iconStyle} onClick={toggleOrientiation} />
         <CollapseIcon className={iconStyle} onClick={toggleShowPanel} />
       </div>
       {showPanel && 
-        <div className="h-full v-full bg-slate-300">
-          panel
+        <div className={`h-full v-full ${!vertical && 'overflow-auto' || ''}`}>
+          {/* TODO ideally we wouldn't re-render the component from scratch? */}
+          {selectedTab === 'Docs' && <Docs adaptor={adaptor} />}
+          {selectedTab === 'Metadata' && <Metadata metadata={undefined} />}
         </div>
       }
     </div>
