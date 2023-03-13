11327d588cfb73bd9a23bcdc0c651b3e4020a2f8
Taylor Downs <downs.taylor@gmail.com>
2023-03-13 14:34:41 +0000

    clean up language for failure emails

--- lib/lightning/pipeline/failure_email.ex
+++ lib/lightning/pipeline/failure_email.ex
@@ -2,13 +2,15 @@ defmodule Lightning.FailureEmail do
   @moduledoc false
   use Phoenix.Swoosh, view: Lightning.FailureNotifierView
 
+  import Lightning.Helpers, only: [ms_to_human: 1]
+
   alias Lightning.Mailer
 
-  defp failure_subject(%{name: name}, failure_count) do
+  defp failure_subject(%{name: name}, failure_count, time_scale) do
     if failure_count < 2 do
       "#{name} failed."
     else
-      "#{name} has failed #{failure_count} times in the last 24 hours."
+      "#{name} has failed #{failure_count} times in the last #{ms_to_human(time_scale)}."
     end
   end
 
@@ -16,13 +18,18 @@ defmodule Lightning.FailureEmail do
     email =
       new()
       |> to(email)
-      |> from(
-        {"Lightning", Application.get_env(:lightning, :email_addresses)[:admin]}
-      )
+      |> from({"Lightning", Application.get_env(:lightning, :email_addresses)[:admin]})
       |> subject(
-        failure_subject(%{name: body_data[:workflow_name]}, body_data[:count])
+        failure_subject(
+          %{name: body_data[:workflow_name]},
+          body_data[:count],
+          body_data[:time_scale]
+        )
       )
-      |> render_body("failure_alert.html", body_data)
+      |> render_body(
+        "failure_alert.html",
+        Map.put(body_data, :duration, ms_to_human(body_data[:time_scale]))
+      )
 
     Mailer.deliver(email)
   end
