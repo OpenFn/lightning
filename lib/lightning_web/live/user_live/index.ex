3c25a9aceab3ae7ef88dad0ef49109e98296b6c1
Elias W. BA <eliaswalyba@gmail.com>
2023-05-11 14:09:41 +0000

    Delete now, cancel delete

--- lib/lightning_web/live/user_live/index.ex
+++ lib/lightning_web/live/user_live/index.ex
@@ -42,7 +42,79 @@ defmodule LightningWeb.UserLive.Index do
     |> assign(:user, Accounts.get_user!(id))
   end
 
+  def handle_event(
+        "cancel_deletion",
+        %{"id" => user_id},
+        socket
+      ) do
+    user = Accounts.get_user!(user_id)
+    Accounts.update_user_details(user, %{scheduled_deletion: nil})
+
+    {:noreply,
+     socket
+     |> put_flash(:info, "User deletion canceled")
+     |> push_navigate(to: ~p"/settings/users")}
+  end
+
+  def handle_event(
+        "delete_now",
+        %{"id" => user_id},
+        socket
+      ) do
+    user = Accounts.get_user!(user_id)
+
+    if Accounts.has_activity_in_projects?(user) do
+      {:noreply,
+       socket
+       |> put_flash(
+         :error,
+         "You can't delete users that have activities in projects"
+       )
+       |> push_navigate(to: ~p"/settings/users")}
+    else
+      Accounts.delete_user(user)
+
+      {:noreply,
+       socket
+       |> put_flash(:info, "User deleted")
+       |> push_navigate(to: ~p"/settings/users")}
+    end
+  end
+
   defp list_users do
     Accounts.list_users()
+  end
+
+  def delete_action(assigns) do
+    if assigns.user.scheduled_deletion do
+      ~H"""
+      <span>
+        <%= link("Cancel deletion",
+          to: "#",
+          phx_click: "cancel_deletion",
+          phx_value_id: @user.id
+        ) %>
+      </span>
+      |
+      <span>
+      <%= link("Delete now",
+          to: "#",
+          phx_click: "delete_now",
+          phx_value_id: @user.id,
+          data: [
+            confirm: "Are you sure you want to delete this user immediately? This action is irreversible."
+          ]
+        ) %>
+      </span>
+      """
+    else
+      ~H"""
+      <span>
+        <.link navigate={Routes.user_index_path(@socket, :delete, @user)}>
+          Delete
+        </.link>
+      </span>
+      """
+    end
   end
 end
