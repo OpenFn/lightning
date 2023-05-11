0dd7f22e11076d2c6a378b4d1ac3c9d7e94907da
mtuchi <mtuchidev@gmail.com>
2023-05-11 22:30:43 +0300

    update permission functions

--- lib/lightning_web/live/project_live/index.ex
+++ lib/lightning_web/live/project_live/index.ex
@@ -128,6 +128,9 @@ defmodule LightningWeb.ProjectLive.Index do
   def can_request_project_deletion(current_user, project) do
     is_nil(project.scheduled_deletion) and
       (can_delete_project(current_user, project) or
-         Users |> Permissions.can(:delete, current_user))
+         Users |> Permissions.can?(:access_admin_space, current_user))
   end
+
+  def can_delete_now(current_user, project),
+    do: !is_nil(project.scheduled_deletion) and current_user.role == :superuser
 end
