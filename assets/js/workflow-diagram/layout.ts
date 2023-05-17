9e79b7b175d4dd6c4be22b2542c5eb2ac77c89f3
Joe Clark <jclark@openfn.org>
2023-05-17 18:35:03 +0100

    workflow-diagram: pass over typings

--- assets/js/workflow-diagram/layout.ts
+++ assets/js/workflow-diagram/layout.ts
@@ -1,28 +1,22 @@
-import { getRectOfNodes, Node, Edge } from 'reactflow';
+import { getRectOfNodes, Node, ReactFlowInstance } from 'reactflow';
 import { stratify, tree } from 'd3-hierarchy';
 import { timer } from 'd3-timer';
-import {
-  FIT_DURATION,
-  FIT_PADDING,
-  NODE_HEIGHT,
-  NODE_WIDTH,
-} from './constants';
+
+import { FIT_PADDING, NODE_HEIGHT, NODE_WIDTH } from './constants';
+import { Flow, Positions } from './types';
 
 const layout = tree<Node>()
   // the node size configures the spacing between the nodes ([width, height])
-  .nodeSize([200, 150])
+  .nodeSize([240, 150])
   // this is needed for creating equal space between all nodes
   .separation(() => 1);
 
-type Model = { nodes: Node[]; edges: Edge[] };
-
-const calculateLayout = (
-  model: Model,
-  update,
-  flow,
-  duration: number | false = 500,
-  onComplete
-) => {
+const calculateLayout = async (
+  model: Flow.Model,
+  update: (newModel: Flow.Model) => any,
+  flow: ReactFlowInstance,
+  duration: number | false = 500
+): Promise<Positions> => {
   const { nodes, edges } = model;
   const hierarchy = stratify<Node>()
     .id(d => d.id)
@@ -36,111 +30,97 @@ const calculateLayout = (
     ...d.data,
     position: { x: d.x, y: d.y },
     // Ensure nodes have a width/height so that we can later do a fit to bounds
-    width: d.width || NODE_WIDTH,
-    height: d.height || NODE_HEIGHT,
+    width: NODE_WIDTH,
+    height: NODE_HEIGHT,
   }));
 
   const newModel = { nodes: newNodes, edges };
+
+  const finalPositions = newNodes.reduce((obj, next) => {
+    obj[next.id] = next.position;
+    return obj;
+  }, {} as Positions);
 
   const hasOldPositions = nodes.find(n => n.position);
 
   // If the old model had no positions, this is a first load and we should not animate
   if (hasOldPositions && duration) {
-    animate(model, newModel, update, flow, duration, onComplete);
+    await animate(model, newModel, update, flow, duration);
   } else {
     update(newModel);
-
-    // TODO deal with this
-    if (onComplete) {
-      const positions = newNodes.reduce((obj, next) => {
-        obj[next.id] = next.position;
-        return obj;
-      }, {});
-      onComplete(positions);
-    }
   }
 
-  // TODO deal with this
-  return newNodes.reduce((obj, next) => {
-    obj[next.id] = next.position;
-    return obj;
-  }, {});
+  return finalPositions;
 };
 
 export default calculateLayout;
 
 export const animate = (
-  from,
-  to,
-  setModel,
-  flowInstance,
-  duration = 500,
-  onComplete
+  from: Flow.Model,
+  to: Flow.Model,
+  setModel: (newModel: Flow.Model) => void,
+  flowInstance: ReactFlowInstance,
+  duration = 500
 ) => {
-  const transitions = to.nodes.map(node => {
-    // We usually animate a node from its previous position
-    let animateFrom = from.nodes.find(({ id }) => id === node.id);
-    if (!animateFrom || !animateFrom.position) {
-      // But if this a node node, animate from its parent (source)
-      const edge = from.edges.find(({ target }) => target === node.id);
-      animateFrom = from.nodes.find(({ id }) => id === edge.source);
-    }
-    return {
-      id: node.id,
-      from: animateFrom.position || { x: 0, y: 0 },
-      to: node.position,
-      node,
-    };
-  });
+  return new Promise<void>(resolve => {
+    const transitions = to.nodes.map(node => {
+      // We usually animate a node from its previous position
+      let animateFrom = from.nodes.find(({ id }) => id === node.id);
+      if (!animateFrom || !animateFrom.position) {
+        // But if this a new node, animate from its parent (source)
+        const edge = from.edges.find(({ target }) => target === node.id);
+        animateFrom = from.nodes.find(({ id }) => id === edge!.source);
+      }
+      return {
+        id: node.id,
+        from: animateFrom!.position || { x: 0, y: 0 },
+        to: node.position,
+        node,
+      };
+    });
 
-  let isFirst = true;
+    let isFirst = true;
 
-  // create a timer to animate the nodes to their new positions
-  const t = timer((elapsed: number) => {
-    const s = elapsed / duration;
+    // create a timer to animate the nodes to their new positions
+    const t = timer((elapsed: number) => {
+      const s = elapsed / duration;
 
-    const currNodes = transitions.map(({ node, from, to }) => ({
-      ...node,
-      position: {
-        // simple linear interpolation
-        x: from.x + (to.x - from.x) * s,
-        y: from.y + (to.y - from.y) * s,
-      },
-    }));
-    setModel({ edges: to.edges, nodes: currNodes });
-
-    if (isFirst) {
-      // Synchronise a fit to the final position with the same duration
-      const bounds = getRectOfNodes(to.nodes);
-      flowInstance.fitBounds(bounds, { duration, padding: FIT_PADDING });
-      isFirst = false;
-    }
-
-    // this is the final step of the animation
-    if (elapsed > duration) {
-      // we are moving the nodes to their destination
-      // this needs to happen to avoid glitches
-      const finalNodes = transitions.map(({ node, to }) => ({
+      const currNodes = transitions.map(({ node, from, to }) => ({
         ...node,
         position: {
-          x: to.x,
-          y: to.y,
+          // simple linear interpolation
+          x: from.x + (to.x - from.x) * s,
+          y: from.y + (to.y - from.y) * s,
         },
       }));
+      setModel({ edges: to.edges, nodes: currNodes });
 
-      setModel({ edges: to.edges, nodes: finalNodes });
-
-      // stop the animation
-      t.stop();
-
-      // TODO deal with this
-      if (onComplete) {
-        const positions = to.nodes.reduce((obj, next) => {
-          obj[next.id] = next.position;
-          return obj;
-        }, {});
-        onComplete(positions);
+      if (isFirst) {
+        // Synchronise a fit to the final position with the same duration
+        const bounds = getRectOfNodes(to.nodes);
+        flowInstance.fitBounds(bounds, { duration, padding: FIT_PADDING });
+        isFirst = false;
       }
-    }
+
+      // this is the final step of the animation
+      if (elapsed > duration) {
+        // we are moving the nodes to their destination
+        // this needs to happen to avoid glitches
+        const finalNodes = transitions.map(({ node, to }) => ({
+          ...node,
+          position: {
+            x: to.x,
+            y: to.y,
+          },
+        }));
+
+        setModel({ edges: to.edges, nodes: finalNodes });
+
+        // stop the animation
+        t.stop();
+
+        resolve();
+      }
+    });
   });
 };
