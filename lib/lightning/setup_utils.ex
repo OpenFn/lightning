eec7d95ece1b7970274cf8d3b59e85550c3ecbe7
Stuart Corbishley <corbish@gmail.com>
2023-06-16 14:11:34 +0200

    Revert Timex usage to DateTime in SetupUtils

--- lib/lightning/setup_utils.ex
+++ lib/lightning/setup_utils.ex
@@ -209,14 +209,14 @@ defmodule Lightning.SetupUtils do
       %{
         job_id: job_2.id,
         exit_code: 1,
-        started_at: Timex.now() |> Timex.shift(seconds: 10),
-        finished_at: Timex.now() |> Timex.shift(seconds: 20)
+        started_at: DateTime.utc_now() |> DateTime.add(10, :second),
+        finished_at: DateTime.utc_now() |> DateTime.add(20, :second)
       },
       %{
         job_id: job_3.id,
         exit_code: 0,
-        started_at: Timex.now() |> Timex.shift(seconds: 10),
-        finished_at: Timex.now() |> Timex.shift(seconds: 20)
+        started_at: DateTime.utc_now() |> DateTime.add(10, :second),
+        finished_at: DateTime.utc_now() |> DateTime.add(20, :second)
       }
     ]
 
@@ -326,14 +326,14 @@ defmodule Lightning.SetupUtils do
       %{
         job_id: send_to_openhim.id,
         exit_code: 1,
-        started_at: Timex.now() |> Timex.shift(seconds: 10),
-        finished_at: Timex.now() |> Timex.shift(seconds: 20)
+        started_at: DateTime.utc_now() |> DateTime.add(10, :second),
+        finished_at: DateTime.utc_now() |> DateTime.add(20, :second)
       },
       %{
         job_id: notify_upload_failed.id,
         exit_code: 0,
-        started_at: Timex.now() |> Timex.shift(seconds: 10),
-        finished_at: Timex.now() |> Timex.shift(seconds: 20)
+        started_at: DateTime.utc_now() |> DateTime.add(21, :second),
+        finished_at: DateTime.utc_now() |> DateTime.add(31, :second)
       }
     ]
 
@@ -416,8 +416,8 @@ defmodule Lightning.SetupUtils do
       %{
         job_id: upload_to_google_sheet.id,
         exit_code: 0,
-        started_at: Timex.now() |> Timex.shift(seconds: 10),
-        finished_at: Timex.now() |> Timex.shift(seconds: 20)
+        started_at: DateTime.utc_now() |> DateTime.add(10, :second),
+        finished_at: DateTime.utc_now() |> DateTime.add(20, :second)
       }
     ]
 
