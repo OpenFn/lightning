242a0614a527c22200f5a905c7200e868b50ebc9
Taylor Downs <downs.taylor@gmail.com>
2023-06-16 08:31:50 +0100

    restore alias, stay DRY

--- lib/lightning/demo.ex
+++ lib/lightning/demo.ex
@@ -3,6 +3,8 @@ defmodule Lightning.Demo do
   Demo encapsulates logic for setting up a demonstration site.
   """
 
+  alias Lightning.SetupUtils
+
   @doc """
   Deletes everything in the database including the superuser and creates a set
   of publicly available users for a demo site via a command that can be run on
@@ -13,8 +15,8 @@ defmodule Lightning.Demo do
 
     {:ok, _, _} =
       Ecto.Migrator.with_repo(Lightning.Repo, fn _repo ->
-        Lightning.SetupUtils.tear_down(destroy_super: true)
-        Lightning.SetupUtils.setup_demo(create_super: true)
+        SetupUtils.tear_down(destroy_super: true)
+        SetupUtils.setup_demo(create_super: true)
       end)
   end
 end
