18c161bf9c0db11cfd4c9053ae92065c6f0ae8cd
Elias W. BA <eliaswalyba@gmail.com>
2023-06-16 10:30:17 +0000

    Make sure workflow new live can work without dummy data
    Fixes #884

--- lib/lightning_web/router.ex
+++ lib/lightning_web/router.ex
@@ -123,6 +123,7 @@ defmodule LightningWeb.Router do
         live "/w/:workflow_id", WorkflowLive, :show
         live "/w", WorkflowLive, :index
         live "/w-new/:id", WorkflowNewLive, :new
+        live "/w-new/:id/edit", WorkflowLive, :edit
         live "/w-new/:id/j/:job_id", WorkflowNewLive, :edit_job
         live "/w-new/:id/t/:trigger_id", WorkflowNewLive, :edit_trigger
         live "/w-new/:id/e/:edge_id", WorkflowNewLive, :edit_edge
