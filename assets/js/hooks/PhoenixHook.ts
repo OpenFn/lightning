3fcc0aa2ce97603625d9cc6cbe9030b1e060f7d6
Stuart Corbishley <corbish@gmail.com>
2023-06-23 12:46:20 +0200

    Forgot to add new hooks directory

---
+++ assets/js/hooks/PhoenixHook.ts
@@ -0,0 +1,20 @@
+export interface LiveSocket {
+  execJS(el: HTMLElement, code: string): void;
+  pushHistoryPatch(url: string, type: string, el?: HTMLElement): void;
+}
+
+export type PhoenixHook<T = {}, Dataset = {}, El = HTMLElement> = {
+  mounted(): void;
+  liveSocket: LiveSocket;
+  el: El & {
+    dataset: Dataset;
+  };
+  destroyed(): void;
+  handleEvent<T = {}>(eventName: string, callback: (payload: T) => void): void;
+  pushEventTo<P = {}, R = any>(
+    selectorOrTarget: string | HTMLElement,
+    event: string,
+    payload: P,
+    callback?: (reply: R, ref: any) => void
+  ): void;
+} & T;
