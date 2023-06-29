a312f76bfa3e4900bbe374bd1645da701e9bc470
Stuart Corbishley <corbish@gmail.com>
2023-06-29 09:46:33 +0200

    Update deps

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,5 @@
+[
+  # Remove when next version of postgrex is released.
+  # https://github.com/elixir-ecto/postgrex/issues/651
+  {"deps/postgrex/lib/postgrex/type_module.ex", :improper_list_constr}
+]
