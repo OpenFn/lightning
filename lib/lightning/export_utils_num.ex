127a6d1b05a221a1455e3de2800a9b6d99c436e5
Taylor Downs <downs.taylor@gmail.com>
2023-06-30 23:25:45 +0100

    adjustments to conform to new spec and CLI

--- lib/lightning/export_utils_num.ex
+++
@@ -1,9 +0,0 @@
-defmodule Lightning.ExportUtilsNum do
-  @moduledoc """
-  A convience module for setting up and using ex_cldr when converting numbers to
-  their strings for use in our yaml
-  """
-  use Cldr,
-    locales: [:en],
-    providers: [Cldr.Number]
-end
