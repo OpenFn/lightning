67741dfa1a0a858653a2ce170457749cba037542
Taylor Downs <downs.taylor@gmail.com>
2023-06-30 19:02:37 +0100

    Revert "Update deps"
    This reverts commit a312f76bfa3e4900bbe374bd1645da701e9bc470.
    
    @stuartc, I'm happy to put this back after merging Zacck's stuff but it breaks the dropdowns
    and makes the review hard. there are no commits after this, so I'm guessing nothing depends on it
    yet. We can discuss on Monday.

--- .dialyzer_ignore.exs
+++
@@ -1,5 +0,0 @@
-[
-  # Remove when next version of postgrex is released.
-  # https://github.com/elixir-ecto/postgrex/issues/651
-  {"deps/postgrex/lib/postgrex/type_module.ex", :improper_list_constr}
-]
