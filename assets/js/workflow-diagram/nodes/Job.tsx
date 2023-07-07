412bfbe303b0f03d2e514c17a6d40e780acbf9d8
Joe Clark <jclark@openfn.org>
2023-07-07 15:29:47 +0100

    workflow-diagram: stubbing in icons

--- assets/js/workflow-diagram/nodes/Job.tsx
+++ assets/js/workflow-diagram/nodes/Job.tsx
@@ -3,6 +3,7 @@ import { Handle, Position, NodeProps } from 'reactflow';
 import Node from './Node';
 import PlusButton from './PlusButton';
 import getAdaptorName from '../util/get-adaptor-name';
+import * as icons from '../components/adaptor-icons';
 
 type NodeData = any;
 
@@ -11,22 +12,35 @@ const JobNode = ({
   sourcePosition = Position.Bottom,
   ...props
 }: NodeProps<NodeData>) => {
-  const toolbar = () => props.data?.allowPlaceholder && <PlusButton />
-  console.log(props.data)
+  const toolbar = () => props.data?.allowPlaceholder && <PlusButton />;
 
-  const adaptor = getAdaptorName(props.data?.adaptor)
-  return (<Node
-    {...props}
-    label={props.data?.name}
-    sublabel={adaptor}
-    targetPosition={targetPosition}
-    sourcePosition={sourcePosition}
-    allowSource
-    toolbar={toolbar}
-  />)
- 
+  const adaptor = getAdaptorName(props.data?.adaptor);
+  const icon = getAdaptorIcon(adaptor);
+  return (
+    <Node
+      {...props}
+      label={props.data?.name}
+      icon={icon}
+      sublabel={adaptor}
+      targetPosition={targetPosition}
+      sourcePosition={sourcePosition}
+      allowSource
+      toolbar={toolbar}
+    />
+  );
 };
 
 JobNode.displayName = 'JobNode';
 
 export default memo(JobNode);
+
+function getAdaptorIcon(adaptor) {
+  // TODO how will we do this?
+  if (adaptor === 'dhis2') {
+    return icons.dhis2;
+  }
+  if (adaptor === 'common') {
+    return <img src="/images/openfn.png" />;
+  }
+  return adaptor;
+}
--- assets/js/workflow-diagram/nodes/Node.tsx
+++ assets/js/workflow-diagram/nodes/Node.tsx
@@ -4,18 +4,19 @@ import { NODE_HEIGHT, NODE_WIDTH } from '../constants';
 
 type NodeData = any;
 
-const Circle = ({ width, height, styles, strokeWidth }) =>
+const Circle = ({ width, height, styles, strokeWidth }) => (
   <ellipse
-  // Note we have to offset the x/y position by the stroke width or the stroke will clip outside the canvas
-    cx={strokeWidth + (width / 2)}
-    cy={strokeWidth + (height / 2)}
+    // Note we have to offset the x/y position by the stroke width or the stroke will clip outside the canvas
+    cx={strokeWidth + width / 2}
+    cy={strokeWidth + height / 2}
     rx={width / 2}
     ry={height / 2}
     strokeWidth={strokeWidth}
     {...styles}
   />
+);
 
-const Rect = ({ width, height, styles, strokeWidth }) => 
+const Rect = ({ width, height, styles, strokeWidth }) => (
   <rect
     // Note we have to offset the x/y position by the stroke width or the stroke will clip outside the canvas
     x={strokeWidth}
@@ -25,32 +26,47 @@ const Rect = ({ width, height, styles, strokeWidth }) =>
     height={height}
     strokeWidth={strokeWidth}
     {...styles}
-  />;
+  />
+);
 
 const Shape = ({ shape, width, height, strokeWidth, styles }) => {
   if (shape === 'circle') {
-    return <Circle width={width} height={height} styles={styles} strokeWidth={strokeWidth}/>
+    return (
+      <Circle
+        width={width}
+        height={height}
+        styles={styles}
+        strokeWidth={strokeWidth}
+      />
+    );
   } else {
-    return <Rect width={width} height={height} styles={styles} strokeWidth={strokeWidth} />
+    return (
+      <Rect
+        width={width}
+        height={height}
+        styles={styles}
+        strokeWidth={strokeWidth}
+      />
+    );
   }
-}
+};
 
 const Label = ({ children }) => {
   if (children && children.length) {
-    return (<p className="line-clamp-2 align-left text-m">
-      {children}
-    </p>)
+    return <p className="line-clamp-2 align-left text-m">{children}</p>;
   }
   return null;
-}
+};
 const SubLabel = ({ children }) => {
   if (children && children.length) {
-    return (<p className="line-clamp-2 align-left text-sm text-slate-500">
-      {children}
-    </p>)
+    return (
+      <p className="line-clamp-2 align-left text-sm text-slate-500">
+        {children}
+      </p>
+    );
   }
   return null;
-}
+};
 
 const Node = ({
   labelClass = '',
@@ -66,6 +82,7 @@ const Node = ({
 
   // New stuff we need to support
   icon, // displayed inside the SVG shape
+  // Is this a string? A component?
   status, // [ok | error | warning, message]. Passed into a status widget
 
   // title and subtitle?
@@ -73,9 +90,7 @@ const Node = ({
   // I think I prefer this to be generic
   label, // main label which appears to the right
   sublabel, // A smaller label to the right
-
 }: NodeProps<NodeData>) => {
-
   // Values to control the svg shape
   // TODO are these values constant?
   const width = 100;
@@ -84,34 +99,80 @@ const Node = ({
   const strokeWidth = 2;
   const style = {
     stroke: '#c0c0c0',
-    fill: 'white'
-  }
+    fill: 'white',
+  };
   // TODO I don't really think we're controlling the node size properly
   // what will very long labels do?
   return (
     <div className="flex flex-row">
       <div>
-        {targetPosition && <Handle
-          type="target"
-          isConnectable={isConnectable}
-          position={targetPosition}
-          style={{ visibility: 'hidden', height: 0, top: 0, left: (strokeWidth + anchorx) }}
-        />}
-        <svg style={{ maxWidth: '110px', maxHeight: '110px' }}>
-          <Shape shape={shape} width={width} height={height} strokeWidth={strokeWidth} styles={style}/>
-        </svg>
-        {sourcePosition && <Handle
-          type="source"
-          isConnectable={isConnectable}
-          position={sourcePosition}
-          style={{ visibility: 'hidden', height: 0, top: height, left: (strokeWidth + anchorx) }}/>}
+        {targetPosition && (
+          <Handle
+            type="target"
+            isConnectable={isConnectable}
+            position={targetPosition}
+            style={{
+              visibility: 'hidden',
+              height: 0,
+              top: 0,
+              left: strokeWidth + anchorx,
+            }}
+          />
+        )}
+        <div>
+          <svg style={{ maxWidth: '110px', maxHeight: '110px' }}>
+            <Shape
+              shape={shape}
+              width={width}
+              height={height}
+              strokeWidth={strokeWidth}
+              styles={style}
+            />
+          </svg>
+          {icon && (
+            <div
+              style={{
+                position: 'absolute',
+                // Position is half of the difference of the actual width, offset for stroke
+                left: 0.1 * width + strokeWidth,
+                top: 0.1 * height + strokeWidth,
+                height: `${0.8 * height}px`,
+                width: `${0.8 * width}px`,
+                //  This should default the colour for our placeholder icons
+                color: '#c0c0c0',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              {typeof icon === 'string' ? (
+                <div className="text-black font-bold">{icon}</div>
+              ) : (
+                icon
+              )}
+            </div>
+          )}
+        </div>
+        {sourcePosition && (
+          <Handle
+            type="source"
+            isConnectable={isConnectable}
+            position={sourcePosition}
+            style={{
+              visibility: 'hidden',
+              height: 0,
+              top: height,
+              left: strokeWidth + anchorx,
+            }}
+          />
+        )}
       </div>
       <div className="flex flex-col flex-1 justify-center ml-2">
-          <Label>{label}</Label>
-          <SubLabel>{sublabel}</SubLabel>
+        <Label>{label}</Label>
+        <SubLabel>{sublabel}</SubLabel>
       </div>
     </div>
-  )
+  );
 };
 
 Node.displayName = 'JobNode';
