59700b19bdc7936214c378f1cc861fa5583239da
Zacck <coderv63@gmail.com>
2023-07-25 08:36:45 +0200

    Build migration for project repos

---
+++ priv/repo/migrations/20230725063042_create_repo_project_connection.exs
@@ -0,0 +1,16 @@
+defmodule Lightning.Repo.Migrations.CreateRepoProjectConnection do
+  use Ecto.Migration
+
+  def change do
+    create table(:project_repos, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :github_installation_id, :string
+      add :target_id, :string
+      add :repo, :string
+      add :branch, :string
+      add :project_id, references(:projects, type: :binary_id)
+
+      timestamps()
+    end
+  end
+end
