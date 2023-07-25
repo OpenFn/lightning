fbbc9a40e9a6fbefa0d8a688b50dbf5028a2655f
Joe Clark <jclark@openfn.org>
2023-07-25 11:38:29 +0100

    workflow-digram: refactor selection

--- mix.exs
+++ mix.exs
@@ -85,7 +85,7 @@ defmodule Lightning.MixProject do
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_dashboard, "~> 0.8.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.19.3"},
+      {:phoenix_live_view, "~> 0.19.5"},
       {:phoenix_storybook, "~> 0.5.2"},
       {:plug_cowboy, "~> 2.5"},
       {:postgrex, ">= 0.0.0"},
