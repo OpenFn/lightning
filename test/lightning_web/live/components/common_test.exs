b934a2789e3c6b3f7b38cb33d6c0cf1bf0c709f8
Taylor Downs <downs.taylor@gmail.com>
2023-07-27 11:57:03 +0100

    fix version chip test

--- test/lightning_web/live/components/common_test.exs
+++ test/lightning_web/live/components/common_test.exs
@@ -82,7 +82,7 @@ defmodule LightningWeb.Components.CommonTest do
                "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
 
       assert html =~
-               "Warning: detected image tag that does not match application version"
+               "Detected image tag that does not match application version"
 
       assert html =~ "v#{elem(:application.get_key(:lightning, :vsn), 1)}"
     end
