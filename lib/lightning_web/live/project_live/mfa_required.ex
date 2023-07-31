01d6762d3e0f02fb60d7d245c6cebd9116623188
Frank Midigo <midigofrank@gmail.com>
2023-07-31 18:12:29 +0300

    chore: test for MFA required page

--- lib/lightning_web/live/project_live/mfa_required.ex
+++ lib/lightning_web/live/project_live/mfa_required.ex
@@ -4,9 +4,21 @@ defmodule LightningWeb.ProjectLive.MFARequired do
   """
   use LightningWeb, :live_view
 
+  on_mount __MODULE__
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, active_menu_item: nil)}
+  end
+
+  def on_mount(:default, _params, _session, socket) do
+    %{current_user: current_user} = socket.assigns
+
+    if current_user.mfa_enabled do
+      {:halt, redirect(socket, to: "/")}
+    else
+      {:cont, socket}
+    end
   end
 
   @impl true
