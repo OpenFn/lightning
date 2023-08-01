f920849b3d3e0d28d1bdb6966eaf166d89a77a27
Zacck <coderv63@gmail.com>
2023-08-01 15:19:28 +0200

    Add boundary module for version control

--- lib/lightning/version_control/version_control.ex
+++ lib/lightning/version_control/version_control.ex
@@ -9,14 +9,38 @@ defmodule Lightning.VersionControl do
   import Ecto.Query, warn: false
   alias Lightning.Repo
   alias Lightning.VersionControl.ProjectRepo
+  alias Lightning.VersionControl.GithubClient
 
   @doc """
   Creates a connection between a project and a github repo
   """
-  def create_github_connection(attrs) do 
+  def create_github_connection(attrs) do
     %ProjectRepo{}
     |> ProjectRepo.changeset(attrs)
     |> Repo.insert()
   end
-end
 
+  def add_github_installation_id(user_id, installation_id) do
+    pending_installation =
+      Repo.one(
+        from p in ProjectRepo,
+          where: p.user_id == ^user_id and is_nil(p.github_installation_id)
+      )
+
+    pending_installation
+    |> ProjectRepo.changeset(%{github_installation_id: installation_id})
+    |> Repo.update()
+  end
+
+  def fetch_installation_repos(installation_id) do
+    {:ok, repos} = GithubClient.get_installation_repos(installation_id)
+
+    {:ok, repos}
+  end
+
+  def fetch_repo_branches(installation_id, repo_name) do
+    {:ok, branches} = GithubClient.get_repo_branches(installation_id, repo_name)
+
+    {:ok, branches}
+  end
+end
