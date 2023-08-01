8f877ae9c3ea67d8a090155846ea043463765ecb
Zacck <coderv63@gmail.com>
2023-08-01 15:20:43 +0200

    Some boundary tests for version control

--- test/lightning/version_control_test.exs
+++ test/lightning/version_control_test.exs
@@ -1,24 +1,54 @@
 defmodule Lightning.VersionControlTest do
   use Lightning.DataCase, async: true
   alias Lightning.VersionControl
+  alias Lightning.VersionControl.ProjectRepo
+
   import Lightning.Factories
 
   describe "Version Control" do
-    test "creates a project github repo connection" do
+    test "creates a project github repo connection record when project and user id are present" do
       project = insert(:project)
+      user = insert(:user)
 
       attrs = %{
         project_id: project.id,
-        github_installation_id: "some id",
-        target_id: "some target",
-        repo: "s/r",
-        branch: "main"
+        user_id: user.id
       }
 
       assert {:ok, repo_connection} =
                VersionControl.create_github_connection(attrs)
 
       assert repo_connection.project_id == project.id
+    end
+
+    test "given a user_id and a installation_id it should update the correct record" do
+      project = insert(:project)
+      [u1, u2] = insert_pair(:user)
+
+      attrs1 = %{project_id: project.id, user_id: u1.id}
+
+      attrs2 = %{project_id: project.id, user_id: u2.id}
+
+      assert Repo.aggregate(ProjectRepo, :count) == 0
+
+      Enum.map([attrs1, attrs2], fn a ->
+        VersionControl.create_github_connection(a)
+      end)
+
+      assert Repo.aggregate(ProjectRepo, :count) == 2
+
+      assert {:ok, updated_connection} =
+               VersionControl.add_github_installation_id(
+                 u1.id,
+                 "some_installation"
+               )
+
+      assert updated_connection.user_id == u1.id
+
+      not_updated = Repo.get_by(ProjectRepo, user_id: u2.id)
+
+      refute not_updated.id == updated_connection.id
+      refute not_updated.github_installation_id
     end
   end
 end
