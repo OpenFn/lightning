251fb3a19e5ff3bee023fa810e09ea97452442ff
Zacck <coderv63@gmail.com>
2023-08-08 14:37:49 +0200

    Work with a base64 encode variable

--- priv/repo/migrations/20230808121910_remove_target_id.exs
+++ priv/repo/migrations/20230808121910_remove_target_id.exs
@@ -2,7 +2,7 @@ defmodule Lightning.Repo.Migrations.RemoveTargetId do
   use Ecto.Migration
 
   def change do
-    alter table(:project_repo) do
+    alter table(:project_repos) do
       remove :target_id
     end
   end
