59a6831c90a7872596d94bb1b3bf9abb82ea3b77
Stuart Corbishley <corbish@gmail.com>
2023-08-24 11:21:52 +0200

    Add Attempts.claim function
    - Moved the Attempt model file into attempts folder
    - Added a `:simple_workflow` factory.
    - Remove `:updated_at` from AttemptRun model.

--- lib/lightning/attempts.ex
+++ lib/lightning/attempts.ex
@@ -1,11 +1,13 @@
 defmodule Lightning.Attempts do
   alias Lightning.Repo
 
+  import Ecto.Query
+
   @doc """
   Enqueue an attempt to be processed.
   """
-  def enqueue(attempt) do
-    Lightning.Attempts.Queue.new(attempt)
+  def enqueue(%Lightning.Attempt{} = attempt) do
+    attempt
     |> Repo.insert()
   end
 
@@ -14,8 +16,41 @@ defmodule Lightning.Attempts do
 
   # Returns `nil` if no attempt is available.
   # """
-  # def claim() do
-  # end
+  def claim(demand \\ 1) do
+    subset_query =
+      Lightning.Attempt
+      |> select([:id])
+      |> where([j], j.state == "available")
+      |> limit(^demand)
+      |> order_by(asc: :inserted_at)
+      |> lock("FOR UPDATE SKIP LOCKED")
+
+    # The Postgres planner may choose to generate a plan that executes a nested
+    # loop over the LIMITing subquery, causing more UPDATEs than LIMIT.
+    # That could cause unexpected updates, including rows that we would
+    # assume would be excluded in the base query in some cases.
+    # The solution is to use a CTE as an "optimization fence" that forces
+    # Postgres _not_ to optimize the query.
+    #
+    # The odd "subset" fragment is required to prevent Ecto from applying the
+    # prefix to the name of the CTE, e.g. "public"."subset".
+    query =
+      Lightning.Attempt
+      |> with_cte("subset", as: ^subset_query)
+      |> join(:inner, [a], x in fragment(~s("subset")), on: true)
+      |> where([a, x], a.id == x.id)
+      |> select([a, _], a)
+
+    updates = [
+      set: [state: "claimed", claimed_at: DateTime.utc_now()]
+    ]
+
+    Repo.transaction(fn ->
+      {_count, jobs} = Repo.update_all(query, updates)
+
+      jobs
+    end)
+  end
 
   # @doc """
   # Removes an attempt from the queue.
