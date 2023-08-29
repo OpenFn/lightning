3b2d69aeb885919af4011172cbb938854713cbc1
Elias W. BA <eliaswalyba@gmail.com>
2023-08-29 16:31:18 +0000

    Change tests target elements to new ones

--- test/lightning_web/live/workflow_live/edit_test.exs
+++ test/lightning_web/live/workflow_live/edit_test.exs
@@ -42,15 +42,11 @@ defmodule LightningWeb.WorkflowLive.EditTest do
       {:ok, view, _html} =
         live(conn, ~p"/projects/#{project.id}/w/#{job.workflow_id}?s=#{job.id}")
 
-      assert has_element?(view, "#builder-#{job.id}")
-
-      # open the new credential modal
+      assert has_element?(view, "#job-pane-#{job.id}")
 
       assert view
              |> element("#new-credential-launcher", "New credential")
              |> render_click()
-
-      # assertions
 
       assert has_element?(view, "#credential-type-picker")
       view |> select_credential_type("http")
@@ -67,57 +63,9 @@ defmodule LightningWeb.WorkflowLive.EditTest do
       {:ok, view, _html} =
         live(conn, ~p"/projects/#{project.id}/w/#{job.workflow_id}?s=#{job.id}")
 
-      # open the new credential modal
-
       assert view
              |> element("#new-credential-launcher", "New credential")
              |> render_click()
-
-      # fill the modal and save
-      view |> select_credential_type("raw")
-      view |> click_continue()
-
-      view
-      |> form("#credential-form",
-        credential: %{
-          name: "newly created credential",
-          body: Jason.encode!(%{"a" => 1})
-        }
-      )
-      |> render_submit()
-
-      # assertions
-
-      refute has_element?(view, "#credential-form")
-
-      assert view
-             |> element(
-               ~S{#job-form select#credentialField option[selected=selected]}
-             )
-             |> render() =~ "newly created credential",
-             "Should have the project credential selected"
-    end
-
-    test "create new credential from edit job form and update the job form", %{
-      conn: conn,
-      project: project,
-      job: job
-    } do
-      {:ok, view, _html} =
-        live(conn, ~p"/projects/#{project.id}/w/#{job.workflow_id}?s=#{job.id}")
-
-      # change the job name so we can assert that the form state had been
-      # kept after saving the new credential
-      view
-      |> form("#job-form", job_form: %{name: "last typed name"})
-      |> render_change()
-
-      # open the new credential modal
-      assert view
-             |> element("#new-credential-launcher", "New credential")
-             |> render_click()
-
-      # fill the modal and save
 
       view |> select_credential_type("raw")
       view |> click_continue()
@@ -131,21 +79,14 @@ defmodule LightningWeb.WorkflowLive.EditTest do
       )
       |> render_submit()
 
-      # assertions
-
       refute has_element?(view, "#credential-form")
 
       assert view
              |> element(
-               ~S{#job-form select#credentialField option[selected=selected]}
+               ~S{[name='workflow[jobs][0][project_credential_id]'] option[selected="selected"]}
              )
              |> render() =~ "newly created credential",
              "Should have the project credential selected"
-
-      assert view
-             |> element(~S{#job-form input#job-form_name})
-             |> render() =~ "last typed name",
-             "Should have kept the job form state after saving the new credential"
     end
   end
 
