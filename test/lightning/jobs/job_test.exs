7e8dd926c8198b9bd0412405e0cd0e9177c64606
Elias <eliaswalyba@gmail.com>
2023-09-04 14:04:36 +0000

    Write tests for the constraint error

--- test/lightning/jobs/job_test.exs
+++ test/lightning/jobs/job_test.exs
@@ -1,4 +1,5 @@
 defmodule Lightning.Jobs.JobTest do
+  alias Lightning.WorkflowsFixtures
   use Lightning.DataCase, async: true
 
   alias Lightning.Jobs.Job
@@ -13,6 +14,35 @@ defmodule Lightning.Jobs.JobTest do
   end
 
   describe "changeset/2" do
+    test "raises a constraint error when jobs in the same workflow have the same name" do
+      workflow = WorkflowsFixtures.workflow_fixture()
+
+      job_attrs = %{
+        name: "Test Job",
+        body: ~s[fn(state => state)],
+        workflow_id: workflow.id
+      }
+
+      {:ok, _} =
+        %Job{}
+        |> Job.changeset(job_attrs)
+        |> Repo.insert()
+
+      {:error, changeset} =
+        %Job{}
+        |> Job.changeset(job_attrs)
+        |> Repo.insert()
+
+      refute changeset.valid?
+
+      assert changeset.errors[:name] ==
+               {"has already been taken",
+                [
+                  constraint: :unique,
+                  constraint_name: "jobs_name_workflow_id_index"
+                ]}
+    end
+
     test "name can't be longer than 100 chars" do
       name = random_job_name(101)
       errors = Job.changeset(%Job{}, %{name: name}) |> errors_on()
