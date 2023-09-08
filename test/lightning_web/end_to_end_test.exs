d93f92c7b3ac3e4525273da75bee902f6fa906e3
Elias <eliaswalyba@gmail.com>
2023-09-08 19:45:16 +0000

    Bump language-http version

--- test/lightning_web/end_to_end_test.exs
+++ test/lightning_web/end_to_end_test.exs
@@ -29,7 +29,7 @@ defmodule LightningWeb.EndToEndTest do
           assert unquote(string) =~ ~r/(?=.*compiler)(?=.*0.0.29)/
 
         :adaptor ->
-          assert unquote(string) =~ ~r/(?=.*language-http)(?=.*5.0.2)/
+          assert unquote(string) =~ ~r/(?=.*language-http)(?=.*5.0.3)/
       end
     end
   end
