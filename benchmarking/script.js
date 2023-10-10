c78986c9413c2516ed5839579c14fc09f74a36b1
Taylor Downs <downs.taylor@gmail.com>
2023-10-10 10:50:34 +0100

    use stages, not VUs

--- benchmarking/script.js
+++ benchmarking/script.js
@@ -4,18 +4,18 @@ import { check } from 'k6';
 const webhookURL =
   'http://localhost:4000/i/cae544ab-03dc-4ccc-a09c-fb4edb255d7a';
 
-const requestsNumber = 10;
-
 export const options = {
-  vus: requestsNumber,
   stages: [
-    { duration: '30s', target: 100 },
+    { duration: '30s', target: 10 },
     { duration: '1m30s', target: 50 },
     { duration: '20s', target: 0 },
   ],
   thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+    http_req_failed: ['rate<0.0001'], // http errors should be less than 0.01%
+    http_req_duration: [
+      'p(95)<250', // 95% of requests should be below 250ms
+      'p(99)<400', // 99% of requests should be below 400ms
+    ],
   },
 };
 
@@ -23,16 +23,16 @@ export function setup() {
   let payload_size = 10;
 
   if (__ENV.PAYLOAD_SIZE != null) {
-    payload_size = parseInt(__ENV.PAYLOAD_SIZE, 10) * 1000
+    payload_size = parseInt(__ENV.PAYLOAD_SIZE, 10) * 1000;
   }
 
   return {
     payload: {
       name: 'lorem',
       surname: 'ipsum',
-      data: '0123456789'.repeat(payload_size / 10) // Repeating string is 10 bytes in size
-    }
-  }
+      data: '0123456789'.repeat(payload_size / 10), // Repeating string is 10 bytes in size
+    },
+  };
 }
 
 export default function (data) {
