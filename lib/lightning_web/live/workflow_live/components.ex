e1a68d5222eaf2ad2e414c329afc396f02b3c6ce
Elias W. BA <eliaswalyba@gmail.com>
2023-10-20 08:44:16 +0000

    Table head color

--- lib/lightning_web/live/workflow_live/components.ex
+++ lib/lightning_web/live/workflow_live/components.ex
@@ -625,8 +625,8 @@ defmodule LightningWeb.WorkflowLive.Components do
     <div class="flow-root">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <table class={["min-w-full border-y border-gray-200", @class]}>
-            <thead class="bg-[#F4F4F5]">
+          <table class={["min-w-full border-y border-gray-200 bg-white", @class]}>
+            <thead class="bg-slate-100 border-gray-200 border-y">
               <tr class="sm:px-6 lg:px-8">
                 <th
                   :if={@on_row_select}
