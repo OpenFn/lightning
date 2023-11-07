4b5a0c74223a1391a781bccc0c2861a10201626b
benedictus-yevu <benedictusyevu@yahoo.com>
2023-11-07 12:18:47 +0000

    re-write of migration to use a single migration that moves the boolean from jobs to edges

--- priv/repo/migrations/20231106201133_add_edge_enabled.exs
+++
@@ -1,9 +0,0 @@
-defmodule Lightning.Repo.Migrations.AddEdgeEnabled do
-  use Ecto.Migration
-
-  def change do
-    alter table(:workflow_edges) do
-      add :enabled, :boolean, null: false, default: true
-    end
-  end
-end
--- priv/repo/migrations/20231107055221_remove_enabled_field_from_jobs_table.exs
+++
@@ -1,9 +0,0 @@
-defmodule Lightning.Repo.Migrations.RemoveEnabledFieldFromJobsTable do
-  use Ecto.Migration
-
-  def change do
-    alter table(:jobs) do
-      remove :enabled
-    end
-  end
-end
