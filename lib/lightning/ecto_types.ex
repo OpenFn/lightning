5d9b0f6a9203318bc0a6ff17ad9c7e9c2be1d993
Stuart Corbishley <corbish@gmail.com>
2023-11-08 08:14:07 +0200

    Parse Unix datetimes with different resolutions

--- lib/lightning/ecto_types.ex
+++ lib/lightning/ecto_types.ex
@@ -6,26 +6,50 @@ defmodule Lightning.UnixDateTime do
   second or microsecond precision. This module allows millisecond precision
   integers to be used as DateTime values without losing precision.
 
+  Microsecond timestamps can also be parsed, but currently they are expected
+  to be provided as strings. This is because Javascript can't represent
+  microsecond timestamps as an integer and BigInt can't be represented
+  as a JSON value.
+
   All functions fallback on the default Ecto types conversion functions.
   """
   use Ecto.Type
   def type, do: :utc_datetime_usec
 
   @doc """
-  Cast a millisecond Unix timestamp to a DateTime.
+  Cast a Unix timestamp to a DateTime.
+
+  Accepts integers and strings, and will attempt to parse the string as an
+  integer. If the integer is 13 digits long, it will be parsed as a millisecond
+  timestamp, and as a microsecond timestamp if it is 16 digits long.
   """
-  def cast(u) when is_integer(u) do
-    DateTime.from_unix(u, :millisecond)
+  def cast(u) do
+    cond do
+      is_integer(u) ->
+        DateTime.from_unix(u, :millisecond)
+
+      byte_size(u) == 13 ->
+        String.to_integer(u)
+        |> DateTime.from_unix(:millisecond)
+
+      byte_size(u) == 16 ->
+        String.to_integer(u)
+        |> DateTime.from_unix(:microsecond)
+
+      is_struct(u, DateTime) ->
+        {:ok, u}
+
+      true ->
+        :error
+    end
     |> case do
       {:ok, dt} ->
-        {:ok, dt |> DateTime.add(0, :microsecond)}
+        Ecto.Type.cast(:utc_datetime_usec, dt)
 
       e ->
         e
     end
   end
-
-  def cast(u), do: Ecto.Type.cast(:utc_datetime_usec, u)
 
   def load(u) do
     Ecto.Type.load(:utc_datetime_usec, u)
@@ -53,9 +77,6 @@ defmodule Lightning.LogMessage do
   use Ecto.Type
   def type, do: :string
 
-  @doc """
-  Cast a millisecond Unix timestamp to a DateTime.
-  """
   def cast(d) when is_binary(d), do: Ecto.Type.cast(:string, d)
 
   def cast(d) when is_map(d) or is_list(d) do
