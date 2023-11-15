698e22321c2e73e49497acaeffed80877d88881e
Rogerio Pontual <rppontual@gmail.com>
2023-11-15 17:34:17 +0000

    Remove simple workflow test

--- test/lightning_web/end_to_end_test.exs
+++ test/lightning_web/end_to_end_test.exs
@@ -18,38 +18,6 @@ defmodule LightningWeb.EndToEndTest do
   describe "webhook triggered attempts" do
     setup :register_and_log_in_superuser
 
-    test "complete an attempt on a simple workflow", %{conn: conn} do
-      project = insert(:project)
-
-      %{triggers: [%{id: webhook_trigger_id}]} =
-        insert(:simple_workflow, project: project)
-
-      # Post to webhook
-      conn = post(conn, "/i/#{webhook_trigger_id}", %{"a" => 1})
-
-      assert %{"work_order_id" => wo_id} = json_response(conn, 200)
-
-      assert %{attempts: [%{id: attempt_id}]} =
-               WorkOrders.get(wo_id, include: [:attempts])
-
-      assert %{runs: []} = Attempts.get(attempt_id, include: [:runs])
-
-      assert %{attempts: [%{id: attempt_id}]} =
-               WorkOrders.get(wo_id, include: [:attempts])
-
-      # wait to complete
-      assert Enum.any?(1..100, fn _i ->
-               Process.sleep(50)
-               %{state: state} = Attempts.get(attempt_id)
-               state == :success
-             end)
-
-      assert %{state: :success, attempts: [%{runs: [run]}]} =
-               WorkOrders.get(wo_id, include: [attempts: [:runs]])
-
-      assert run.exit_reason == "success"
-    end
-
     test "complete an attempt on a complex workflow with parallel jobs", %{
       conn: conn
     } do
@@ -103,6 +71,8 @@ defmodule LightningWeb.EndToEndTest do
              |> Enum.map(&select_dataclip_body(&1.output_dataclip_id)["x"])
              |> Enum.frequencies()
              |> Enum.all?(fn {_x, count} -> count == 2 end)
+
+      assert %{state: :success} = WorkOrders.get(wo_id)
     end
 
     test "the whole thing", %{conn: conn} do
@@ -179,6 +149,8 @@ defmodule LightningWeb.EndToEndTest do
                %{state: state} = Attempts.get(attempt_id)
                state == :success
              end)
+
+      assert %{state: :success} = WorkOrders.get(wo_id)
 
       # All runs are associated with the same project and attempt and proper job
       %{runs: runs} = Attempts.get(attempt_id, include: [:runs])
