173e531c1b7574465424711cf7ccde8d7f09b6ec
Rogerio Pontual <rppontual@gmail.com>
2023-11-20 12:30:46 +0000

    Reintroduce handle [nil] log message

--- test/lightning_web/channels/attempt_channel_test.exs
+++ test/lightning_web/channels/attempt_channel_test.exs
@@ -475,7 +475,7 @@ defmodule LightningWeb.AttemptChannelTest do
       assert errors == %{message: ["This field can't be blank."]}
     end
 
-    test "attempt:log nil message can't be blank", %{
+    test "attempt:log message can't be nil", %{
       socket: socket,
       attempt: attempt,
       workflow: workflow
@@ -498,6 +498,36 @@ defmodule LightningWeb.AttemptChannelTest do
           # we expect a 16 character string for microsecond resolution
           "timestamp" => "1699444653874088",
           "message" => nil
+        })
+
+      assert_reply ref, :error, errors
+
+      assert errors == %{message: ["This field can't be blank."]}
+    end
+
+    test "attempt:log message can't be [nil]", %{
+      socket: socket,
+      attempt: attempt,
+      workflow: workflow
+    } do
+      # { id, job_id, input_dataclip_id }
+      run_id = Ecto.UUID.generate()
+      [job] = workflow.jobs
+
+      ref =
+        push(socket, "run:start", %{
+          "run_id" => run_id,
+          "job_id" => job.id,
+          "input_dataclip_id" => attempt.dataclip_id
+        })
+
+      assert_reply ref, :ok, _
+
+      ref =
+        push(socket, "attempt:log", %{
+          # we expect a 16 character string for microsecond resolution
+          "timestamp" => "1699444653874088",
+          "message" => [nil]
         })
 
       assert_reply ref, :error, errors
