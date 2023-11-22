45ef0f2a27a95fffdccaef4d8506f193dcd31784
Rogerio Pontual <rppontual@gmail.com>
2023-11-22 15:27:40 +0000

    Fix Attempt lv show test response

--- test/lightning_web/live/attempt_live/show_test.exs
+++ test/lightning_web/live/attempt_live/show_test.exs
@@ -4,21 +4,20 @@ defmodule LightningWeb.AttemptLive.ShowTest do
   import Phoenix.LiveViewTest
   import Lightning.Factories
 
-  alias Lightning.Attempts
   alias Lightning.WorkOrders
   alias Phoenix.LiveView.AsyncResult
 
   describe "handle_async/3" do
-    setup :register_and_log_in_superuser
+    setup :register_and_log_in_user
     setup :create_project_for_current_user
 
     test "with exit error", %{conn: conn, project: project} do
-      %{triggers: [%{id: webhook_trigger_id}], jobs: [job]} =
+      %{triggers: [%{id: webhook_trigger_id}]} =
         insert(:simple_workflow, project: project)
 
       # Post to webhook
-      post(conn, "/i/#{webhook_trigger_id}", %{"x" => 1})
-      %{"work_order_id" => wo_id} = json_response(conn, 200)
+      conn2 = post(conn, "/i/#{webhook_trigger_id}", %{"x" => 1})
+      %{"work_order_id" => wo_id} = json_response(conn2, 200)
 
       %{attempts: [%{id: attempt_id}]} =
         WorkOrders.get(wo_id, include: [:attempts])
