0448b391a85811d79d673c4986a41aab862e4844
Rogerio Pontual <rppontual@gmail.com>
2023-11-22 14:53:24 +0000

    Remove skip for attempts lv test

--- test/lightning_web/live/attempt_live/show_test.exs
+++ test/lightning_web/live/attempt_live/show_test.exs
@@ -12,33 +12,19 @@ defmodule LightningWeb.AttemptLive.ShowTest do
     setup :register_and_log_in_superuser
     setup :create_project_for_current_user
 
-    @tag :skip
     test "with exit error", %{conn: conn, project: project} do
       %{triggers: [%{id: webhook_trigger_id}], jobs: [job]} =
         insert(:simple_workflow, project: project)
 
       # Post to webhook
-      conn = post(conn, "/i/#{webhook_trigger_id}", %{"x" => 1})
+      post(conn, "/i/#{webhook_trigger_id}", %{"x" => 1})
       %{"work_order_id" => wo_id} = json_response(conn, 200)
 
-      %{attempts: [attempt]} =
+      %{attempts: [%{id: attempt_id}]} =
         WorkOrders.get(wo_id, include: [:attempts])
 
-      # other attempt run
-      insert(:attempt_run,
-        run:
-          build(:run,
-            job: job,
-            input_dataclip: build(:dataclip),
-            output_dataclip: build(:dataclip)
-          ),
-        attempt: attempt
-      )
-
-      %{runs: [run_id]} = Attempts.get(attempt.id, include: [:runs])
-
       {:ok, view, _html} =
-        live(conn, ~p"/projects/#{project.id}/runs/#{run_id}")
+        live(conn, ~p"/projects/#{project.id}/attempts/#{attempt_id}")
 
       %{socket: socket} = :sys.get_state(view.pid)
       initial_async = AsyncResult.loading()
