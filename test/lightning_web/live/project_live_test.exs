7716fd36f52394b7b00eebdad89dcdec31f5f8b7
Taylor Downs <downs.taylor@gmail.com>
2023-11-22 18:45:39 +0000

    better tests for project user sorting

--- test/lightning_web/live/project_live_test.exs
+++ test/lightning_web/live/project_live_test.exs
@@ -919,7 +919,7 @@ defmodule LightningWeb.ProjectLiveTest do
 
     test "project members can edit their own digest frequency and failure alert settings",
          %{conn: conn, user: authenticated_user} do
-      unauthenticated_user = user_fixture()
+      unauthenticated_user = user_fixture(first_name: "Bob")
 
       {:ok, project} =
         Lightning.Projects.create_project(%{
