97217de07eca760e1a90936e0eb81f0491a14b07
benedictus-yevu <benedictusyevu@yahoo.com>
2023-11-23 04:53:31 +0000

    audit trail events for webhook auth method changes

--- lib/lightning/auditing.ex
+++ lib/lightning/auditing.ex
@@ -5,14 +5,54 @@ defmodule Lightning.Auditing do
 
   import Ecto.Query
   alias Lightning.Repo
+  alias Lightning.Credentials.Audit, as: CredentialAudit
+  alias Lightning.Workflows.WebhookAuthMethodAudit, as: WebhookAuthAudit
+  alias Lightning.Accounts.User
 
   def list_all(params \\ %{}) do
-    from(a in Lightning.Credentials.Audit.base_query(),
-      left_join: u in Lightning.Accounts.User,
-      on: [id: a.actor_id],
-      select_merge: %{actor: u},
+    credential_audit_query = create_audit_query(CredentialAudit)
+    webhook_auth_audit_query = create_audit_query(WebhookAuthAudit)
+
+    combined_query =
+      combine_audit_queries(credential_audit_query, webhook_auth_audit_query)
+
+    final_query = create_final_query(combined_query)
+
+    Repo.paginate(final_query, params)
+  end
+
+  defp create_audit_query(audit_module) do
+    from a in audit_module.base_query(),
+      select: %{
+        id: a.id,
+        event: a.event,
+        item_type: a.item_type,
+        item_id: a.item_id,
+        changes: a.changes,
+        actor_id: a.actor_id,
+        inserted_at: a.inserted_at
+      }
+  end
+
+  defp combine_audit_queries(query1, query2) do
+    from a in subquery(query1),
+      union_all: ^subquery(query2)
+  end
+
+  defp create_final_query(combined_query) do
+    from a in subquery(combined_query),
+      left_join: u in User,
+      on: u.id == a.actor_id,
+      select: %{
+        id: a.id,
+        event: a.event,
+        item_type: a.item_type,
+        item_id: a.item_id,
+        changes: a.changes,
+        actor_id: a.actor_id,
+        inserted_at: a.inserted_at,
+        actor: u
+      },
       order_by: [desc: a.inserted_at]
-    )
-    |> Repo.paginate(params)
   end
 end
