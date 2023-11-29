6dfb65d9ec33ab3cb25ff0b4e1ee4b6eafaf2d68
Rogerio Pontual <44991200+jyeshe@users.noreply.github.com>
2023-11-29 17:00:02 +0000

    Worker to 0.2.11, use config port for test, publish attempt update only if persisted (#1433)
    * Use config port for test env
    
    * Bump worker, publish attempt update only if persisted (#1489)
    
    * Bump worker to 0.2.11
    
    * Publish attempt update only if persisted
    
    * Handle attempts update error
    
    * Revert to use assert_receive on e2e

--- config/runtime.exs
+++ config/runtime.exs
@@ -183,15 +183,22 @@ url_scheme = System.get_env("URL_SCHEME", "https")
 
 # The webserver port will always prefer and environment variable _when_
 # given, otherwise it uses the existing config and lastly defaults to 4000.
+
 port =
-  (System.get_env("PORT") ||
-     Application.get_env(:lightning, LightningWeb.Endpoint)
-     |> Keyword.get(:http, port: nil)
-     |> Keyword.get(:port) ||
-     4000)
-  |> case do
-    p when is_binary(p) -> String.to_integer(p)
-    p when is_integer(p) -> p
+  case config_env() do
+    :test ->
+      Application.get_env(:lightning, LightningWeb.Endpoint)[:url][:port]
+
+    _dev_prod ->
+      (System.get_env("PORT") ||
+         Application.get_env(:lightning, LightningWeb.Endpoint)
+         |> Keyword.get(:http, port: nil)
+         |> Keyword.get(:port) ||
+         4000)
+      |> case do
+        p when is_binary(p) -> String.to_integer(p)
+        p when is_integer(p) -> p
+      end
   end
 
 # Use the `PRIMARY_ENCRYPTION_KEY` env variable if available, else fall back
