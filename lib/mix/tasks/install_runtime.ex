96b184d5626fb665ba893942061f2c37bc8afc78
josephjclark <josephjclark@gmail.com>
2023-12-07 20:41:33 +0000

    update cli to 0.4.10 (#1539)

--- lib/mix/tasks/install_runtime.ex
+++ lib/mix/tasks/install_runtime.ex
@@ -43,7 +43,7 @@ defmodule Mix.Tasks.Lightning.InstallRuntime do
 
   def packages() do
     ~W(
-      @openfn/cli@0.0.35
+      @openfn/cli@0.4.10
       @openfn/language-common@latest
     )
   end
