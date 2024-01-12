415dbb50a93b6db531f25b6d0524278d51e5a795
Frank Midigo <midigofrank@gmail.com>
2024-01-12 10:37:10 +0300

    rename AdaptorState to SafeSecrets

--- lib/lightning/safe_secrets.ex
+++ lib/lightning/safe_secrets.ex
@@ -1,17 +1,17 @@
-defmodule Lightning.AdaptorState do
+defmodule Lightning.SafeSecrets do
   @moduledoc false
 
   @type t :: %__MODULE__{configuration: map()}
   @derive Jason.Encoder
   defstruct [:configuration]
 
-  defimpl Inspect, for: Lightning.AdaptorState do
+  defimpl Inspect, for: Lightning.SafeSecrets do
     def inspect(%{configuration: _config}, _opts) do
       Kernel.inspect(%{"configuration" => "[FILTERED]"})
     end
   end
 
   def new(config) do
-    struct(Lightning.AdaptorState, configuration: config)
+    struct(Lightning.SafeSecrets, configuration: config)
   end
 end
