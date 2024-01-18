8baf9645ee069275c86d4593030e29ec62bc442f
Taylor Downs <downs.taylor@gmail.com>
2024-01-18 21:21:49 +0000

    don't ignore postgrex/type_module.ex anymore

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,7 +1,4 @@
 [
-  # Remove when next version of postgrex is released.
-  # https://github.com/elixir-ecto/postgrex/issues/651
-  {"deps/postgrex/lib/postgrex/type_module.ex", :improper_list_constr},
   {"lib/lightning/runtime/handler.ex", :no_return},
   {"lib/lightning/runtime/handler.ex", :call_with_opaque},
   {"lib/lightning/task_worker.ex", :call_with_opaque}
