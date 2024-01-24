73df6284346b1496059826c125658639ef9f791b
Frank Midigo <midigofrank@gmail.com>
2024-01-24 13:18:00 +0300

    remove duplicate migration code

--- priv/repo/migrations/20240124054202_add_wiped_to_dataclips.exs
+++ priv/repo/migrations/20240124054202_add_wiped_to_dataclips.exs
@@ -9,10 +9,5 @@ defmodule Lightning.Repo.Migrations.AddWipedToDataclips do
     alter table("steps") do
       modify :input_dataclip_id, :binary_id, null: true, from: {:binary_id, null: false}
     end
-
-    # TODO: remove this when done
-    alter table("projects") do
-      add :retention_policy, :string
-    end
   end
 end
