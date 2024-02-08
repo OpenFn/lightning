e65290419afd58a9148fa977843da31fcec45265
Taylor Downs <taylor@openfn.org>
2024-02-08 16:22:28 +0000

    bump live_view again, fix #1724 (#1725)
    * bump live_view again, fix ##1724
    
    * update changelog

--- mix.exs
+++ mix.exs
@@ -113,7 +113,7 @@ defmodule Lightning.MixProject do
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_dashboard, "~> 0.8"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.1"},
+      {:phoenix_live_view, "~> 0.20.5"},
       {:phoenix_storybook, "~> 0.5.2"},
       {:plug_cowboy, "~> 2.5"},
       {:postgrex, ">= 0.0.0"},
