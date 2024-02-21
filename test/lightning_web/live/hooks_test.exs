626c86adb3d4657023edf0291d645480598375f1
Rogerio Pontual <rppontual@gmail.com>
2024-02-21 10:12:36 +0000

    Avoid a flaky mock error

--- test/lightning_web/live/hooks_test.exs
+++ test/lightning_web/live/hooks_test.exs
@@ -1,5 +1,5 @@
 defmodule LightningWeb.HooksTest do
-  use LightningWeb.ConnCase, async: true
+  use LightningWeb.ConnCase, async: false
   import Phoenix.LiveViewTest
 
   import Lightning.Factories
