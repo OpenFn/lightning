56162cbcf945f2798477395093736157fa3bb8af
Rogerio Pontual <rppontual@gmail.com>
2024-02-22 10:35:43 +0000

    Flaky on report worker

--- test/lightning/usage_tracking/worker_test.exs
+++ test/lightning/usage_tracking/worker_test.exs
@@ -58,7 +58,7 @@ defmodule Lightning.UsageTracking.WorkerTest do
       report = Report |> Repo.one()
 
       assert %Report{submitted: true} = report
-      assert DateTime.diff(DateTime.utc_now(), report.submitted_at, :second) < 1
+      assert DateTime.diff(DateTime.utc_now(), report.submitted_at, :second) <= 1
       assert_data_populated(report)
     end
 
