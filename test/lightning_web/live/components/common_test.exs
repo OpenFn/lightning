03d4728d0c671cf4383cc13366f2f4364810f5fb
Rory McKinley <rorymckinley@gmail.com>
2024-02-28 10:48:20 +0200

    Use put_temporary_env

--- test/lightning_web/live/components/common_test.exs
+++ test/lightning_web/live/components/common_test.exs
@@ -1,21 +1,16 @@
 defmodule LightningWeb.Components.CommonTest do
   use LightningWeb.ConnCase, async: false
 
+  import Lightning.ApplicationHelpers, only: [put_temporary_env: 3]
   import Phoenix.LiveViewTest
 
   describe "version_chip on docker release" do
     setup do
-      prev = Application.get_env(:lightning, :image_info)
-
-      Application.put_env(:lightning, :image_info,
+      put_temporary_env(:lightning, :image_info,
         image_tag: "v#{Application.spec(:lightning, :vsn)}",
         branch: "main",
         commit: "abcdef7"
       )
-
-      on_exit(fn ->
-        Application.put_env(:lightning, :image_info, prev)
-      end)
     end
 
     test "displays the version and a badge" do
@@ -28,23 +23,16 @@ defmodule LightningWeb.Components.CommonTest do
       # Check for the badge icon
       assert html =~
                "M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-
     end
   end
 
   describe "version_chip on docker edge" do
     setup do
-      prev = Application.get_env(:lightning, :image_info)
-
-      Application.put_env(:lightning, :image_info,
+      put_temporary_env(:lightning, :image_info,
         image_tag: "edge",
         branch: "main",
         commit: "abcdef7"
       )
-
-      on_exit(fn ->
-        Application.put_env(:lightning, :image_info, prev)
-      end)
     end
 
     test "displays the SHA and a cube" do
@@ -62,22 +50,15 @@ defmodule LightningWeb.Components.CommonTest do
 
   describe "version_chip on tag mismatch" do
     setup do
-      prev = Application.get_env(:lightning, :image_info)
-
-      Application.put_env(:lightning, :image_info,
+      put_temporary_env(:lightning, :image_info,
         image_tag: "vX.Y.Z",
         branch: "main",
         commit: "abcdef7"
       )
-
-      on_exit(fn ->
-        Application.put_env(:lightning, :image_info, prev)
-      end)
     end
 
     test "displays the version and a badge" do
       html = render_component(&LightningWeb.Components.Common.version_chip/1)
-
 
       assert html =~
                "Detected image tag that does not match application version"
@@ -92,17 +73,11 @@ defmodule LightningWeb.Components.CommonTest do
 
   describe "version_chip on tag mismatch where image_tag == 'edge'" do
     setup do
-      prev = Application.get_env(:lightning, :image_info)
-
-      Application.put_env(:lightning, :image_info,
+      put_temporary_env(:lightning, :image_info,
         image_tag: "edge",
         branch: "main",
         commit: "abcdef7"
       )
-
-      on_exit(fn ->
-        Application.put_env(:lightning, :image_info, prev)
-      end)
     end
 
     test "displays the SHA and a cube" do
@@ -120,17 +95,11 @@ defmodule LightningWeb.Components.CommonTest do
 
   describe "version_chip all other cases" do
     setup do
-      prev = Application.get_env(:lightning, :image_info)
-
-      Application.put_env(:lightning, :image_info,
+      put_temporary_env(:lightning, :image_info,
         image_tag: nil,
         branch: "main",
         commit: "abcdef7"
       )
-
-      on_exit(fn ->
-        Application.put_env(:lightning, :image_info, prev)
-      end)
     end
 
     test "displays the Lightning version without an icon" do
