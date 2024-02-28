c3e128492940a1d672d1fa0a87ed334563ec5bbf
Rory McKinley <rorymckinley@gmail.com>
2024-02-28 10:15:38 +0200

    Rearrange test assertions
    * Easier to debug if the first failure is not on the image

--- test/lightning_web/live/components/common_test.exs
+++ test/lightning_web/live/components/common_test.exs
@@ -21,13 +21,14 @@ defmodule LightningWeb.Components.CommonTest do
     test "displays the version and a badge" do
       html = render_component(&LightningWeb.Components.Common.version_chip/1)
 
+      assert html =~ "Docker image tag found"
+      assert html =~ "tagged release build"
+      assert html =~ "v#{Application.spec(:lightning, :vsn)}"
+
       # Check for the badge icon
       assert html =~
                "M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
 
-      assert html =~ "Docker image tag found"
-      assert html =~ "tagged release build"
-      assert html =~ "v#{Application.spec(:lightning, :vsn)}"
     end
   end
 
@@ -49,13 +50,13 @@ defmodule LightningWeb.Components.CommonTest do
     test "displays the SHA and a cube" do
       html = render_component(&LightningWeb.Components.Common.version_chip/1)
 
-      # Check for the cube icon
-      assert html =~
-               "M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-
       assert html =~ "Docker image tag found"
       assert html =~ "unreleased build"
       assert html =~ "abcdef7"
+
+      # Check for the cube icon
+      assert html =~
+               "M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
     end
   end
 
@@ -77,14 +78,15 @@ defmodule LightningWeb.Components.CommonTest do
     test "displays the version and a badge" do
       html = render_component(&LightningWeb.Components.Common.version_chip/1)
 
-      # Check for the warning icon
-      assert html =~
-               "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
 
       assert html =~
                "Detected image tag that does not match application version"
 
       assert html =~ "v#{elem(:application.get_key(:lightning, :vsn), 1)}"
+
+      # Check for the warning icon
+      assert html =~
+               "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
     end
   end
 end
