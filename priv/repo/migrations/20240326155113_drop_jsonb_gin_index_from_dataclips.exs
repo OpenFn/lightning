c58f5076613c8ad153a807ce495ec77c1315e6f7
Taylor Downs <downs.taylor@gmail.com>
2024-03-27 14:02:27 +0000

    disable ddl trans, migration lock; add indexes to steps

--- priv/repo/migrations/20240326155113_drop_jsonb_gin_index_from_dataclips.exs
+++ priv/repo/migrations/20240326155113_drop_jsonb_gin_index_from_dataclips.exs
@@ -1,8 +1,14 @@
 defmodule Lightning.Repo.Migrations.DropJsonbGinIndexFromDataclips do
   use Ecto.Migration
 
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
   def up do
     drop index(:dataclips, [:body])
+
+    create index(:steps, [:input_dataclip_id])
+    create index(:steps, [:output_dataclip_id])
   end
 
   def down do
