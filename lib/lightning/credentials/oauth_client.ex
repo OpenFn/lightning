1240a9726ed62dee9f91c5bd1713b64fcbcff656
Elias W. BA <eliaswalyba@gmail.com>
2024-04-02 00:53:55 +0000

    Ecto model: add oauth_client.ex model

---
+++ lib/lightning/credentials/oauth_client.ex
@@ -0,0 +1,44 @@
+defmodule Lightning.Credentials.OauthClient do
+  @moduledoc """
+  The OAuthClient model.
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Lightning.Accounts.User
+  alias Lightning.Credentials.Credential
+  alias Lightning.Projects.ProjectOauthClient
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "oauth_clients" do
+    field :name, :string
+    field :client_id, :string
+    field :client_secret, :string
+    field :base_url, :string
+
+    belongs_to :user, User
+
+    has_many :credentials, Credential
+    has_many :project_oauth_clients, ProjectOauthClient
+    has_many :projects, through: [:project_oauth_clients, :project]
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(oauth_client, attrs) do
+    oauth_client
+    |> cast(attrs, [
+      :name,
+      :client_id,
+      :client_secret,
+      :base_url,
+      :user_id
+    ])
+    |> validate_required([:name, :client_id, :client_secret, :base_url])
+    |> cast_assoc(:project_oauth_clients)
+    |> assoc_constraint(:user)
+  end
+end
