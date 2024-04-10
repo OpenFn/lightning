8ddba042d4cdb4d4685e8454b27180860dfea666
Rory McKinley <rorymckinley@gmail.com>
2024-04-10 18:29:36 +0200

    Messing around with kafka_ex

--- lib/lightning/application.ex
+++ lib/lightning/application.ex
@@ -103,7 +103,15 @@ defmodule Lightning.Application do
       adaptor_service_childspec,
       {Lightning.TaskWorker, name: :cli_task_worker},
       {Lightning.Runtime.RuntimeManager,
-       worker_secret: Lightning.Config.worker_secret()}
+       worker_secret: Lightning.Config.worker_secret()},
+      %{
+        id: KafkaEx.ConsumerGroup,
+        start: {
+          KafkaEx.ConsumerGroup,
+          :start_link,
+          [ExampleGenConsumer, "example_group", ["test"], []]
+        }
+      }
       # Start a worker by calling: Lightning.Worker.start_link(arg)
       # {Lightning.Worker, arg}
     ]
