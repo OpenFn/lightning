8227467885cfedbd15dcdaceff9214ae4a24de18
Stuart Corbishley <corbish@gmail.com>
2024-04-11 07:48:18 +0200

    remove unused fixture

--- test/support/fixtures/invocation_fixtures.ex
+++ test/support/fixtures/invocation_fixtures.ex
@@ -21,34 +21,4 @@ defmodule Lightning.InvocationFixtures do
 
     dataclip
   end
-
-  @doc """
-  Generate a step.
-  """
-  def step_fixture(attrs \\ []) when is_list(attrs) do
-    attrs =
-      attrs
-      |> Keyword.put_new_lazy(:project_id, fn ->
-        Lightning.ProjectsFixtures.project_fixture().id
-      end)
-
-    {:ok, step} =
-      attrs
-      |> Keyword.put_new_lazy(:job_id, fn ->
-        Lightning.JobsFixtures.job_fixture(project_id: attrs[:project_id]).id
-      end)
-      |> Keyword.put_new_lazy(:input_dataclip_id, fn ->
-        dataclip_fixture(project_id: attrs[:project_id]).id
-      end)
-      |> Enum.into(%{
-        exit_reason: nil,
-        finished_at: nil,
-        log: [],
-        event_id: nil,
-        started_at: nil
-      })
-      |> Lightning.Invocation.create_step()
-
-    step
-  end
 end
