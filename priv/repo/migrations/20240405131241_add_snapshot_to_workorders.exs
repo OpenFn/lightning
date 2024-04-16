7afe2d855f707851c9f464f935caba4dfdb80f14
Stuart Corbishley <corbish@gmail.com>
2024-04-16 09:11:16 +0200

    Add snapshot and remove constraint for jobs on steps

--- priv/repo/migrations/20240405131241_add_snapshot_to_workorders.exs
+++ priv/repo/migrations/20240405131241_add_snapshot_to_workorders.exs
@@ -23,5 +23,14 @@ defmodule Lightning.Repo.Migrations.AddSnapshotToWorkorders do
       modify :starting_job_id, :binary_id,
         from: references(:jobs, type: :binary_id, on_delete: :delete_all)
     end
+
+    alter table(:steps) do
+      add :snapshot_id,
+          references(:workflow_snapshots, on_delete: :delete_all, type: :binary_id),
+          null: true
+
+      modify :job_id, :binary_id,
+        from: references(:jobs, type: :binary_id, on_delete: :delete_all)
+    end
   end
 end
