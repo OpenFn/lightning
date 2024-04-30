da60a1127de8ebde5b5a9a40ec0ea40e94b0d73d
Rory McKinley <rorymckinley@gmail.com>
2024-04-30 16:06:45 +0200

    WIP
    * Fix changese terrors caused by adding kafka_configuration
    * Test supervisor + children against KAfka cluster from IEx
    * Extend PipelineWorker to check for existing children
    * Extend PipelineWorker to return error if startig children breaks
    * If we are going to use a cron to run the PipelineWorker, have the
      worker exit if it sees that the PipelineSupervisor already has
      children
    * Consider triggering the Worker as a once-off job and just not test?

--- test/lightning/kafka_triggers/pipeline_supervisor_test.exs
+++ test/lightning/kafka_triggers/pipeline_supervisor_test.exs
@@ -1,21 +1,11 @@
 defmodule Lightning.KafkaTriggers.PipelineSupervisorTest do
-  use Lightning.DataCase, async: false
+  use Lightning.DataCase, async: true
 
   alias Lightning.KafkaTriggers.PipelineSupervisor
-  alias Lightning.Workflows.Trigger
 
   describe ".start_link/1" do
-    test "starts all enabled Kafka triggers" do
-      _trigger_1 =
-        insert(:trigger, type: :kafka, kafka_configuration: %{}, enabled: true)
-      _trigger_2 =
-        insert(:trigger, type: :kafka, kafka_configuration: %{}, enabled: true)
-
-      Trigger |> Repo.all() |> IO.inspect(label: :test)
-
-      assert {:ok, pid} = start_supervised({PipelineSupervisor, test_pid: self()})
-
-      assert Supervisor.count_children(pid).active == 2
+    test "starts the supervisor with an empty collection of children" do
+      assert {:ok, _pid} = start_supervised(PipelineSupervisor)
     end
   end
 end
