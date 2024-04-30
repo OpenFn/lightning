69987e55794c3b29a3ec970f78ab2a7d3be0b7f4
Rory McKinley <rorymckinley@gmail.com>
2024-04-30 16:06:45 +0200

    WIP
    * Fix changese terrors caused by adding kafka_configuration
    * Test supervisor + children against KAfka cluster from IEx
    * Extend PipelineWorker to check for existing children
    * Extend PipelineWorker to return error if startig children breaks
    * If we are going to use a cron to run the PipelineWorker, have the
      worker exit if it sees that the PipelineSupervisor already has
      children
    * Consider triggering the Worker as a once-off job and just not test?

--- lib/lightning/kafka_triggers/pipeline_supervisor.ex
+++ lib/lightning/kafka_triggers/pipeline_supervisor.ex
@@ -1,30 +1,12 @@
 defmodule Lightning.KafkaTriggers.PipelineSupervisor do
-  use Supervisor
+  use DynamicSupervisor
 
-  alias Lightning.KafkaTriggers
-  alias Lightning.Repo
-  alias Lightning.Workflows.Trigger
-  # alias Lightning.KafkaTriggers.Pipeline
-
-  def start_link(opts) do
-    test_pid = Keyword.get(opts, :test_pid)
-
-    {:ok, pid} = Supervisor.start_link(__MODULE__, [], opts)
-
-    Ecto.Adapters.SQL.Sandbox.allow(Repo, test_pid, pid)
-
-    {:ok, pid}
+  def start_link(_opts) do
+    DynamicSupervisor.start_link(__MODULE__, [], name: :kafka_pipeline_supervisor)
   end
 
   @impl true
   def init(_opts) do
-      Trigger |> Repo.all() |> IO.inspect(label: :code)
-    children =
-      KafkaTriggers.find_enabled_triggers()
-      |> IO.inspect()
-      |> Enum.map(& %{id: &1.id})
-      |> IO.inspect()
-      
-    Supervisor.init(children, strategy: :one_for_one)
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 end
