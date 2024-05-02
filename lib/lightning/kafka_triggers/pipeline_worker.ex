9d25aa8ece12f54849fb8a8c216fc9e8ec761fde
Rory McKinley <rorymckinley@gmail.com>
2024-05-02 16:43:06 +0200

    PielineWorker exits early if sup has kids

--- lib/lightning/kafka_triggers/pipeline_worker.ex
+++ lib/lightning/kafka_triggers/pipeline_worker.ex
@@ -4,42 +4,45 @@ defmodule Lightning.KafkaTriggers.PipelineWorker do
   alias Lightning.KafkaTriggers.Pipeline
 
   use Oban.Worker,
-    queue: :background,
-    max_attempts: 1
+    queue: :background
 
   @impl Oban.Worker
   def perform(_args) do
     supervisor = GenServer.whereis(:kafka_pipeline_supervisor)
 
-    KafkaTriggers.find_enabled_triggers
-    |> Enum.map(fn trigger ->
-      %{
-        "group_id" => group_id,
-        "hosts" => hosts_list,
-        "topics" => topics,
-      } = trigger.kafka_configuration
+    %{specs: child_count} = DynamicSupervisor.count_children(supervisor)
 
-      hosts = hosts_list |> Enum.map(fn [host, port] -> {host, port} end)
+    if child_count == 0 do
+      KafkaTriggers.find_enabled_triggers
+      |> Enum.map(fn trigger ->
+        %{
+          "group_id" => group_id,
+          "hosts" => hosts_list,
+          "topics" => topics,
+        } = trigger.kafka_configuration
 
-      %{
-        id: trigger.id,
-        start: {
-          Pipeline,
-          :start_link,
-          [
+        hosts = hosts_list |> Enum.map(fn [host, port] -> {host, port} end)
+
+        %{
+          id: trigger.id,
+          start: {
+            Pipeline,
+            :start_link,
             [
-              group_id: group_id,
-              hosts: hosts,
-              name: trigger.id |> String.to_atom(),
-              topics: topics
+              [
+                group_id: group_id,
+                hosts: hosts,
+                name: trigger.id |> String.to_atom(),
+                topics: topics
+              ]
             ]
-          ]
+          }
         }
-      }
-    end)
-    |> Enum.each(fn child_spec ->
-      DynamicSupervisor.start_child(supervisor, child_spec)
-    end)
+      end)
+      |> Enum.each(fn child_spec ->
+        DynamicSupervisor.start_child(supervisor, child_spec)
+      end)
+    end
 
     :ok
   end
--- test/lightning/kafka_triggers/pipeline_worker_test.exs
+++ test/lightning/kafka_triggers/pipeline_worker_test.exs
@@ -14,11 +14,71 @@ defmodule Lightning.KafkaTriggers.PipelineWorkerTest do
       %{pid: pid}
     end
 
-    test "asks the Pipeline supervisor to start a child for each trigger", %{
+    test "does not start children if supervisor already has children", %{
       pid: pid
     } do
       with_mock DynamicSupervisor,
-        [start_child: fn(_sup_pid, _child_spec) -> {:ok, "fake-pid"} end] do
+        [
+          start_child: fn _sup_pid, _child_spec -> {:ok, "fake-pid"} end,
+          count_children: fn _sup_pid -> %{specs: 1} end
+        ] do
+
+        _trigger_1 =
+          insert(
+            :trigger,
+            type: :kafka,
+            kafka_configuration: configuration(1),
+            enabled: true
+          )
+        _trigger_2 =
+          insert(
+            :trigger,
+            type: :kafka,
+            kafka_configuration: configuration(2),
+            enabled: true
+          )
+
+        perform_job(PipelineWorker, %{})
+
+        assert_called(DynamicSupervisor.count_children(pid))
+        assert_not_called(DynamicSupervisor.start_child(:_, :_))
+      end
+    end
+
+    test "returns :ok if supervisor already has chidren" do
+      with_mock DynamicSupervisor,
+        [
+          start_child: fn _sup_pid, _child_spec -> {:ok, "fake-pid"} end,
+          count_children: fn _sup_pid -> %{specs: 1} end
+        ] do
+
+        _trigger_1 =
+          insert(
+            :trigger,
+            type: :kafka,
+            kafka_configuration: configuration(1),
+            enabled: true
+          )
+        _trigger_2 =
+          insert(
+            :trigger,
+            type: :kafka,
+            kafka_configuration: configuration(2),
+            enabled: true
+          )
+
+        assert perform_job(PipelineWorker, %{}) == :ok
+      end
+    end
+
+    test "asks supervisor to start a child for each trigger", %{
+      pid: pid
+    } do
+      with_mock DynamicSupervisor,
+        [
+          start_child: fn _sup_pid, _child_spec -> {:ok, "fake-pid"} end,
+          count_children: fn _sup_pid -> %{specs: 0} end
+        ] do
 
         trigger_1 =
           insert(
@@ -37,10 +97,10 @@ defmodule Lightning.KafkaTriggers.PipelineWorkerTest do
 
         perform_job(PipelineWorker, %{})
 
-        assert called(
+        assert_called(
           DynamicSupervisor.start_child(pid, child_spec(trigger_1, 1))
         )
-        assert called(
+        assert_called(
           DynamicSupervisor.start_child(pid, child_spec(trigger_2, 2))
         )
       end
@@ -48,7 +108,10 @@ defmodule Lightning.KafkaTriggers.PipelineWorkerTest do
 
     test "returns :ok" do
       with_mock DynamicSupervisor,
-        [start_child: fn(_sup_pid, _child_spec) -> {:ok, "fake-pid"} end] do
+        [
+          start_child: fn _sup_pid, _child_spec -> {:ok, "fake-pid"} end,
+          count_children: fn _sup_pid -> %{specs: 0} end
+        ] do
         assert perform_job(PipelineWorker, %{}) == :ok
       end
     end
