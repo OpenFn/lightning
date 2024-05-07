e16b3b512f2f9a9895e528b330ce84e64589fba3
Rory McKinley <rorymckinley@gmail.com>
2024-05-07 12:42:10 +0200

    Prep for ssl

--- lib/lightning/kafka_triggers/pipeline.ex
+++ lib/lightning/kafka_triggers/pipeline.ex
@@ -39,22 +39,27 @@ defmodule Lightning.KafkaTriggers.Pipeline do
   defp build_producer_opts(opts) do
     hosts = opts |> Keyword.get(:hosts)
     group_id = opts |> Keyword.get(:group_id)
-    sasl = opts |> Keyword.get(:sasl)
     topics = opts |> Keyword.get(:topics)
 
-    base_opts = [
+    [
+      client_config: client_config(opts),
       hosts: hosts,
       group_id: group_id,
       topics: topics,
       offset_reset_policy: :earliest,
     ]
+  end
+
+  defp client_config(opts) do
+    sasl = opts |> Keyword.get(:sasl)
 
     case sasl do
       nil ->
-        base_opts
+        []
       sasl ->
         {mechanism, username, password} = sasl
-        [{:client_config, [{:sasl, {String.to_atom(mechanism), username, password}}, {:ssl, true}]} | base_opts]
+        # [{:client_config, [{:sasl, {String.to_atom(mechanism), username, password}}, {:ssl, true}]} | base_opts]
+        [{:sasl, {String.to_atom(mechanism), username, password}}]
     end
   end
 end
