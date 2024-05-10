7432d4c6bb65c1776e74a3354e2a99c0f0c9cf80
Rory McKinley <rorymckinley@gmail.com>
2024-05-10 07:21:21 +0200

    Update testing methods

--- kafka_testing/populate_kafka_triggers.exs
+++ kafka_testing/populate_kafka_triggers.exs
@@ -72,94 +72,126 @@ PipelineWorker.perform(%Oban.Job{args: %{}})
 # DynamicSupervisor.terminate_child(sup, bar)
 
 defmodule KafkaTestingUtils do
+  alias Lightning.Workflows.Trigger
+  alias Lightning.Repo
+
+  import Ecto.Query
+
   def switch_to_auth_config do
-    alias Lightning.Workflows.Trigger
-    alias Lightning.Repo
-
-    import Ecto.Query
-
     query = from t in Trigger, where: t.type == :kafka
 
     triggers = Repo.all(query)
 
     triggers
-    |> Enum.each(fn %{kafka_configuration: config} = trigger -> 
+    |> Enum.each(fn %{kafka_configuration: config} = trigger ->
       changes = %{
         "hosts" => [["localhost", 9094]],
         "sasl" => ["plain", "user", "bitnami"],
         "ssl" => false
       }
-      new_config = config |> Map.merge(changes) 
-      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config}) 
+      new_config = config |> Map.merge(changes)
+      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config})
       changeset |> Repo.update()
     end)
   end
 
   def switch_to_non_auth_config do
-    alias Lightning.Workflows.Trigger
-    alias Lightning.Repo
-
-    import Ecto.Query
-
     query = from t in Trigger, where: t.type == :kafka
 
     triggers = Repo.all(query)
 
     triggers
-    |> Enum.each(fn %{kafka_configuration: config} = trigger -> 
+    |> Enum.each(fn %{kafka_configuration: config} = trigger ->
       changes = %{
         "hosts" => [["localhost", 9096], ["localhost", 9095], ["localhost", 9094]],
         "sasl" => nil,
         "ssl" => false,
       }
-      new_config = config |> Map.merge(changes) 
-      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config}) 
+      new_config = config |> Map.merge(changes)
+      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config})
       changeset |> Repo.update()
     end)
   end
 
   def switch_to_confluent_config(host_and_port, username, password) do
     [host, port] = String.split(host_and_port, ":")
-    alias Lightning.Workflows.Trigger
-    alias Lightning.Repo
-
-    import Ecto.Query
 
     query = from t in Trigger, where: t.type == :kafka
 
     triggers = Repo.all(query)
 
     triggers
-    |> Enum.each(fn %{kafka_configuration: config} = trigger -> 
+    |> Enum.each(fn %{kafka_configuration: config} = trigger ->
       changes = %{
         "hosts" => [[host, String.to_integer(port)]],
         "sasl" => ["plain", username, password],
         "ssl" => true
       }
-      new_config = config |> Map.merge(changes) 
-      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config}) 
+      new_config = config |> Map.merge(changes)
+      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config})
       changeset |> Repo.update()
     end)
   end
 
   def add_empty_partition_timestamps_to_config do
-    alias Lightning.Workflows.Trigger
-    alias Lightning.Repo
-
-    import Ecto.Query
-
     query = from t in Trigger, where: t.type == :kafka
 
     triggers = Repo.all(query)
 
     triggers
-    |> Enum.each(fn %{kafka_configuration: config} = trigger -> 
+    |> Enum.each(fn %{kafka_configuration: config} = trigger ->
       changes = %{
         "partition_timestamps" => %{}
       }
-      new_config = config |> Map.merge(changes) 
-      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config}) 
+      new_config = config |> Map.merge(changes)
+      changeset = Trigger.changeset(trigger, %{kafka_configuration: new_config})
       changeset |> Repo.update()
     end)
+  end
+
+  def set_initial_offset_reset_policy(policy) do
+    all_kafka_triggers()
+    |> Enum.each(fn trigger ->
+      trigger
+      |> update_kafka_configuration("initial_offset_reset_policy", policy)
+    end)
+  end
+
+  def change_group_ids(group_id_mappings) do
+    group_id_mappings
+    |> Enum.each(fn [old_id, new_id] ->
+      all_kafka_triggers()
+      |> find_trigger_for_group(old_id)
+      |> update_kafka_configuration("group_id", new_id)
+    end)
+  end
+
+  def all_kafka_triggers() do
+    query = from t in Trigger, where: t.type == :kafka
+
+    Repo.all(query)
+  end
+
+  defp find_trigger_for_group(triggers, required_group_id) do
+    triggers
+    |> Enum.find(fn trigger ->
+      %Trigger{
+        kafka_configuration: %{
+          "group_id" => group_id
+        }
+      } = trigger
+
+      group_id == required_group_id
+    end)
+  end
+
+  defp update_kafka_configuration(trigger, key, val) do
+    %{kafka_configuration: old_config} = trigger
+
+    new_config = old_config |> Map.merge(%{key => val})
+
+    trigger
+    |> Trigger.changeset(%{kafka_configuration: new_config})
+    |> Repo.update()
   end
 end
