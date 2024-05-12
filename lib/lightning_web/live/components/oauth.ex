90faa655e5f96e9950450d86bca033bc734526aa
Elias W. BA <eliaswalyba@gmail.com>
2024-05-12 13:22:12 +0000

    Use Heroicons instead of raw SVG

--- lib/lightning_web/live/components/oauth.ex
+++ lib/lightning_web/live/components/oauth.ex
@@ -112,18 +112,7 @@ defmodule LightningWeb.Components.Oauth do
     <div class="rounded-md bg-yellow-50 border border-yellow-200 p-4">
       <div class="flex">
         <div class="flex-shrink-0">
-          <svg
-            class="h-5 w-5 text-yellow-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Heroicons.exclamation_triangle class="h-5 w-5 text-yellow-400" />
         </div>
         <div class="ml-3">
           <h3 class="text-sm font-medium text-yellow-800">Something went wrong.</h3>
@@ -153,18 +142,7 @@ defmodule LightningWeb.Components.Oauth do
     <div class="rounded-md bg-yellow-50 border border-yellow-200 p-4">
       <div class="flex">
         <div class="flex-shrink-0">
-          <svg
-            class="h-5 w-5 text-yellow-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Heroicons.exclamation_triangle class="h-5 w-5 text-yellow-400" />
         </div>
         <div class="ml-3">
           <h3 class="text-sm font-medium text-yellow-800">Something went wrong.</h3>
@@ -194,18 +172,7 @@ defmodule LightningWeb.Components.Oauth do
     <div class="rounded-md bg-yellow-50 border border-yellow-200 p-4">
       <div class="flex">
         <div class="flex-shrink-0">
-          <svg
-            class="h-5 w-5 text-yellow-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Heroicons.exclamation_triangle class="h-5 w-5 text-yellow-400" />
         </div>
         <div class="ml-3">
           <h3 class="text-sm font-medium text-yellow-800">Something went wrong.</h3>
@@ -233,18 +200,7 @@ defmodule LightningWeb.Components.Oauth do
     <div class="rounded-md bg-yellow-50 border border-yellow-200 p-4">
       <div class="flex">
         <div class="flex-shrink-0">
-          <svg
-            class="h-5 w-5 text-yellow-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Heroicons.exclamation_triangle class="h-5 w-5 text-yellow-400" />
         </div>
         <div class="ml-3">
           <h3 class="text-sm font-medium text-yellow-800">Something went wrong.</h3>
@@ -275,18 +231,7 @@ defmodule LightningWeb.Components.Oauth do
     <div class="rounded-md bg-yellow-50 border border-yellow-200 p-4">
       <div class="flex">
         <div class="flex-shrink-0">
-          <svg
-            class="h-5 w-5 text-yellow-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Heroicons.exclamation_triangle class="h-5 w-5 text-yellow-400" />
         </div>
         <div class="ml-3">
           <h3 class="text-sm font-medium text-yellow-800">Something went wrong.</h3>
@@ -315,18 +260,7 @@ defmodule LightningWeb.Components.Oauth do
     <div class="rounded-md bg-red-50 p-4 mb-4">
       <div class="flex">
         <div class="flex-shrink-0">
-          <svg
-            class="h-5 w-5 text-red-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Heroicons.exclamation_triangle class="h-5 w-5 text-red-400" />
         </div>
         <div class="ml-3">
           <h3 class="text-sm font-medium text-red-800">Oauth client not found.</h3>
@@ -349,18 +283,7 @@ defmodule LightningWeb.Components.Oauth do
     >
       <div class="flex">
         <div class="flex-shrink-0">
-          <svg
-            class="h-5 w-5 text-blue-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Heroicons.information_circle class="h-5 w-5 text-blue-400" />
         </div>
         <div class="ml-3 flex-1 md:flex md:justify-between">
           <p class="text-sm text-slate-700">
