b7ef75ac9f644368160a3cd6d93363cd56834a8e
Taylor Downs <downs.taylor@gmail.com>
2024-05-16 16:12:59 +0100

    styling for userinfo block

--- lib/lightning_web/live/credential_live/oauth_component.ex
+++ lib/lightning_web/live/credential_live/oauth_component.ex
@@ -317,38 +317,36 @@ defmodule LightningWeb.CredentialLive.OauthComponent do
 
   def error_block(%{type: :userinfo_failed} = assigns) do
     ~H"""
-    <div class="rounded-md bg-yellow-50 border border-yellow-200 p-4">
+    <div class="rounded-md bg-blue-50 p-4">
       <div class="flex">
         <div class="flex-shrink-0">
           <svg
-            class="h-5 w-5 text-yellow-400"
+            class="h-5 w-5 text-blue-400"
             viewBox="0 0 20 20"
             fill="currentColor"
             aria-hidden="true"
           >
             <path
               fill-rule="evenodd"
-              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
               clip-rule="evenodd"
             />
           </svg>
         </div>
-        <div class="ml-3">
-          <h3 class="text-sm font-medium text-yellow-800">Something went wrong.</h3>
-          <div class="mt-2 text-sm text-yellow-700">
-            <p class="text-sm mt-2">
-              Failed retrieving your information. Please
-              <a
-                href="#"
-                phx-click="try_userinfo_again"
-                phx-target={@myself}
-                class="hover:underline text-primary-900"
-              >
-                try again.
-              </a>
-            </p>
-            <.helpblock provider={@provider} type={@type} />
-          </div>
+        <div class="ml-3 flex-1 md:flex md:justify-between">
+          <p class="text-sm text-blue-700">
+            That seemed to work, but we couldn't fetch your user information. You can save your credential now or try again.
+          </p>
+          <p class="mt-3 text-sm md:ml-6 md:mt-0">
+            <a
+              href="#"
+              class="whitespace-nowrap font-medium text-blue-700 hover:text-blue-600"
+              phx-click="try_userinfo_again"
+              phx-target={@myself}
+            >
+              Try again <span aria-hidden="true"> &rarr;</span>
+            </a>
+          </p>
         </div>
       </div>
     </div>
