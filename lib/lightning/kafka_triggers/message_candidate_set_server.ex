3cbe5d1c4d29e8539b9c9655dd9bc597392aabe8
Rory McKinley <rorymckinley@gmail.com>
2024-05-20 14:17:49 +0200

    MessageCandidateSetServer basic implementation

---
+++ lib/lightning/kafka_triggers/message_candidate_set_server.ex
@@ -0,0 +1,38 @@
+defmodule Lightning.KafkaTriggers.MessageCandidateSetServer do
+  use GenServer
+
+  alias Lightning.KafkaTriggers
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def next_candidate_set do
+    GenServer.call(__MODULE__, :next_candidate_set)
+  end
+
+  @impl true
+  def init(_opts) do
+    {:ok, []}
+  end
+
+  @impl true
+  def handle_call(:next_candidate_set, _from, current_sets) do
+    {candidate_set, remaining_sets} = pop_candidate(current_sets)
+
+    {:reply, candidate_set, remaining_sets}
+  end
+
+  defp pop_candidate([]) do
+    case KafkaTriggers.find_message_candidate_sets() do
+      [candidate_set | remaining_sets] ->
+        {candidate_set, remaining_sets}
+      [] ->
+        {nil, []} 
+    end
+  end
+
+  defp pop_candidate([candidate_set | remaining_sets]) do
+    {candidate_set, remaining_sets}
+  end
+end
--- lib/lightning/kafka_triggers/trigger_kafka_message.ex
+++ lib/lightning/kafka_triggers/trigger_kafka_message.ex
@@ -3,15 +3,19 @@ defmodule Lightning.KafkaTriggers.TriggerKafkaMessage do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Lightning.Workflows.Trigger
+
   @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "trigger_kafka_messages" do
-    field :trigger_id,  :binary_id
     field :work_order_id, :binary_id
     field :topic, :string
     field :key, :string
     field :message_timestamp, :integer
     field :metadata, :map
     field :data, :binary
+
+    belongs_to :trigger, Trigger
 
     timestamps()
   end
@@ -38,5 +42,6 @@ defmodule Lightning.KafkaTriggers.TriggerKafkaMessage do
     message
     |> cast(changes, cast_changes)
     |> validate_required(required_changes)
+    |> assoc_constraint(:trigger) # No test for this - test it
   end
 end
