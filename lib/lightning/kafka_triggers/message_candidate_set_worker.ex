c364f0aff6ee43704e735183f5346fa34fef9697
Rory McKinley <rorymckinley@gmail.com>
2024-05-21 11:19:40 +0200

    WIP

--- lib/lightning/kafka_triggers/message_candidate_set_worker.ex
+++ lib/lightning/kafka_triggers/message_candidate_set_worker.ex
@@ -1,12 +1,16 @@
 defmodule Lightning.KafkaTriggers.MessageCandidateSetWorker do
   use GenServer
 
+  alias Lightning.KafkaTriggers
+
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, [])
   end
 
   @impl true
   def init(_opts) do
-    {:ok, ["foo", "bar"]}
+    KafkaTriggers.send_after(self(), :request_candidate_set, 100)
+
+    {:ok, []}
   end
 end
