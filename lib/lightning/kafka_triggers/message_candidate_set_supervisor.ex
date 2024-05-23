662d24bc4f4c6487a11739455188376170cba9f6
Rory McKinley <rorymckinley@gmail.com>
2024-05-23 14:48:24 +0200

    Handle null keys

---
+++ lib/lightning/kafka_triggers/message_candidate_set_supervisor.ex
@@ -0,0 +1,20 @@
+defmodule Lightning.KafkaTriggers.MessageCandidateSetSupervisor do
+  use Supervisor
+
+  alias Lightning.KafkaTriggers.MessageCandidateSetServer
+  alias Lightning.KafkaTriggers.MessageCandidateSetWorker
+
+  def start_link(_opts) do
+    Supervisor.start_link(__MODULE__, [])
+  end
+
+  @impl true
+  def init(_opts) do
+    children = [
+      MessageCandidateSetServer,
+      MessageCandidateSetWorker,
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
