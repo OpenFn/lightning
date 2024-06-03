ad8a5928fe97230fe1f040362ea7a131413c07fd
Rory McKinley <rorymckinley@gmail.com>
2024-06-03 15:56:12 +0200

    Cheap and nasty trigger enabling/disabling

--- lib/lightning_web/live/workflow_live/edit.ex
+++ lib/lightning_web/live/workflow_live/edit.ex
@@ -1220,7 +1220,6 @@ defmodule LightningWeb.WorkflowLive.Edit do
              to: ~p"/projects/#{project.id}/w/#{workflow.id}?#{query_params}",
              replace: true
            )}
-           |> IO.inspect(label: :boom)
 
         {:error, %{text: message}} ->
           {:noreply, put_flash(socket, :error, message)}
