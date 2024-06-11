2402d414b175fab797b40d3f028563050cd7087a
Rory McKinley <rorymckinley@gmail.com>
2024-06-11 12:44:34 +0200

    Add Kafka icon (thanks, Frank!)

--- assets/js/workflow-diagram/components/trigger-icons.tsx
+++ assets/js/workflow-diagram/components/trigger-icons.tsx
@@ -32,3 +32,29 @@ export const lockClosedIcon = (
   </svg>
 
 );
+
+export const kafkaIcon = (
+  <svg viewBox="1 1 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="m15.54 12.97c-.68 0-1.3.25-1.78.67l-1.29-.75c.09-.29.13-.6.13-.92s-.04-.63-.1-.92l1.23-.73c.48.44 1.12.68 1.81.68 1.49 0 2.7-1.19 2.7-2.68s-1.21-2.69-2.7-2.69-2.7 1.21-2.7 2.7c0 .17.02.37.05.55l-1.25.72c-.43-.45-1-.78-1.64-.95v-1.39c1.09-.35 1.88-1.37 1.88-2.57 0-1.49-1.21-2.69-2.7-2.69s-2.68 1.2-2.68 2.69c0 1.2.76 2.21 1.84 2.57v1.4c-1.48.38-2.58 1.71-2.58 3.31s1.1 2.94 2.58 3.31v1.45c-1.08.36-1.84 1.37-1.84 2.57 0 1.49 1.19 2.7 2.68 2.7s2.7-1.21 2.7-2.7c0-1.2-.79-2.22-1.88-2.57v-1.44c.64-.16 1.2-.49 1.64-.94l1.26.73c-.04.19-.06.38-.06.58 0 1.49 1.21 2.7 2.7 2.7s2.7-1.21 2.7-2.7-1.21-2.69-2.7-2.69m0-5.97c.74 0 1.33.59 1.33 1.32s-.59 1.34-1.33 1.34-1.33-.6-1.33-1.34.59-1.32 1.33-1.32m-7.69-2.31c0-.74.59-1.34 1.33-1.34s1.32.6 1.32 1.34-.58 1.34-1.32 1.34-1.33-.6-1.33-1.34m2.65 14.61c0 .74-.58 1.34-1.32 1.34s-1.33-.6-1.33-1.34.59-1.34 1.33-1.34 1.32.6 1.32 1.34m-1.32-5.41c-1.06 0-1.92-.86-1.92-1.92s.86-1.92 1.92-1.92 1.92.86 1.92 1.92-.86 1.92-1.92 1.92m6.36 3.11c-.74 0-1.33-.6-1.33-1.34s.59-1.33 1.33-1.33 1.33.6 1.33 1.33-.59 1.34-1.33 1.34z" fill="#71757E" />
+    <defs>
+      <filter id="filter0_dd_365_7433" x="0" y="0" width="2000" height="2000" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feMorphology radius="1" operator="erode" in="SourceAlpha" result="effect1_dropShadow_365_7433" />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="3" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_365_7433" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feMorphology radius="1" operator="erode" in="SourceAlpha" result="effect2_dropShadow_365_7433" />
+        <feOffset dy="2" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.06 0" />
+        <feBlend mode="normal" in2="effect1_dropShadow_365_7433" result="effect2_dropShadow_365_7433" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_365_7433" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+);
