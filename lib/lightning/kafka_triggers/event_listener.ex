3778c77a8eded41a23340b7a670e35b4614f03be
Rory McKinley <rorymckinley@gmail.com>
2024-06-25 15:48:10 +0200

    Go home in the red

--- lib/lightning/kafka_triggers/event_listener.ex
+++ lib/lightning/kafka_triggers/event_listener.ex
@@ -1,6 +1,7 @@
 defmodule Lightning.KafkaTriggers.EventListener do
   use GenServer
 
+  alias Lightning.KafkaTriggers
   alias Lightning.Workflows.Triggers.Events
 
   def start_link(_opts) do
@@ -12,5 +13,15 @@ defmodule Lightning.KafkaTriggers.EventListener do
     Events.subscribe_to_kafka_trigger_updated()
 
     {:ok, %{}}
+  end
+
+  @impl true
+  def handle_info(%Events.KafkaTriggerUpdated{trigger: trigger}, state) do
+    supervisor = GenServer.whereis(:kafka_pipeline_supervisor)
+
+    supervisor |> KafkaTriggers.update_pipeline(trigger)
+    # PipelineSupervisor.update_trigger(trigger)
+
+    {:noreply, state}
   end
 end
