6e25aa3db6d03e39f5e94a34f79e40eaad86df35
josephjclark <josephjclark@gmail.com>
2024-07-02 13:10:35 +0100

    Dynamic zoom (#2230)
    * workflow-diagram: work out whether we need to fit before laying out for a trigger
    
    * workflow-diagram: tidy implementation
    
    * workflow-diagram: scale visible rect to allow a bleed
    
    * workflow-diagram: remove nasty autofit toolbar
    
    * workflow-diagram: use real dom size and also fit when a placeholder is removed
    
    * workflow-diagram: improve autofit behaviour
    
    When autofitting, we have to use the OLD as well as NEW positions to decide what to fit
    
    * workflow-diagram: fix the maths and tidy up
    
    * workflow-diagram: fit to visible on resize
    
    * changelog
    
    * workflow-diagram: remove console logs
    
    * workflow-diagram: remove duplicate prop
    
    * workflow-diagram: tidy comments
    
    * remove one more comment

--- assets/js/workflow-diagram/layout.ts
+++ assets/js/workflow-diagram/layout.ts
@@ -4,17 +4,26 @@ import { getRectOfNodes, ReactFlowInstance } from 'reactflow';
 
 import { FIT_PADDING } from './constants';
 import { Flow, Positions } from './types';
+import { getVisibleRect, isPointInRect } from './util/viewport';
 
-export type LayoutOpts = { duration: number | false; autofit: boolean };
+export type LayoutOpts = {
+  duration?: number | false;
+  autofit?: boolean | Flow.Node[];
+};
 
 const calculateLayout = async (
   model: Flow.Model,
   update: (newModel: Flow.Model) => any,
   flow: ReactFlowInstance,
-  options: LayoutOuts = {}
+  viewBounds: { width: number; height: number },
+  options: Omit<LayoutOpts, 'autofit'> = {}
 ): Promise<Positions> => {
   const { nodes, edges } = model;
   const { duration } = options;
+
+  // Before we layout, work out whether there are any new unpositioned placeholders
+  // @ts-ignore _default is a temporary flag added by us
+  const newPlaceholders = model.nodes.filter(n => n.position?._default);
 
   const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
   g.setGraph({
@@ -47,9 +56,78 @@ const calculateLayout = async (
 
   const hasOldPositions = nodes.find(n => n.position);
 
+  let autofit: LayoutOpts['autofit'] = false;
+  let doFit: boolean = false;
+  const fitTargets: Flow.Node[] = [];
+
+  if (hasOldPositions) {
+    const oldPositions = nodes.reduce((obj, next) => {
+      obj[next.id] = next.position;
+      return obj;
+    }, {} as Positions);
+
+    // When updating the layout, we should try and fit to the currently visible nodes
+    // This usually just occurs when adding or removing placeholder nodes
+
+    // First work out the size of the current viewpoint in canvas coordinates
+    if (newPlaceholders.length) {
+      const rect = getVisibleRect(flow.getViewport(), viewBounds, 0.9);
+      // Now work out the visible nodes, paying special attention to the placeholder
+      //
+      for (const id in finalPositions) {
+        // Check the node's old position to see if it was visible before the layout
+        // if it's a new node, take the new position
+        const pos = oldPositions[id] || finalPositions;
+        const isInside = isPointInRect(pos, rect);
+        const node = newModel.nodes.find(n => n.id === id)!;
+
+        if (isInside) {
+          // if the node was previously visible, add it to the fit list
+          fitTargets.push(node);
+          // but also, if the NEW position is NOT visible, we need to force a layout
+          if (!doFit && !isPointInRect(finalPositions[id], rect)) {
+            doFit = true;
+          }
+        } else if (node?.type === 'placeholder') {
+          // If the placeholder is NOT visible within the bounds,
+          // include it in the set of visible nodes and force a fit
+          doFit = true;
+          fitTargets.push({
+            ...node,
+            // cheat on the size so we get a better fit
+            height: 100,
+            width: 100,
+          });
+        }
+      }
+    } else {
+      // otherwise, if running a layout, fit to the currently visible nodes
+      // this usually means we've removed a placeholder and lets us tidy up
+      doFit = true;
+      const rect = getVisibleRect(flow.getViewport(), viewBounds, 1.1);
+      for (const id in finalPositions) {
+        // again, use the OLD position to work out visibility
+        const pos = oldPositions[id] || finalPositions;
+        const isInside = isPointInRect(pos, rect);
+        if (isInside) {
+          const node = newModel.nodes.find(n => n.id === id)!;
+          fitTargets.push(node);
+        }
+      }
+    }
+
+    // Useful debugging
+    //console.log(fitTargets.map(n => n.data?.name ?? n.type));
+  }
+
+  // If we need to run a fit, save the set of visible nodes as the fit target
+  if (doFit) {
+    autofit = fitTargets;
+  }
+
   // If the old model had no positions, this is a first load and we should not animate
   if (hasOldPositions && duration) {
-    await animate(model, newModel, update, flow, options);
+    await animate(model, newModel, update, flow, { duration, autofit });
   } else {
     update(newModel);
   }
@@ -102,9 +180,16 @@ export const animate = (
 
       if (isFirst) {
         // Synchronise a fit to the final position with the same duration
-        const bounds = getRectOfNodes(to.nodes);
+        let fitTarget = to.nodes;
+        if (typeof autofit !== 'boolean') {
+          fitTarget = autofit;
+        }
+        const bounds = getRectOfNodes(fitTarget);
         if (autofit) {
-          flowInstance.fitBounds(bounds, { duration, padding: FIT_PADDING });
+          flowInstance.fitBounds(bounds, {
+            duration: typeof duration === 'number' ? duration : 0,
+            padding: FIT_PADDING,
+          });
         }
         isFirst = false;
       }
