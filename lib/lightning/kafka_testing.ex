9f47ce9d83cd1681dd4f4a8e1095833cbb14228e
Rory McKinley <rorymckinley@gmail.com>
2024-07-08 17:34:08 +0200

    Remove Test files

--- lib/lightning/kafka_testing.ex
+++
@@ -1,6 +0,0 @@
-defmodule Lightning.KafkaTesting.Utils do
-  def which_children() do
-    supervisor = GenServer.whereis(:kafka_pipeline_supervisor)
-    Supervisor.which_children(supervisor)
-  end
-end
