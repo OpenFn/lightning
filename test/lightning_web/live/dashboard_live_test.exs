301433d0aa40a7b079b4e93717500b0479690555
Elias W. BA <eliaswalyba@gmail.com>
2024-07-21 23:05:40 +0000

    Test user's projects are listed

--- test/lightning_web/live/dashboard_live_test.exs
+++ test/lightning_web/live/dashboard_live_test.exs
@@ -2,9 +2,7 @@ defmodule LightningWeb.DashboardLiveTest do
   use LightningWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
-
-  setup :register_and_log_in_user
-  setup :create_project_for_current_user
+  import Lightning.Factories
 
   describe "Index" do
     setup :register_and_log_in_user
@@ -25,6 +23,24 @@ defmodule LightningWeb.DashboardLiveTest do
     } do
       {:ok, index_live, _html} = live(conn, ~p"/")
 
+      assert index_live
+             |> has_element?("nav#side-menu a[href='/projects']", "Projects")
+
+      assert index_live
+             |> has_element?("nav#side-menu a[href='/profile']", "User Profile")
+
+      assert index_live
+             |> has_element?(
+               "nav#side-menu a[href='/credentials']",
+               "Credentials"
+             )
+
+      assert index_live
+             |> has_element?(
+               "nav#side-menu a[href='/profile/tokens']",
+               "API Tokens"
+             )
+
       assert {:ok, profile_live, _html} =
                index_live
                |> element("nav#side-menu a", "User Profile")
@@ -35,6 +51,82 @@ defmodule LightningWeb.DashboardLiveTest do
              |> element("nav#side-menu a", "Credentials")
              |> render_click()
              |> follow_redirect(conn, ~p"/credentials")
+    end
+
+    test "User's projects are listed", %{conn: conn, user: user} do
+      project_1 =
+        insert(:project, project_users: [%{user: user, role: :owner}])
+
+      project_2 =
+        insert(:project,
+          project_users: [
+            %{user: user, role: :admin},
+            %{user: build(:user), role: :owner}
+          ]
+        )
+
+      project_3 =
+        insert(:project,
+          project_users: [%{user: build(:user), role: :owner}]
+        )
+
+      insert_list(2, :simple_workflow, project: project_1)
+
+      {:ok, view, _html} = live(conn, ~p"/")
+
+      refute has_element?(view, "#projects-table-row-#{project_3.id}")
+
+      [project_1, project_2]
+      |> Enum.each(fn project ->
+        assert view |> has_element?("tr#projects-table-row-#{project.id}")
+
+        assert view
+               |> has_element?(
+                 "tr#projects-table-row-#{project.id} > td:nth-child(1) > a[href='/projects/#{project.id}/w']",
+                 project.name
+               )
+
+        assert view
+               |> has_element?(
+                 "tr#projects-table-row-#{project.id} > td:nth-child(2)",
+                 project
+                 |> Repo.preload(:project_users)
+                 |> Map.get(:project_users)
+                 |> Enum.find(fn pu -> pu.user_id == user.id end)
+                 |> Map.get(:role)
+                 |> Atom.to_string()
+                 |> String.capitalize()
+               )
+
+        assert view
+               |> has_element?(
+                 "tr#projects-table-row-#{project.id} > td:nth-child(3)",
+                 project
+                 |> Repo.preload(:workflows)
+                 |> Map.get(:workflows)
+                 |> Enum.count()
+                 |> to_string()
+               )
+
+        assert view
+               |> has_element?(
+                 "tr#projects-table-row-#{project.id} > td:nth-child(4) > a[href='/projects/#{project.id}/settings#collaboration']",
+                 project
+                 |> Repo.preload(:project_users)
+                 |> Map.get(:project_users)
+                 |> Enum.count()
+                 |> to_string()
+               )
+
+        assert view
+               |> has_element?(
+                 "tr#projects-table-row-#{project.id} > td:nth-child(5)",
+                 Lightning.Helpers.format_date(
+                   project.updated_at,
+                   "%d/%b/%Y %H:%M:%S"
+                 )
+               )
+      end)
     end
   end
 end
