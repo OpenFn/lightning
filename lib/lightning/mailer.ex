94607a68b3245f1a3d9ca14dda12683d1d89a711
Stuart Corbishley <corbish@gmail.com>
2024-08-06 16:58:50 +0200

    Add defimpl for Lightning mailer to generate recipients

--- lib/lightning/mailer.ex
+++ lib/lightning/mailer.ex
@@ -1,4 +1,10 @@
 defmodule Lightning.Mailer do
   @moduledoc false
   use Swoosh.Mailer, otp_app: :lightning
+
+  defimpl Swoosh.Email.Recipient, for: Lightning.Accounts.User do
+    def format(%Lightning.Accounts.User{} = user) do
+      {user.email, "#{user.first_name} #{user.last_name}"}
+    end
+  end
 end
