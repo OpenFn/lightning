2a675d36ec1d048bdc203ce21fe2ac1c66f2c1fe
Elias W. BA <eliaswalyba@gmail.com>
2024-08-08 12:02:35 +0000

    Change email

--- lib/lightning_web/live/dashboard_live/account_confirmation_modal.ex
+++ lib/lightning_web/live/dashboard_live/account_confirmation_modal.ex
@@ -52,9 +52,7 @@ defmodule LightningWeb.AccountConfirmationModal do
 
   @impl true
   def handle_event("resend-confirmation-email", _, socket) do
-    Lightning.Accounts.deliver_user_confirmation_instructions(
-      socket.assigns.current_user
-    )
+    Lightning.Accounts.remind_account_confirmation(socket.assigns.current_user)
 
     {:noreply, assign(socket, :email_sent, true)}
   end
@@ -80,7 +78,7 @@ defmodule LightningWeb.AccountConfirmationModal do
             Confirmation email sent successfully
           </div>
           <div :if={not @email_sent}>
-            For security purposes, we have blocked access to your accounts, projects and workflows until you confirm your account. Please click resend confirmation email to receive instructions on how to confirm your OpenFn account or update your email address if you have not received a confirmation email.
+            For security purposes, we have blocked access to your projects and workflows until you confirm your account. Please click resend confirmation email to receive instructions on how to confirm your OpenFn account or update your email address if you have not received a confirmation email.
           </div>
         </div>
 
@@ -91,6 +89,7 @@ defmodule LightningWeb.AccountConfirmationModal do
               type="button"
               phx-click="resend-confirmation-email"
               phx-target={@myself}
+              disabled={@email_sent}
               class="ml-3 inline-flex justify-center rounded-md disabled:bg-primary-300 bg-primary-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 sm:w-auto"
             >
               Resend confirmation email
