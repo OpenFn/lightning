76453c5a94d2e88f731f3ccd98ca77377c55b3cf
Joe Clark <jclark@openfn.org>
2024-08-12 16:28:33 +0100

    hookup puppeteer

--- assets/test/snapshots.tsx
+++ assets/test/snapshots.tsx
@@ -1,158 +1,73 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { JSDOM } from 'jsdom';
+import puppeteer from 'puppeteer';
 
-import mockReactFlow from './mock-react-flow';
+import path from 'node:path';
+import esbuild from 'esbuild';
+import { copy } from 'esbuild-plugin-copy';
+import postcss from 'esbuild-postcss';
 
-import { toPng, toSvg } from 'html-to-image';
+const PORT = 3322;
 
-import WorkflowDiagram from '../js/workflow-diagram/WorkflowDiagram';
-import { createWorkflowStore } from '../js/workflow-editor/store';
+async function server() {
+  // copied from dev-server.mjs
+  const ctx = await esbuild.context({
+    entryPoints: ['dev-server/src/index.tsx'],
+    outdir: 'dev-server/dist/',
+    bundle: true,
+    splitting: true,
+    sourcemap: false,
+    format: 'esm',
+    target: ['es2020'],
+    plugins: [
+      postcss(),
+      copy({
+        assets: {
+          from: ['./dev-server/public/*'],
+          to: ['.'],
+        },
+      }),
+    ],
+  });
 
-import { generateImage } from 'jsdom-screenshot';
+  ctx.serve({
+    servedir: 'dev-server/dist',
+    port: PORT,
+  });
+}
 
-import fs from 'node:fs/promises';
+const wait = async () =>
+  new Promise(resolve => {
+    setTimeout(resolve, 500);
+  });
 
-const { window } = new JSDOM(
-  `<html><body><div id="root">x</div></body></html>`
-);
+async function test() {
+  console.log(' >> test');
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    executablePath: '/snap/bin/chromium', // needed for some reason on my local machine??
+  });
+  console.log(' >> browser ready', browser.isConnected());
+  console.log(await browser.userAgent());
+  console.log(await browser.version());
+  // console.log(await browser.pages());
+  const page = await browser.newPage();
+  console.log(' >>page ready');
 
-const document = window.document;
+  // Navigate the page to a URL
+  await page.goto(`http://localhost:${PORT}`);
+  console.log(' >> navigated');
 
-global.document = document;
-global.navigator = {};
-// @ts-ignore
-global.window = window;
+  await page.setViewport({ width: 3000, height: 3000 });
 
-// Map some globals from JS dom to make sure react flow sort of works
-global.HTMLElement = window.Element;
-global.SVGElement = window.SVGElement;
+  await wait();
 
-// hmm, wierd stuff needed for html-to-image
-global.HTMLCanvasElement = window.HTMLCanvasElement;
-global.HTMLVideoElement = window.HTMLVideoElement;
-global.HTMLIFrameElement = window.HTMLIFrameElement;
-global.HTMLTextAreaElement = window.HTMLTextAreaElement;
-global.HTMLInputElement = window.HTMLInputElement;
-global.HTMLSelectElement = window.HTMLSelectElement;
-global.HTMLImageElement = window.HTMLImageElement;
-global.XMLSerializer = window.XMLSerializer;
-global.Image = window.Image;
-global.SVGImageElement = window.HTMLImageElement;
-global.Element = window.Element;
-mockReactFlow();
+  await page.screenshot({
+    path: 'page.png',
+  });
 
-// first we load a workflow diagram
+  // await browser.close();
 
-const root = document.getElementById('root');
-// nothing here?
-createRoot(root).render(<h1>blah</h1>);
+  console.log(' >> done!');
+  process.exit(0);
+}
 
-// wait for the render to actually run
-// awkward
-// setTimeout(() => {
-//   console.log(root.textContent);
-//   console.log(document.body.outerHTML);
-// }, 100);
-
-const model = {
-  triggers: [
-    {
-      id: 't1',
-      workflow_id: 'w',
-      type: 'webhook',
-      enabled: true,
-      name: 'Trigger',
-      webhook_url: 'www',
-      has_auth_method: false,
-    },
-  ],
-  jobs: [
-    {
-      id: 'n1',
-      workflow_id: 'w',
-      body: '.',
-      enabled: true,
-      name: 'Step 1',
-    },
-  ],
-  edges: [
-    {
-      id: 't1-n1',
-      source_trigger_id: 't1',
-      target_job_id: 'n1',
-      name: 'edge',
-    },
-  ],
-};
-
-// const store = createWorkflowStore(model, () => {});
-
-// createRoot(root).render(<WorkflowDiagram store={store}></WorkflowDiagram>);
-
-setTimeout(async () => {
-  /**
-   * Ok it's not enough just to take the SVG
-   * Some thing gets rendered to SVG, and some things get rendered to HTML
-   * The diagram is a mix of both
-   * So this simple approach won't work. Dang.
-   */
-  // const [svg] = document.getElementsByTagName('svg');
-  // console.log(svg.outerHTML);
-  // await fs.writeFile(__dirname + '/out.svg', svg.outerHTML);
-
-  // TODO OK so why does this break?
-  // getComputedStyle is not implemneted.
-  console.log(' >>> TO PNG');
-  // const dataUrl = await toPng(document.querySelector('.react-flow__viewport'), {
-  // const dataUrl = await toPng(document.getElementById('root'), {
-  //   // backgroundColor: '#1a365d',
-  //   // width: 500,
-  //   // height: 500,
-  //   // style: {
-  //   //   width: 500,
-  //   //   height: 500,
-  //   //   // TODO need to run a fit then do this
-  //   //   transform: `translate(0px, 0px) scale(1)`,
-  //   // },
-  // });
-
-  // take screenshot
-  generateImage();
-
-  console.log('DONE');
-  // console.log({ dataUrl });
-}, 500);
-
-// // why can't I find the svg in the dom?
-
-// // setTimeout(() => {
-// //   const out = fs.createWriteStream(__dirname + '/test.png');
-// //   const stream = canvas.createPNGStream();
-// //   stream.pipe(out);
-// //   out.on('finish', () => console.log('The PNG file was created.'));
-// // }, 100);
-
-// // toPng(root).then(function (dataUrl) {
-// //   console.log(dataUrl);
-// //   // var img = new Image();
-// //   // img.src = dataUrl;
-// //   // document.body.appendChild(img);
-// // });
-
-// toPng(document.querySelector('.react-flow__viewport'), {
-//   backgroundColor: '#1a365d',
-//   width: 500,
-//   height: 500,
-//   style: {
-//     width: 500,
-//     height: 500,
-//     // TODO need to run a it then do this)
-//     transform: `translate(0px, 0px) scale(1)`,
-//   },
-// }).then(dataUrl => {
-//   console.log(dataUrl);
-//   var img = new Image();
-//   img.src = dataUrl;
-//   document.body.appendChild(img);
-// });
+server().then(test);
