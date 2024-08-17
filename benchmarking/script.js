0be4f3e245990356ce5e0c9589541990e07e09c4
Taylor Downs <taylor@openfn.org>
2024-08-17 08:17:24 +0100

    Update benchmarking (#2394)
    * update benchmarking
    
    * default to localhost

--- benchmarking/script.js
+++ benchmarking/script.js
@@ -8,17 +8,31 @@ const webhookURL =
 export const options = {
   discardResponseBodies: true,
   scenarios: {
-    webhookRequests: {
-      executor: 'ramping-arrival-rate',
-      startRate: 1,
+    // test: {
+    //   executor: 'constant-arrival-rate',
+    //   rate: 1,
+    //   timeUnit: '1s',
+    //   duration: '1s',
+    //   preAllocatedVUs: 100,
+    // },
+    open_model: {
+      executor: 'constant-arrival-rate',
+      rate: 50,
       timeUnit: '1s',
-      preAllocatedVUs: 50,
-      stages: [
-        { duration: '30s', target: 50 }, // go from 1 to 50 rps in the first 30 seconds
-        { duration: '1m30s', target: 50 }, // hold at 50 rps for 1.5 minutes
-        { duration: '20s', target: 0 }, // ramp down back to 0 rps over the last 30 seconds
-      ],
+      duration: '5m',
+      preAllocatedVUs: 100,
     },
+    // webhookRequests: {
+    //   executor: 'ramping-arrival-rate',
+    //   startRate: 1,
+    //   timeUnit: '1s',
+    //   preAllocatedVUs: 500,
+    //   stages: [
+    //     { duration: '30s', target: 50 }, // go from 1 to 50 rps in the first 30 seconds
+    //     { duration: '1m30s', target: 50 }, // hold at 50 rps for 1.5 minutes
+    //     { duration: '20s', target: 0 }, // ramp down back to 0 rps over the last 30 seconds
+    //   ],
+    // },
   },
   thresholds: {
     http_req_failed: ['rate<0.0001'], // http errors should be less than 0.01%
@@ -49,5 +63,13 @@ export default function (data) {
   const payload = JSON.stringify(data.payload);
   const headers = { 'Content-Type': 'application/json' };
   const res = http.post(webhookURL, payload, { headers });
+  if (res.status != 200)
+    console.log(
+      res.status,
+      res.status_text,
+      res.body,
+      res.error,
+      res.error_code
+    );
   check(res, { 'status was 200': r => r.status == 200 });
 }
