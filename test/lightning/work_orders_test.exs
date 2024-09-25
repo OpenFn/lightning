3ca717b11d8f5066d6183ca97935e69606fab259
Elias W. BA <eliaswalyba@gmail.com>
2024-09-25 15:28:37 +0000

    Fix tests

--- test/lightning/work_orders_test.exs
+++ test/lightning/work_orders_test.exs
@@ -1902,26 +1902,39 @@ defmodule Lightning.WorkOrdersTest do
 
         user_id = user.id
 
-        assert [
-                 %Oban.Job{
-                   args:
-                     %{
-                       "workorders_ids" => ^workorders_ids1,
-                       "created_by" => ^user_id
-                     } =
-                       args1
-                 },
-                 %Oban.Job{
-                   args:
-                     %{
-                       "workorders_ids" => ^workorders_ids2,
-                       "created_by" => ^user_id
-                     } =
-                       args2
-                 }
-               ] = all_enqueued(queue: :scheduler) |> Enum.sort_by(& &1.id)
+        enqueued_jobs = all_enqueued(queue: :scheduler)
 
-        assert :ok = perform_job(RetryManyWorkOrdersJob, args1)
+        assert length(enqueued_jobs) == 2,
+               "Expected 2 jobs, got #{length(enqueued_jobs)}"
+
+        all_expected_ids = workorders_ids1 ++ workorders_ids2
+
+        all_actual_ids =
+          Enum.flat_map(enqueued_jobs, fn job ->
+            assert %{"created_by" => ^user_id, "workorders_ids" => ids} =
+                     job.args
+
+            ids
+          end)
+
+        assert Enum.sort(all_actual_ids) == Enum.sort(all_expected_ids),
+               "The workorder IDs in the enqueued jobs do not match the expected IDs"
+
+        job_with_workorder_1 =
+          Enum.find(enqueued_jobs, fn job ->
+            "#{workorder_1.id}" in job.args["workorders_ids"]
+          end)
+
+        job_with_workorder_101 =
+          Enum.find(enqueued_jobs, fn job ->
+            "#{workorder_101.id}" in job.args["workorders_ids"]
+          end)
+
+        assert job_with_workorder_1, "No job found containing workorder_1"
+        assert job_with_workorder_101, "No job found containing workorder_101"
+
+        assert :ok =
+                 perform_job(RetryManyWorkOrdersJob, job_with_workorder_1.args)
 
         assert Repo.get_by(Lightning.Run,
                  work_order_id: workorder_1.id,
@@ -1933,7 +1946,8 @@ defmodule Lightning.WorkOrdersTest do
                  starting_job_id: job_a.id
                )
 
-        assert :ok = perform_job(RetryManyWorkOrdersJob, args2)
+        assert :ok =
+                 perform_job(RetryManyWorkOrdersJob, job_with_workorder_101.args)
 
         assert Repo.get_by(Lightning.Run,
                  work_order_id: workorder_101.id,
