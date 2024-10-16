a5f4e1704df6e1c278e9d9aab8f41bb80f83bc04
Elias W. BA <eliaswalyba@gmail.com>
2024-10-16 14:58:43 +0000

    Tests for copy_error/4

--- test/lightning/helpers_test.exs
+++ test/lightning/helpers_test.exs
@@ -3,6 +3,9 @@ defmodule Lightning.HelpersTest do
 
   import Lightning.Helpers, only: [coerce_json_field: 2]
 
+  alias Ecto.Changeset
+  alias Lightning.Helpers
+
   test "coerce_json_field/2 will transform a json string inside a map by it's key" do
     input = %{
       "body" =>
@@ -53,5 +56,67 @@ defmodule Lightning.HelpersTest do
              name: "Sadio Mane",
              goals: 123
            }
+  end
+
+  describe "copy_error/4" do
+    test "copies an error from one key to another" do
+      changeset = %Changeset{errors: [name: {"has already been taken", []}]}
+      updated_changeset = Helpers.copy_error(changeset, :name, :raw_name)
+
+      assert updated_changeset.errors[:name] == {"has already been taken", []}
+
+      assert updated_changeset.errors[:raw_name] ==
+               {"has already been taken", []}
+    end
+
+    test "returns the changeset unchanged if original_key does not exist" do
+      changeset = %Changeset{errors: [email: {"is invalid", []}]}
+      updated_changeset = Helpers.copy_error(changeset, :name, :raw_name)
+
+      assert updated_changeset == changeset
+      refute Keyword.has_key?(updated_changeset.errors, :raw_name)
+    end
+
+    test "overwrites the new_key error if it exists and overwrite is true" do
+      changeset = %Changeset{
+        errors: [
+          name: {"has already been taken", []},
+          raw_name: {"is invalid", []}
+        ]
+      }
+
+      updated_changeset = Helpers.copy_error(changeset, :name, :raw_name)
+
+      assert updated_changeset.errors[:raw_name] ==
+               {"has already been taken", []}
+    end
+
+    test "does not overwrite the new_key error if overwrite is false" do
+      changeset = %Changeset{
+        errors: [
+          name: {"has already been taken", []},
+          raw_name: {"is invalid", []}
+        ]
+      }
+
+      updated_changeset =
+        Helpers.copy_error(changeset, :name, :raw_name, overwrite: false)
+
+      assert updated_changeset.errors[:raw_name] == {"is invalid", []}
+    end
+
+    test "returns the changeset unchanged if new_key already exists and overwrite is false" do
+      changeset = %Changeset{
+        errors: [
+          name: {"has already been taken", []},
+          raw_name: {"is invalid", []}
+        ]
+      }
+
+      updated_changeset =
+        Helpers.copy_error(changeset, :name, :raw_name, overwrite: false)
+
+      assert updated_changeset.errors[:raw_name] == {"is invalid", []}
+    end
   end
 end
