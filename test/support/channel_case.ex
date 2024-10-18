e51a865abd324c619ea913ae685cacceb9cf64c5
Stuart Corbishley <corbish@gmail.com>
2024-10-18 15:38:10 +0200

    Improve time sensitivity on ProjectRepoConnection tests

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -35,7 +35,7 @@ defmodule LightningWeb.ChannelCase do
   setup tags do
     Mox.stub_with(Lightning.MockConfig, Lightning.Config.API)
 
-    Mox.stub_with(LightningMock, Lightning.Stub)
+    Mox.stub_with(LightningMock, Lightning.API)
 
     # Default to Hackney adapter so that Bypass dependent tests continue working
     Mox.stub_with(Lightning.Tesla.Mock, Tesla.Adapter.Hackney)
