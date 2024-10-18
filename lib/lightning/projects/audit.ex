f4f89bcbb4e9c94857e41ef4a5a1c639464ebf0e
Rory McKinley <rorymckinley@gmail.com>
2024-10-18 09:11:51 +0200

    Extraction complete - clean up update code next

--- lib/lightning/projects/audit.ex
+++ lib/lightning/projects/audit.ex
@@ -26,4 +26,21 @@ defmodule Lightning.Projects.Audit do
       |> Lightning.Auditing.Audit.save(Repo)
     end
   end
+
+  def audit_dataclip_retention_period_updated(project, changeset, user) do
+    if Changeset.changed?(changeset, :dataclip_retention_period) do
+      dataclip_retention_period =
+        Changeset.get_change(changeset, :dataclip_retention_period)
+
+      event_changeset =
+        changeset
+        |> Map.merge(
+          %{changes: %{dataclip_retention_period: dataclip_retention_period}}
+        )
+
+      "dataclip_retention_period_updated"
+      |> event(project.id, user.id, event_changeset)
+      |> Lightning.Auditing.Audit.save(Repo)
+    end
+  end
 end
--- test/lightning/projects/audit_test.exs
+++ test/lightning/projects/audit_test.exs
@@ -104,4 +104,103 @@ defmodule Lightning.Projects.AuditTest do
       assert Auditing.Audit |> Repo.one() == nil
     end
   end
+
+  describe "./audit_dataclip_retention_period_updated/3"  do
+    setup do
+      project =
+        insert(
+          :project,
+          dataclip_retention_period: 14,
+          history_retention_period: 90,
+          retention_policy: :retain_all
+        )
+
+      user = insert(:user)
+
+      %{
+        project: project,
+        user: user
+      }
+    end
+
+    test "creates an event if the dataclip retention period is updated", %{
+      project: %{id: project_id} = project,
+      user: %{id: user_id} = user
+    } do
+      attrs = %{
+        dataclip_retention_period: 7,
+        history_retention_period: 30,
+        retention_policy: :retain_with_errors
+      }
+
+      changeset = project |> Project.changeset(attrs)
+
+      project
+      |> Audit.audit_dataclip_retention_period_updated(changeset, user)
+
+      audit_event = Auditing.Audit |> Repo.one!()
+
+      assert %{
+        event: "dataclip_retention_period_updated",
+        item_type: "project",
+        item_id:  ^project_id,
+        actor_id: ^user_id,
+        changes: changes
+      } = audit_event
+
+      assert changes == %Changes{
+        before: %{"dataclip_retention_period" => 14},
+        after: %{"dataclip_retention_period" => 7}
+      }
+    end
+
+    test "creates an event if the dataclip retention period is set to nil", %{
+      project: %{id: project_id} = project,
+      user: %{id: user_id} = user
+    } do
+      attrs = %{
+        dataclip_retention_period: nil,
+        history_retention_period: 30,
+        retention_policy: :retain_with_errors
+      }
+
+      changeset = project |> Project.changeset(attrs)
+
+      project
+      |> Audit.audit_dataclip_retention_period_updated(changeset, user)
+
+      audit_event = Auditing.Audit |> Repo.one!()
+
+      assert %{
+        event: "dataclip_retention_period_updated",
+        item_type: "project",
+        item_id:  ^project_id,
+        actor_id: ^user_id,
+        changes: changes
+      } = audit_event
+
+      assert changes == %Changes{
+        before: %{"dataclip_retention_period" => 14},
+        after: %{"dataclip_retention_period" => nil}
+      }
+    end
+
+    test "does not create event if dataclip retention period is not updated", %{
+      project: project,
+      user: user
+    } do
+      attrs = %{
+        dataclip_retention_period: 14,
+        history_retention_period: 30,
+        retention_policy: :retain_with_errors
+      }
+
+      changeset = project |> Project.changeset(attrs)
+
+      project
+      |> Audit.audit_dataclip_retention_period_updated(changeset, user)
+
+      assert Auditing.Audit |> Repo.one() == nil
+    end
+  end
 end
