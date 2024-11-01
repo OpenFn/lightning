73553a0648f375acd06e73889a4a3cef4800166b
Stuart Corbishley <corbish@gmail.com>
2024-11-01 13:39:39 +0200

    Use inserted_at for collections cursor

--- lib/lightning/policies/collections.ex
+++ lib/lightning/policies/collections.ex
@@ -16,7 +16,7 @@ defmodule Lightning.Policies.Collections do
   @type actions :: :access_collection
   @spec authorize(actions(), Lightning.Accounts.User.t(), Collection.t()) ::
           :ok | {:error, :unauthorized}
-  def authorize(:access_collection, user = %User{}, collection = %Collection{}) do
+  def authorize(:access_collection, %User{} = user, %Collection{} = collection) do
     Lightning.Policies.Permissions.can(
       Lightning.Policies.ProjectUsers,
       :access_project,
@@ -25,7 +25,7 @@ defmodule Lightning.Policies.Collections do
     )
   end
 
-  def authorize(:access_collection, run = %Run{}, collection = %Collection{}) do
+  def authorize(:access_collection, %Run{} = run, %Collection{} = collection) do
     Lightning.Runs.get_project_id_for_run(run) == collection.project_id
   end
 end
