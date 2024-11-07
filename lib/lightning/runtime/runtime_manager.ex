ee55e5f7f3015d0193ac5d2d4bb74898a4bc3bda
Stuart Corbishley <corbish@gmail.com>
2024-11-07 08:32:09 +0200

    Use Endpoint to dynamically set URI for local RuntimeManager

--- lib/lightning/runtime/runtime_manager.ex
+++ lib/lightning/runtime/runtime_manager.ex
@@ -44,6 +44,7 @@ defmodule Lightning.Runtime.RuntimeManager do
               port: 2222,
               repo_dir: nil,
               worker_secret: nil,
+              endpoint: nil,
               ws_url: "ws://localhost:4000/worker",
               col_url: "http://localhost:4000/collections"
 
@@ -59,6 +60,7 @@ defmodule Lightning.Runtime.RuntimeManager do
           Application.get_env(:lightning, __MODULE__, [])
           |> Keyword.merge(args)
         )
+        |> maybe_put_urls()
 
       {_, args} = args |> Keyword.split(config |> Map.keys())
 
@@ -108,6 +110,26 @@ defmodule Lightning.Runtime.RuntimeManager do
 
         _ ->
           [nil]
+      end
+    end
+
+    defp maybe_put_urls(config) do
+      if config.endpoint do
+        config
+        |> Map.merge(%{
+          ws_url:
+            Phoenix.VerifiedRoutes.unverified_url(config.endpoint, "/worker")
+            |> URI.parse()
+            |> Map.put(:scheme, "ws")
+            |> URI.to_string(),
+          col_url:
+            Phoenix.VerifiedRoutes.unverified_url(
+              config.endpoint,
+              "/collections"
+            )
+        })
+      else
+        config
       end
     end
   end
