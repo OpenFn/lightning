4724380fb96768d23dccf171368495929974ecb8
Elias W. BA <eliaswalyba@gmail.com>
2024-11-13 19:54:56 +0200

    Fix Broken Input Dataclip UI (#2670)
    * Fix input dataclip UI
    
    * Stretch instead of wrapper_class

--- lib/lightning_web/components/new_inputs.ex
+++ lib/lightning_web/components/new_inputs.ex
@@ -170,6 +170,10 @@ defmodule LightningWeb.Components.NewInputs do
 
   attr :class, :string, default: ""
 
+  attr :stretch, :boolean,
+    default: false,
+    doc: "control the wrapping div classes of some components like the textarea"
+
   attr :display_errors, :boolean, default: true
 
   attr :tooltip, :any, default: nil
@@ -248,7 +252,7 @@ defmodule LightningWeb.Components.NewInputs do
 
   def input(%{type: "textarea"} = assigns) do
     ~H"""
-    <div phx-feedback-for={@name}>
+    <div phx-feedback-for={@name} class={@stretch && "h-full"}>
       <.label :if={@label} for={@id}>
         <%= @label %><span
           :if={Map.get(@rest, :required, false)}
