d28472042f60f060f4ac7128c526f092fa275b80
Rory McKinley <rorymckinley@gmail.com>
2024-11-20 07:55:49 +0200

    Remove comment

--- lib/lightning/work_orders.ex
+++ lib/lightning/work_orders.ex
@@ -110,7 +110,6 @@ defmodule Lightning.WorkOrders do
   def create_for(%Job{} = job, multi, opts) do
     multi
     |> Multi.put(:workflow, opts[:workflow])
-    # get_or_create_snapshot unnecessary due to build_for?
     |> get_or_create_snapshot(opts[:actor])
     |> Multi.insert(:workorder, build_for(job, opts |> Map.new()))
     |> Runs.enqueue()
