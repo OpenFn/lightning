6701642f82f27206047a493c46b59faca3ad8996
Rory McKinley <rorymckinley@gmail.com>
2024-11-26 06:56:45 +0200

    Revert "Mock WIP"
    This reverts commit 385fbfe2f9e2d657349f8c99b9c536949386e319.

--- lib/lightning/version_control/version_control_usage_limiter.ex
+++ lib/lightning/version_control/version_control_usage_limiter.ex
@@ -1,15 +1,13 @@
 defmodule Lightning.VersionControl.VersionControlUsageLimiter do
   @moduledoc false
 
-  @behaviour Lightning.VersionControl.VersionControlUsageLimiting
-
+  alias Lightning.Extensions.UsageLimiting
   alias Lightning.Extensions.UsageLimiting.Action
   alias Lightning.Extensions.UsageLimiting.Context
   alias Lightning.Services.UsageLimiter
 
-  # @spec limit_github_sync(project_id :: Ecto.UUID.t() | nil) ::
-  #         :ok | {:error, UsageLimiting.message()}
-  @impl true
+  @spec limit_github_sync(project_id :: Ecto.UUID.t() | nil) ::
+          :ok | {:error, UsageLimiting.message()}
   def limit_github_sync(project_id) when is_binary(project_id) do
     case UsageLimiter.limit_action(%Action{type: :github_sync}, %Context{
            project_id: project_id
@@ -22,6 +20,5 @@ defmodule Lightning.VersionControl.VersionControlUsageLimiter do
     end
   end
 
-  @impl true
   def limit_github_sync(nil), do: :ok
 end
