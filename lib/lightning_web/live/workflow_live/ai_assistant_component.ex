3e414919fabffdb8b93650198ef1c12fbb23c480
Elias W. BA <eliaswalyba@gmail.com>
2024-11-28 12:53:58 +0000

    Ctrl + Enter to Send a Message to the AI Assistant

--- lib/lightning_web/live/workflow_live/ai_assistant_component.ex
+++ lib/lightning_web/live/workflow_live/ai_assistant_component.ex
@@ -449,6 +449,8 @@ defmodule LightningWeb.WorkflowLive.AiAssistantComponent do
             type="submit"
             disabled={@disabled}
             tooltip={@tooltip}
+            phx-hook="SendMessageViaCtrlEnter"
+            form="ai-assistant-form"
           >
             Send
           </.button>
