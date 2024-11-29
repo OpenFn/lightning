cb9cc748f4d726718f56db307ebf214c96c4de83
Rory McKinley <rorymckinley@gmail.com>
2024-11-29 09:40:05 +0200

    Audit provisioning events

--- test/lightning/workflows/audit_test.exs
+++ test/lightning/workflows/audit_test.exs
@@ -28,4 +28,72 @@ defmodule Lightning.Workflows.AuditTest do
              } = changeset
     end
   end
+
+  describe ".provisioner_event/2" do
+    test "returns a changeset for an `insert` event" do
+      %{id: user_id} = user = insert(:user)
+      workflow_id = Ecto.UUID.generate()
+
+      changeset = Audit.provisioner_event(:insert, workflow_id, user)
+
+      assert %{
+               changes: %{
+                 event: "inserted_by_provisioner",
+                 item_id: ^workflow_id,
+                 item_type: "workflow",
+                 actor_id: ^user_id,
+                 changes: %{
+                   changes: changes
+                 }
+               },
+               valid?: true
+             } = changeset
+
+      assert changes == %{}
+    end
+
+    test "returns a changeset for a `delete` event" do
+      %{id: user_id} = user = insert(:user)
+      workflow_id = Ecto.UUID.generate()
+
+      changeset = Audit.provisioner_event(:delete, workflow_id, user)
+
+      assert %{
+               changes: %{
+                 event: "deleted_by_provisioner",
+                 item_id: ^workflow_id,
+                 item_type: "workflow",
+                 actor_id: ^user_id,
+                 changes: %{
+                   changes: changes
+                 }
+               },
+               valid?: true
+             } = changeset
+
+      assert changes == %{}
+    end
+
+    test "returns a changeset for a `update` event" do
+      %{id: user_id} = user = insert(:user)
+      workflow_id = Ecto.UUID.generate()
+
+      changeset = Audit.provisioner_event(:update, workflow_id, user)
+
+      assert %{
+               changes: %{
+                 event: "updated_by_provisioner",
+                 item_id: ^workflow_id,
+                 item_type: "workflow",
+                 actor_id: ^user_id,
+                 changes: %{
+                   changes: changes
+                 }
+               },
+               valid?: true
+             } = changeset
+
+      assert changes == %{}
+    end
+  end
 end
