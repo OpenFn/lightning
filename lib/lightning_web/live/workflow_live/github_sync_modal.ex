19565551e33cdcaa792d26a06bcf59bdcbe2ebd4
Frank Midigo <midigofrank@gmail.com>
2025-01-08 15:20:58 +0300

    use hero icon

--- lib/lightning_web/live/workflow_live/github_sync_modal.ex
+++ lib/lightning_web/live/workflow_live/github_sync_modal.ex
@@ -56,28 +56,10 @@ defmodule LightningWeb.WorkflowLive.GithubSyncModal do
               <div class="rounded-md bg-blue-50 p-4">
                 <div class="flex">
                   <div class="flex-shrink-0">
-                    <svg
+                    <.icon
+                      name="hero-arrow-path"
                       class="animate-spin h-5 w-5 text-blue-400"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        class="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        stroke-width="4"
-                      >
-                      </circle>
-                      <path
-                        class="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      >
-                      </path>
-                    </svg>
+                    />
                   </div>
                   <div class="ml-3">
                     <p class="text-sm text-blue-700">
@@ -169,7 +151,7 @@ defmodule LightningWeb.WorkflowLive.GithubSyncModal do
               form="workflow-form"
               disabled={!@verify_connection.ok?}
             >
-              Save
+              Save and Sync
             </.button>
             <button
               type="button"
