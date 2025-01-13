dba49fce1188170cd186f59b9c51079989acd85a
Rory McKinley <rorymckinley@gmail.com>
2025-01-13 10:13:10 +0200

    Debugging metrics connection issues

--- lib/lightning_web/prom_ex_plug_authorization.ex
+++ lib/lightning_web/prom_ex_plug_authorization.ex
@@ -5,10 +5,15 @@ defmodule LightningWeb.PromExPlugAuthorization do
   """
   @behaviour Unplug.Predicate
 
+  require Logger
+
   @impl true
   def call(conn, _vars) do
     config = Application.get_env(:lightning, Lightning.PromEx)
 
+    Logger.error(
+      "ENDPOINT provided token: #{Plug.Conn.get_req_header(conn, "authorization")}"
+    )
     if config[:metrics_endpoint_authorization_required] do
       valid_token?(
         Plug.Conn.get_req_header(conn, "authorization"),
@@ -24,6 +29,7 @@ defmodule LightningWeb.PromExPlugAuthorization do
   end
 
   defp valid_token?(["Bearer " <> provided_token], expected_token) do
+    Logger.error("ENDPOINT Comparing Token #{Plug.Crypto.secure_compare(provided_token, expected_token)}")
     Plug.Crypto.secure_compare(provided_token, expected_token)
   end
 
@@ -32,6 +38,8 @@ defmodule LightningWeb.PromExPlugAuthorization do
   end
 
   defp valid_scheme?(provided_scheme, expected_scheme) do
+    Logger.error("ENDPOINT Provided Scheme: #{provided_scheme}")
+    Logger.error("ENDPOINT Comparing Scheme: #{provided_scheme == expected_scheme}")
     provided_scheme == expected_scheme
   end
 end
