d3999a56e4e04d0374aabe03ae61c1c19c842eb7
Elias W. BA <eliaswalyba@gmail.com>
2025-01-15 12:16:51 +0000

    Format

--- assets/js/common.js
+++ assets/js/common.js
@@ -29,7 +29,6 @@ export function initiateSaveAndRun(buttonElement) {
   }
 }
 
-
 export function randomUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c =>
     (c === 'x' ? (Math.random() * 16) | 0 : 'r&0x3' | '0x8').toString(16)
