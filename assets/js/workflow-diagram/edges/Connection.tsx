6473eff3ec567cb3df0de88d6a471b7df05230b6
Xandor Schiefer <xandor@pixeltheory.dev>
2025-02-15 16:27:20 +0200

    Fix various client-side type errors (#2850)
    Just the low-hanging fruit.

--- assets/js/workflow-diagram/edges/Connection.tsx
+++ assets/js/workflow-diagram/edges/Connection.tsx
@@ -1,7 +1,12 @@
 import { EDGE_COLOR_SELECTED } from '../styles';
 import { BezierEdge } from 'reactflow';
 
-export default props => {
+export default (props: {
+  fromX: number;
+  fromY: number;
+  toX: number;
+  toY: number;
+}) => {
   const { fromX, fromY, toX, toY } = props;
   return (
     <BezierEdge
