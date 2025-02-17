de046233122bb9dd8bea6aae77d2f02f5bcb2136
Stuart Corbishley <corbish@gmail.com>
2025-02-17 11:22:43 +0200

    Bump Node to 22.12.0 in CI and .tool-versions (#2938)
    * Bump Node to 22.12.0 in CI and .tool-versions
    
    Also bumping Postgres to 17.3 for CI.
    
    * Fix ava tests for Node 22
    
    * Update CHANGELOG
    
    [ci skip]

--- test/integration/web_and_worker_test.exs
+++ test/integration/web_and_worker_test.exs
@@ -252,7 +252,7 @@ defmodule Lightning.WebAndWorkerTest do
           |> Map.get(:message)
         end)
 
-      assert version_logs =~ "▸ node.js                  18.17"
+      assert version_logs =~ "▸ node.js                  22.12"
       assert version_logs =~ "▸ worker                   1.9"
       assert version_logs =~ "▸ @openfn/language-http    3.1.12"
 
