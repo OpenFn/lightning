60e40b8766cb1bfdeaf981d30720d4c5a9572440
Frank Midigo <midigofrank@gmail.com>
2025-02-19 16:34:25 +0300

    catch exception program_limit_exceeded when dataclip ts_vector is too big

---
+++ priv/repo/migrations/20250219122902_catch_ts_vector_size_exception.exs
@@ -0,0 +1,61 @@
+defmodule Lightning.Repo.Migrations.CatchTsVectorSizeException do
+  use Ecto.Migration
+
+  def up do
+    execute "DROP TRIGGER IF EXISTS set_search_vector ON dataclips"
+
+    execute "DROP FUNCTION IF EXISTS update_dataclip_search_vector"
+
+    execute("""
+    CREATE OR REPLACE FUNCTION update_dataclip_search_vector()
+    RETURNS trigger
+    LANGUAGE plpgsql
+    AS $function$
+    BEGIN
+      BEGIN
+        UPDATE dataclips
+        SET search_vector = jsonb_to_tsvector('english_nostop', body, '"all"')
+        WHERE id = NEW.id;
+      EXCEPTION
+        WHEN program_limit_exceeded THEN
+          RAISE NOTICE 'Message too long for tsvector at id: %. Error: %', NEW.id, SQLERRM;
+      END;
+
+      RETURN NEW;
+    END;
+    $function$;
+    """)
+
+    execute("""
+    CREATE TRIGGER set_search_vector
+    AFTER INSERT ON dataclips FOR EACH ROW
+    WHEN (NEW."search_vector" IS NULL)
+    EXECUTE PROCEDURE update_dataclip_search_vector()
+    """)
+  end
+
+  def down do
+    execute "DROP TRIGGER IF EXISTS set_search_vector ON dataclips"
+
+    execute "DROP FUNCTION IF EXISTS update_dataclip_search_vector"
+
+    execute("""
+    CREATE OR REPLACE FUNCTION update_dataclip_search_vector()
+    RETURNS trigger
+    LANGUAGE plpgsql
+    AS $function$
+      begin
+        UPDATE log_lines SET search_vector = jsonb_to_tsvector('english_nostop', body, '"all"') WHERE id = NEW.id;
+        RETURN NEW;
+      end
+    $function$ ;
+    """)
+
+    execute("""
+    CREATE TRIGGER set_search_vector
+    AFTER INSERT ON dataclips FOR EACH ROW
+    WHEN (NEW."search_vector" IS NULL)
+    EXECUTE PROCEDURE update_dataclip_search_vector()
+    """)
+  end
+end
