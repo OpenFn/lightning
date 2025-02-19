aac8d45153c85b4a29a0c9c0734af0464fc01621
Frank Midigo <midigofrank@gmail.com>
2025-02-19 17:19:48 +0300

    simplify migration

--- priv/repo/migrations/20250219122902_catch_ts_vector_size_exception.exs
+++ priv/repo/migrations/20250219122902_catch_ts_vector_size_exception.exs
@@ -1,61 +1,38 @@
 defmodule Lightning.Repo.Migrations.CatchTsVectorSizeException do
   use Ecto.Migration
 
-  def up do
-    execute "DROP TRIGGER IF EXISTS set_search_vector ON dataclips"
-
-    execute "DROP FUNCTION IF EXISTS update_dataclip_search_vector"
-
-    execute("""
-    CREATE OR REPLACE FUNCTION update_dataclip_search_vector()
-    RETURNS trigger
-    LANGUAGE plpgsql
-    AS $function$
-    BEGIN
+  def change do
+    execute(
+      """
+      CREATE OR REPLACE FUNCTION update_dataclip_search_vector()
+      RETURNS trigger
+      LANGUAGE plpgsql
+      AS $function$
       BEGIN
-        UPDATE dataclips
-        SET search_vector = jsonb_to_tsvector('english_nostop', body, '"all"')
-        WHERE id = NEW.id;
-      EXCEPTION
-        WHEN program_limit_exceeded THEN
-          RAISE NOTICE 'Message too long for tsvector at id: %. Error: %', NEW.id, SQLERRM;
-      END;
+        BEGIN
+          UPDATE dataclips
+          SET search_vector = jsonb_to_tsvector('english_nostop', body, '"all"')
+          WHERE id = NEW.id;
+        EXCEPTION
+          WHEN program_limit_exceeded THEN
+            RAISE NOTICE 'Message too long for tsvector at id: %. Error: %', NEW.id, SQLERRM;
+        END;
 
-      RETURN NEW;
-    END;
-    $function$;
-    """)
-
-    execute("""
-    CREATE TRIGGER set_search_vector
-    AFTER INSERT ON dataclips FOR EACH ROW
-    WHEN (NEW."search_vector" IS NULL)
-    EXECUTE PROCEDURE update_dataclip_search_vector()
-    """)
-  end
-
-  def down do
-    execute "DROP TRIGGER IF EXISTS set_search_vector ON dataclips"
-
-    execute "DROP FUNCTION IF EXISTS update_dataclip_search_vector"
-
-    execute("""
-    CREATE OR REPLACE FUNCTION update_dataclip_search_vector()
-    RETURNS trigger
-    LANGUAGE plpgsql
-    AS $function$
-      begin
-        UPDATE dataclips SET search_vector = jsonb_to_tsvector('english_nostop', body, '"all"') WHERE id = NEW.id;
         RETURN NEW;
-      end
-    $function$ ;
-    """)
-
-    execute("""
-    CREATE TRIGGER set_search_vector
-    AFTER INSERT ON dataclips FOR EACH ROW
-    WHEN (NEW."search_vector" IS NULL)
-    EXECUTE PROCEDURE update_dataclip_search_vector()
-    """)
+      END;
+      $function$;
+      """,
+      """
+      CREATE OR REPLACE FUNCTION update_dataclip_search_vector()
+      RETURNS trigger
+      LANGUAGE plpgsql
+      AS $function$
+        begin
+          UPDATE dataclips SET search_vector = jsonb_to_tsvector('english_nostop', body, '"all"') WHERE id = NEW.id;
+          RETURN NEW;
+        end
+      $function$ ;
+      """
+    )
   end
 end
