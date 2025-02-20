c68f7fda13acd8d99ba3b4d12cc7beacdca9fe15
Elias W. BA <eliaswalyba@gmail.com>
2025-02-20 21:29:46 +0000

    Remove duplicated test

--- test/lightning/credentials/credential_test.exs
+++ test/lightning/credentials/credential_test.exs
@@ -33,61 +33,6 @@ defmodule Lightning.Credentials.CredentialTest do
         "expires_in" => 3245
       })
     end
-
-    # test "user_id not valid for transfer" do
-    #   %{id: user_id, first_name: first_name, last_name: last_name} =
-    #     insert(:user, first_name: "Elias", last_name: "BA")
-
-    #   insert(:project, name: "project-a", project_users: [%{user_id: user_id}])
-
-    #   %{id: project_id_b, name: project_name_b} =
-    #     insert(:project, name: "project-b")
-
-    #   %{id: project_id_c, name: project_name_c} =
-    #     insert(:project, name: "project-c")
-
-    #   errors =
-    #     Credential.changeset(
-    #       insert(:credential,
-    #         project_credentials: [
-    #           %{project_id: project_id_b},
-    #           %{project_id: project_id_c}
-    #         ]
-    #       ),
-    #       %{user_id: user_id}
-    #     )
-    #     |> errors_on()
-
-    #   assert errors[:user_id] == [
-    #            "Invalid owner: #{first_name} #{last_name} doesn't have access to #{project_name_b}, #{project_name_c}. Please grant them access or select another owner."
-    #          ]
-    # end
-
-    # test "user_id is valid for transfer" do
-    #   %{id: user_id_1} = insert(:user)
-    #   %{id: user_id_2} = insert(:user)
-
-    #   %{id: project_id} =
-    #     insert(:project,
-    #       name: "some-name",
-    #       project_users: [%{user_id: user_id_1}, %{user_id: user_id_2}]
-    #     )
-
-    #   credential =
-    #     insert(:credential,
-    #       user_id: user_id_1,
-    #       project_credentials: [%{project_id: project_id}]
-    #     )
-
-    #   errors =
-    #     Credential.changeset(
-    #       credential,
-    #       %{user_id: user_id_2}
-    #     )
-    #     |> errors_on()
-
-    #   assert errors[:user_id] == nil
-    # end
   end
 
   describe "encryption" do
