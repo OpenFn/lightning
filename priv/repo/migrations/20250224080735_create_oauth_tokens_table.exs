5cb103877a9c36480a69e6930865ab77fc62319e
Elias W. BA <eliaswalyba@gmail.com>
2025-02-25 14:06:28 +0000

    Database migrations - OauthToken table - Migrate old oauth credentials to the new schema

---
+++ priv/repo/migrations/20250224080735_create_oauth_tokens_table.exs
@@ -0,0 +1,25 @@
+defmodule Lightning.Repo.Migrations.CreateOauthTokensTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:oauth_tokens, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :body, :binary, null: false
+      add :scopes, {:array, :string}, null: false
+      add :oauth_client_id, references(:oauth_clients, type: :binary_id)
+      add :user_id, references(:users, type: :binary_id, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+
+    create index(:oauth_tokens, [:oauth_client_id])
+    create index(:oauth_tokens, [:user_id])
+    create unique_index(:oauth_tokens, [:user_id, :oauth_client_id, :scopes])
+
+    alter table(:credentials) do
+      add :oauth_token_id, references(:oauth_tokens, type: :binary_id)
+    end
+
+    create index(:credentials, [:oauth_token_id])
+  end
+end
