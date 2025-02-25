2bb4d72f984d3cc9f320eb7cdfa1a46783aebe8e
Elias W. BA <eliaswalyba@gmail.com>
2025-02-25 15:13:49 +0000

    Refactor and clean

--- lib/lightning_web/live/credential_live/index.ex
+++ lib/lightning_web/live/credential_live/index.ex
@@ -93,7 +93,6 @@ defmodule LightningWeb.CredentialLive.Index do
 
   defp list_credentials(user) do
     Credentials.list_credentials(user)
-    |> IO.inspect(label: "Credentials")
     |> Enum.map(fn c ->
       project_names =
         Map.get(c, :projects, [])
