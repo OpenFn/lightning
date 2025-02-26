fd733d9e735e41ba74010503ced4872ef842d2d7
Frank Midigo <midigofrank@gmail.com>
2025-02-26 16:07:19 +0300

    update changelog

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lightning.MixProject do
   def project do
     [
       app: :lightning,
-      version: "2.10.15",
+      version: "2.10.16-pre.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [
