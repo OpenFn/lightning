c29128320e546a9fad6c3f4d96d17b1334426741
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-05-08 16:21:04 +0000

    fix: input type to search

--- assets/js/manual-runner/ManualRunner.tsx
+++ assets/js/manual-runner/ManualRunner.tsx
@@ -446,7 +446,7 @@ const NoneView: React.FC<{
               onChange={e => {
                 setQuery(e.target.value);
               }}
-              type="email"
+              type="search"
               className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pr-3 pl-10 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300"
               placeholder="Filter inputs"
             />
