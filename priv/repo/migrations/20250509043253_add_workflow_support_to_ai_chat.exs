928b3d2f1cd8fc02475eec7bc9cfb5dda13d5292
Elias W. BA <eliaswalyba@gmail.com>
2025-05-09 14:26:59 +0000

    Fix migration file errors

--- priv/repo/migrations/20250509043253_add_workflow_support_to_ai_chat.exs
+++ priv/repo/migrations/20250509043253_add_workflow_support_to_ai_chat.exs
@@ -8,13 +8,18 @@ defmodule Lightning.Repo.Migrations.AddWorkflowSupportToAiChat do
 
     alter table(:ai_chat_sessions) do
       add :session_type, :string, default: "job"
-      add :project_id, references(:projects, on_delete: :delete_all), null: true
-      add :workflow_id, references(:workflows, on_delete: :nilify_all), null: true
+      add :project_id, references(:projects, on_delete: :delete_all, type: :binary_id), null: true
 
-      modify :job_id, references(:jobs, on_delete: :delete_all), null: true
+      add :workflow_id, references(:workflows, on_delete: :nilify_all, type: :binary_id),
+        null: true
     end
 
     create index(:ai_chat_sessions, [:project_id])
     create index(:ai_chat_sessions, [:workflow_id])
+
+    execute(
+      "ALTER TABLE ai_chat_sessions ALTER COLUMN job_id DROP NOT NULL",
+      "ALTER TABLE ai_chat_sessions ALTER COLUMN job_id SET NOT NULL"
+    )
   end
 end
