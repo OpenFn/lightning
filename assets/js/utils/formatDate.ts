a9d8f005d4ed5f139e69b2674569d9fc2c2273c6
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-05-09 13:02:38 +0000

    fix: update date formatting

--- assets/js/utils/formatDate.ts
+++ assets/js/utils/formatDate.ts
@@ -1,16 +1,9 @@
-export default function formatDate(
-  date: Date,
-  locale: string = 'en-US'
-): string {
-  return new Intl.DateTimeFormat(locale, {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-  })
-    .format(date)
-    .replace(/\//g, '-');
+export default function formatDate(date: Date): string {
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const dd = String(date.getDate()).padStart(2, '0');
+  const hh = String(date.getHours()).padStart(2, '0');
+  const mins = String(date.getMinutes()).padStart(2, '0');
+  const secs = String(date.getSeconds()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd} ${hh}:${mins}:${secs}`;
 }
