a30d60086dbde6a2fc384a7c7660a4bb7aa19ded
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-05-12 12:20:09 +0000

    feat: update search bar

--- assets/js/manual-run-panel/views/ExistingView.tsx
+++ assets/js/manual-run-panel/views/ExistingView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DataclipTypeNames, DataclipTypes, FilterTypes, type Dataclip, type SetDates } from "../types";
 import useOutsideClick from "#/hooks/useOutsideClick";
 import Pill from "../Pill";
-import { CalendarDaysIcon, CheckIcon, DocumentTextIcon, MagnifyingGlassIcon, RectangleGroupIcon } from "@heroicons/react/24/outline";
+import { CalendarDaysIcon, CheckIcon, DocumentTextIcon, MagnifyingGlassIcon, RectangleGroupIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import formatDate from "#/utils/formatDate";
 import truncateUid from "#/utils/truncateUID";
 
@@ -52,111 +52,120 @@ const ExistingView: React.FC<ExistingViewProps> = ({
   return (
     <>
       <div className="flex flex-col gap-3">
-        <div>
-          <div className="relative flex gap-2">
+        <div className="flex gap-2">
+          <div className="relative rounded-md shadow-xs flex grow">
             <input
               value={query}
               onChange={e => {
                 setQuery(e.target.value);
               }}
-              type="search"
-              className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pr-3 pl-10 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300"
+              type="text"
+              className="focus:outline focus:outline-2 focus:-outline-offset-2 focus:ring-0  disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 border-slate-300 focus:border-slate-400 focus:outline-indigo-600 block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Filter inputs"
             />
-            <div className="absolute left-1 top-1 rounded p-1.5 border border-transparent text-center text-sm">
-              <MagnifyingGlassIcon className={iconStyle} />
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
             </div>
-            <div className="relative inline-block">
-              <button
-                onClick={() => {
-                  setDateOpen(p => !p);
-                }}
-                className="border rounded-md px-3 py-1 h-full flex justify-center items-center hover:bg-slate-100 hover:border-slate-300"
-              >
-                <CalendarDaysIcon className="w-6 h-6 text-slate-700" />
-              </button>
-              <div
-                ref={calendarRef}
-                className={`absolute right-0 ml-1.5 z-10 mt-2 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none min-w-[260px] ${dateOpen ? '' : 'hidden'
-                  } `}
-              >
-                <div className="py-3" role="none">
-                  <div className="px-4 py-1 text-gray-500 text-sm">
-                    Filter by Date Created
-                  </div>
-                  <div className="px-4 py-1 text-gray-700 text-sm">
-                    <label htmlFor="created-after">Created After</label>
-                    <input
-                      value={selectedDates.after}
-                      id="created-after"
-                      onChange={e => {
-                        setSelectedDates(p => ({
-                          after: e.target.value,
-                          before: p.before,
-                        }));
-                      }}
-                      className="focus:outline focus:outline-2 focus:outline-offset-1 block w-full rounded-lg border-slate-300 text-slate-900 focus:ring-0 sm:text-sm sm:leading-6"
-                      type="date"
-                    />
-                  </div>
-                  <div className="px-4 py-1 text-gray-700 text-sm">
-                    <label htmlFor="created-before">Created Before</label>
-                    <input
-                      value={selectedDates.before}
-                      id="created-before"
-                      onChange={e => {
-                        setSelectedDates(p => ({
-                          after: p.after,
-                          before: e.target.value,
-                        }));
-                      }}
-                      className="focus:outline focus:outline-2 focus:outline-offset-1 block w-full rounded-lg border-slate-300 text-slate-900 focus:ring-0 sm:text-sm sm:leading-6"
-                      type="date"
-                    />
-                  </div>
+            {
+              query.trim() ?
+                <div
+                  onClick={() => { setQuery("") }}
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer">
+                  <XMarkIcon className="h-5 w-5 text-gray-400" />
+                </div>
+                : null
+            }
+          </div>
+          <div className="relative inline-block">
+            <button
+              onClick={() => {
+                setDateOpen(p => !p);
+              }}
+              className="border rounded-md px-3 py-1 h-full flex justify-center items-center hover:bg-slate-100 hover:border-slate-300"
+            >
+              <CalendarDaysIcon className="w-6 h-6 text-slate-700" />
+            </button>
+            <div
+              ref={calendarRef}
+              className={`absolute right-0 ml-1.5 z-10 mt-2 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none min-w-[260px] ${dateOpen ? '' : 'hidden'
+                } `}
+            >
+              <div className="py-3" role="none">
+                <div className="px-4 py-1 text-gray-500 text-sm">
+                  Filter by Date Created
+                </div>
+                <div className="px-4 py-1 text-gray-700 text-sm">
+                  <label htmlFor="created-after">Created After</label>
+                  <input
+                    value={selectedDates.after}
+                    id="created-after"
+                    onChange={e => {
+                      setSelectedDates(p => ({
+                        after: e.target.value,
+                        before: p.before,
+                      }));
+                    }}
+                    className="focus:outline focus:outline-2 focus:outline-offset-1 block w-full rounded-lg border-slate-300 text-slate-900 focus:ring-0 sm:text-sm sm:leading-6"
+                    type="date"
+                  />
+                </div>
+                <div className="px-4 py-1 text-gray-700 text-sm">
+                  <label htmlFor="created-before">Created Before</label>
+                  <input
+                    value={selectedDates.before}
+                    id="created-before"
+                    onChange={e => {
+                      setSelectedDates(p => ({
+                        after: p.after,
+                        before: e.target.value,
+                      }));
+                    }}
+                    className="focus:outline focus:outline-2 focus:outline-offset-1 block w-full rounded-lg border-slate-300 text-slate-900 focus:ring-0 sm:text-sm sm:leading-6"
+                    type="date"
+                  />
                 </div>
               </div>
             </div>
-            <div className="relative inline-block">
-              <button
-                onClick={() => {
-                  setTypesOpen(p => !p);
-                }}
-                className="border rounded-md px-3 py-1 h-full flex justify-center items-center hover:bg-slate-100 hover:border-slate-300"
-              >
-                <RectangleGroupIcon className="w-6 h-6 text-slate-700" />
-              </button>
-              <ul
-                ref={typesRef}
-                className={`absolute z-10 mt-2 bg-white ring-1 ring-black/5 focus:outline-none rounded-md shadow-lg right-0 w-auto overflow-hidden ${typesOpen ? '' : 'hidden'
-                  } `}
-              >
-                {DataclipTypes.map(type => {
-                  return (
-                    <li
-                      key={type}
-                      onClick={() => {
-                        setSelectedclipType(
-                          type === selectedcliptype ? '' : type
-                        );
-                      }}
-                      className={`px-4 py-2 hover:bg-slate-100 cursor-pointer text-nowrap flex items-center gap-2 text-base text-slate-700 ${type === selectedcliptype
-                        ? 'bg-blue-200 text-blue-700'
-                        : ''
+          </div>
+          <div className="relative inline-block">
+            <button
+              onClick={() => {
+                setTypesOpen(p => !p);
+              }}
+              className="border rounded-md px-3 py-1 h-full flex justify-center items-center hover:bg-slate-100 hover:border-slate-300"
+            >
+              <RectangleGroupIcon className="w-6 h-6 text-slate-700" />
+            </button>
+            <ul
+              ref={typesRef}
+              className={`absolute z-10 mt-2 bg-white ring-1 ring-black/5 focus:outline-none rounded-md shadow-lg right-0 w-auto overflow-hidden ${typesOpen ? '' : 'hidden'
+                } `}
+            >
+              {DataclipTypes.map(type => {
+                return (
+                  <li
+                    key={type}
+                    onClick={() => {
+                      setSelectedclipType(
+                        type === selectedcliptype ? '' : type
+                      );
+                    }}
+                    className={`px-4 py-2 hover:bg-slate-100 cursor-pointer text-nowrap flex items-center gap-2 text-base text-slate-700 ${type === selectedcliptype
+                      ? 'bg-blue-200 text-blue-700'
+                      : ''
+                      }`}
+                  >
+                    {' '}
+                    {DataclipTypeNames[type]}{' '}
+                    <CheckIcon
+                      strokeWidth={3}
+                      className={`${iconStyle} ${type !== selectedcliptype ? 'invisible' : ''
                         }`}
-                    >
-                      {' '}
-                      {DataclipTypeNames[type]}{' '}
-                      <CheckIcon
-                        strokeWidth={3}
-                        className={`${iconStyle} ${type !== selectedcliptype ? 'invisible' : ''
-                          }`}
-                      />{' '}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+                    />{' '}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
           <div className="flex gap-1 mt-2">{pills}</div>
         </div>
