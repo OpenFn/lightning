d36f3ebaf62f2d65694033725854c1216d91da53
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-05-12 11:13:33 +0000

    feat: center justify

--- assets/js/manual-run-panel/views/EmptyView.tsx
+++ assets/js/manual-run-panel/views/EmptyView.tsx
@@ -3,7 +3,7 @@ import { InformationCircleIcon } from "@heroicons/react/24/outline";
 const EmptyView: React.FC = () => {
   return (
     <div className="h-16 p-4">
-      <div className="text-gray-500 pb-2 flex gap-1">
+      <div className="text-gray-500 pb-2 justify-center flex gap-1">
         <div className="flex mt-0.5"><InformationCircleIcon className={`h-5 w-5 text-yellow-600 pb-[1px]`} /></div>
         An empty object will be used as the input for this run.
       </div>
