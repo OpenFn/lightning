9b8fe59e5f0aca7b61984230133887e6ed7bdbae
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-06-23 11:28:20 +0000

    fix: width & height in auto layout.

--- assets/js/workflow-diagram/layout.ts
+++ assets/js/workflow-diagram/layout.ts
@@ -41,11 +41,16 @@ const calculateLayout = async (
 
   Dagre.layout(g, { disableOptimalOrderHeuristic: true });
 
+  // we set width & height above for dagre calculations. 
+  // don't set width & height below. let reactflow calculate that!
   const newModel = {
     nodes: nodes.map(node => {
-      const { x, y, width, height } = g.node(node.id);
+      const { x, y } = g.node(node.id);
 
-      return { ...node, position: { x, y }, width, height };
+      return {
+        ...node,
+        position: { x, y },
+      };
     }),
     edges,
   };
