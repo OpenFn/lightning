b5e81737e74eaf4cc4a46a6ef10e741222929322
Stuart Corbishley <corbish@gmail.com>
2025-07-10 15:35:06 +0200

    Fix: rounded corners on tables

--- lib/lightning_web/components/table.ex
+++ lib/lightning_web/components/table.ex
@@ -101,7 +101,9 @@ defmodule LightningWeb.Components.Table do
         ]}>
           {render_slot(@body)}
         </tbody>
-        <tfoot>{render_slot(@footer)}</tfoot>
+        <%= for footer <- @footer do %>
+          <tfoot>{footer}</tfoot>
+        <% end %>
       </table>
       <%= if @page && @url do %>
         <div class="border-t border-gray-200">
@@ -148,6 +150,7 @@ defmodule LightningWeb.Components.Table do
         "transition-colors duration-150",
         "has-[td]:hover:bg-gray-50",
         @onclick && "cursor-pointer",
+        "last:rounded-b-lg",
         "[&>td:first-child]:py-4 [&>td:first-child]:pr-3 [&>td:first-child]:pl-4 [&>td:first-child]:sm:pl-6",
         "[&>th:first-child]:py-3.5 [&>th:first-child]:pr-3 [&>th:first-child]:pl-4 [&>th:first-child]:sm:pl-6",
         "[&>td:not(:first-child):not(:last-child)]:px-3 [&>td:not(:first-child):not(:last-child)]:py-4",
@@ -262,7 +265,14 @@ defmodule LightningWeb.Components.Table do
 
   def td(assigns) do
     ~H"""
-    <td id={@id} class={["text-sm text-gray-500", @class]} {@rest}>
+    <td
+      id={@id}
+      class={[
+        "text-sm text-gray-500 first:rounded-bl-lg last:rounded-br-lg",
+        @class
+      ]}
+      {@rest}
+    >
       {render_slot(@inner_block)}
     </td>
     """
