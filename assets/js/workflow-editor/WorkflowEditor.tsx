f26c5ef4c0fe7842bb5c682ed420feca60d954c8
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-07-18 11:37:29 +0000

    fix: no need for history mode

--- assets/js/workflow-editor/WorkflowEditor.tsx
+++ assets/js/workflow-editor/WorkflowEditor.tsx
@@ -39,12 +39,10 @@ export const WorkflowEditor: WithActionProps<{ selection: string }> = (props) =>
   }
 
   const onRunChangeHandler = (id: string, version: number) => {
-    console.log("clicked:", version)
     const currentUrl = new URL(window.location.href);
     const nextUrl = new URL(currentUrl);
     nextUrl.searchParams.set('a', id);
     nextUrl.searchParams.set('v', version.toString());
-    nextUrl.searchParams.set('m', 'history')
     props.navigate(nextUrl.toString());
   }
 
