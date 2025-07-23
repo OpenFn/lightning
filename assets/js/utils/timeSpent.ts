c9ea1348a2b61a741c4ca990528b7333b1afd86f
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-07-23 08:07:53 +0000

    fix: remove log

--- assets/js/utils/timeSpent.ts
+++ assets/js/utils/timeSpent.ts
@@ -2,7 +2,6 @@ export function timeSpent(dateStr1: string, dateStr2: string) {
   const date1 = new Date(dateStr1).getTime();
   const date2 = new Date(dateStr2).getTime();
   const diffMs = Math.abs(date2 - date1);
-  console.log(date1, date2, diffMs);
 
   if (diffMs >= 3600000) {
     return `${(diffMs / 3600000).toFixed(2)}hours`;
