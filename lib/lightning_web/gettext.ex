44e0a54ca9b8b9d9318da96c00eaa3335032516c
Stuart Corbishley <corbish@gmail.com>
2025-07-25 16:29:19 +0200

    Update dependencies and refactor Gettext usage
    - Upgrade certifi to version 2.15.0
    - Upgrade expo to version 1.1.0
    - Upgrade gettext to version 0.26.2
    - Upgrade hackney to version 1.25.0
    - Upgrade timex to version 3.7.13
    - Upgrade tzdata to version 1.1.3
    - Upgrade unicode_util_compat to version 0.7.1
    - Refactor Gettext usage to new style

--- lib/lightning_web/gettext.ex
+++ lib/lightning_web/gettext.ex
@@ -5,7 +5,7 @@ defmodule LightningWeb.Gettext do
   By using [Gettext](https://hexdocs.pm/gettext),
   your module gains a set of macros for translations, for example:
 
-      import LightningWeb.Gettext
+      use Gettext, backend: LightningWeb.Gettext
 
       # Simple translation
       gettext("Here is the string to translate")
@@ -20,5 +20,5 @@ defmodule LightningWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :lightning
+  use Gettext.Backend, otp_app: :lightning
 end
