812f8df351b3726008687c01f59512d2d70d0a18
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-07-29 02:42:02 +0000

    fix: don't disable trigger with startinfo

--- assets/js/workflow-diagram/nodes/Node.tsx
+++ assets/js/workflow-diagram/nodes/Node.tsx
@@ -94,7 +94,7 @@ const Node = ({
   const startInfo = data?.startInfo as { started_at: string, startBy: string } | undefined;
   const isErrorRun = runData?.exit_reason !== "success";
   // TODO: remember triggers
-  const didRun = data.isRun ? (!!runData || !!data?.startInfo) : true
+  const didRun = data.isRun ? (!!runData || (!!data?.startInfo && isTriggerNode)) : true
 
   const { width, height, anchorx, strokeWidth, style } = nodeIconStyles(
     selected,
