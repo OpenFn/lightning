0e6c26dfad216b6e16db6c88fbba5479cc328aa4
Stuart Corbishley <corbish@gmail.com>
2025-08-04 09:35:20 +0200

    Forking simpler non-LiveView macro controlled React hook component
    The original implementation is now called HeexReactComponent, which the
    jsx macro now uses. The existing parts of the application use this right
    now.
    
    Making room for the new CollaborativeEditor component with needing
    LiveView to be involved at a rendering level. So that means we won't
    have the application/json props etc. The 'new' ReactComponent hook uses
    simple attr plucking and a replaceEqualDeep to give us the same prop
    passing behaviour.

--- assets/js/react/lib/is-react-hooked-element.ts
+++ assets/js/react/lib/is-react-hooked-element.ts
@@ -5,9 +5,4 @@ export const isReactHookedElement = (
 ): el is ReactHookedElement =>
   el instanceof HTMLElement &&
   el.dataset['reactName'] !== undefined &&
-  el.dataset['reactFile'] !== undefined &&
-  Boolean(
-    Array.from(el.attributes).find(
-      attr => attr.name === 'phx-hook' && attr.value === 'ReactComponent'
-    )
-  );
+  el.dataset['reactFile'] !== undefined;
