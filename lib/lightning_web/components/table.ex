c3a694cb35b9e1ad7b324be017172c9c3596dd67
Taylor Downs <taylor@openfn.org>
2025-08-05 09:02:48 +0200

    fix #3476

--- lib/lightning_web/components/table.ex
+++ lib/lightning_web/components/table.ex
@@ -94,7 +94,7 @@ defmodule LightningWeb.Components.Table do
     ~H"""
     <div
       id={@id}
-      class="overflow-x-auto shadow ring-1 ring-black/5 sm:rounded-lg bg-gray-50"
+      class="shadow ring-1 ring-black/5 sm:rounded-lg bg-gray-50"
     >
       <table class={["min-w-full", @divide && "divide-y divide-gray-200"]}>
         <thead>{render_slot(@header)}</thead>
