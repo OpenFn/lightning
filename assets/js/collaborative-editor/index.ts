0607139f6a9c5124a89391392f38e312c2a37b33
Stuart Corbishley <corbish@gmail.com>
2025-08-06 12:38:31 +0200

    Start of WorkflowEditor and WorkflowStoreProvider
    - Abstracted connection and awareness state into SessionProvider.
    - Fixed scrolling

--- assets/js/collaborative-editor/index.ts
+++ assets/js/collaborative-editor/index.ts
@@ -1,3 +1,13 @@
 export { CollaborativeEditor } from './CollaborativeEditor';
+export { SessionProvider, useSession } from './contexts/SessionProvider';
 export { TodoStoreProvider, useTodoStore } from './contexts/TodoStoreProvider';
+export {
+  WorkflowStoreProvider,
+  useWorkflowStore,
+} from './contexts/WorkflowStoreProvider';
+export { WorkflowEditor } from './components/WorkflowEditor';
+export { WorkflowHeader } from './components/WorkflowHeader';
+export { JobsList } from './components/JobsList';
+export { JobItem } from './components/JobItem';
 export type { TodoItem, AwarenessUser, TodoStore } from './types/todo';
+export type { WorkflowJob, Workflow, WorkflowStore } from './types/workflow';
