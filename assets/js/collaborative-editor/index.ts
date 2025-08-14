9527dbd88e59f84224aaaf5b4f00afc8e94c1616
Stuart Corbishley <corbish@gmail.com>
2025-08-14 09:42:26 +0200

    Use Trigger type from Zod
    Remove remaining Todo related files

--- assets/js/collaborative-editor/index.ts
+++ assets/js/collaborative-editor/index.ts
@@ -1,13 +1,9 @@
 export { CollaborativeEditor } from "./CollaborativeEditor";
-export { JobItem } from "./components/JobItem";
-export { JobsList } from "./components/JobsList";
 export { WorkflowEditor } from "./components/WorkflowEditor";
 export { WorkflowHeader } from "./components/WorkflowHeader";
 export { SessionProvider, useSession } from "./contexts/SessionProvider";
-export { TodoStoreProvider, useTodoStore } from "./contexts/TodoStoreProvider";
 export {
   useWorkflowStore,
-  WorkflowStoreProvider,
+  WorkflowStoreProvider
 } from "./contexts/WorkflowStoreProvider";
-export type { AwarenessUser, TodoItem, TodoStore } from "./types/todo";
-export type { Workflow, WorkflowJob, WorkflowStore } from "./types/workflow";
+
