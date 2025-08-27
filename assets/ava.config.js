de9c92f14fda945dcf91949a6e10a06191b79f95
Stuart Corbishley <corbish@gmail.com>
2025-08-27 08:18:27 +0200

    Switch ts-node for tsimp for running tests.
    ts-node has barely been updated in a while and has issues with Typescript 5.0+ and tsconfigs that have multiple extends items.
    
    - Adjusted `eslint.config.js` to remove the `test` directory from the browser files mapping.
    - Removed `ts-node` from `package.json` and `package-lock.json` to streamline dependencies.

--- assets/ava.config.js
+++ assets/ava.config.js
@@ -1,15 +1,15 @@
-const processOnly = new Set(["--loader=ts-node/esm"]);
-
 export default {
   extensions: {
     js: true,
     ts: "module",
   },
   nodeArguments: ["--import=tsimp", "--no-warnings"],
-  filterNodeArgumentsForWorkerThreads: argument => !processOnly.has(argument),
   files: ["test/**/*.test.ts"],
   environmentVariables: {
     TS_NODE_PROJECT: "tsconfig.test.json",
     TSIMP_DIAG: "ignore",
+  },
+  watchMode: {
+    ignoreChanges: [".tsimp/**/*"],
   },
 };
