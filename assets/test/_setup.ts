bc0bed8c38492fc7967dee85dac048fafc988e45
Stuart Corbishley <corbish@gmail.com>
2025-08-27 12:35:07 +0200

    Make logging configurable on ava

---
+++ assets/test/_setup.ts
@@ -0,0 +1,6 @@
+// Suppress debug logs during tests
+console.debug = () => {};
+
+// Or if you want to capture them but not display:
+const originalDebug = console.debug;
+console.debug = process.env.LOG_LEVEL === "debug" ? originalDebug : () => {};
