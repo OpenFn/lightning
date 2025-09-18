e73ca05a74a4bd417982868893e55982310cc666
Taylor Downs <taylor@openfn.org>
2025-09-18 10:29:13 +0200

    ok

--- lib/lightning_web/router.ex
+++ lib/lightning_web/router.ex
@@ -88,6 +88,8 @@ defmodule LightningWeb.Router do
 
     resources "/jobs", API.JobController, only: [:index, :show]
     # resources "/runs", API.RunController, only: [:index, :show]
+
+    resources "/credentials", API.CredentialController, only: [:create]
   end
 
   ## Collections
