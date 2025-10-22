a92f7874d543d146b6e99b3c76100dedce3b175b
Elias W. BA <eliaswalyba@gmail.com>
2025-10-22 20:37:40 +0000

    Add tests for sandbox indicator banners

--- test/lightning_web/live/workflow_live/edit_test.exs
+++ test/lightning_web/live/workflow_live/edit_test.exs
@@ -4919,6 +4919,149 @@ defmodule LightningWeb.WorkflowLive.EditTest do
     end
   end
 
+  describe "sandbox indicator banner" do
+    test "shows banner when viewing workflow in sandbox project", %{conn: conn} do
+      user = insert(:user)
+      parent_project = insert(:project, name: "Production Project")
+
+      sandbox =
+        insert(:sandbox,
+          parent: parent_project,
+          name: "test-sandbox",
+          project_users: [%{user_id: user.id, role: :owner}]
+        )
+
+      workflow = workflow_fixture(project_id: sandbox.id)
+
+      conn = log_in_user(conn, user)
+
+      {:ok, _view, html} =
+        live(
+          conn,
+          ~p"/projects/#{sandbox.id}/w/#{workflow.id}?v=#{workflow.lock_version}"
+        )
+
+      assert html =~ "You are currently working in the sandbox"
+      assert html =~ sandbox.name
+      assert html =~ "Switch to #{parent_project.name}"
+      assert html =~ "/projects/#{parent_project.id}/w"
+    end
+
+    test "does not show banner when viewing workflow in root project", %{
+      conn: conn
+    } do
+      user = insert(:user)
+
+      project =
+        insert(:project,
+          name: "Production Project",
+          project_users: [%{user_id: user.id, role: :owner}]
+        )
+
+      workflow = workflow_fixture(project_id: project.id)
+
+      conn = log_in_user(conn, user)
+
+      {:ok, _view, html} =
+        live(
+          conn,
+          ~p"/projects/#{project.id}/w/#{workflow.id}?v=#{workflow.lock_version}"
+        )
+
+      refute html =~ "You are currently working in the sandbox"
+      refute html =~ "Switch to"
+    end
+
+    test "shows correct root project in deeply nested sandbox", %{conn: conn} do
+      user = insert(:user)
+      root_project = insert(:project, name: "Root Project")
+
+      sandbox_a =
+        insert(:sandbox,
+          parent: root_project,
+          name: "sandbox-a",
+          project_users: [%{user_id: user.id, role: :owner}]
+        )
+
+      sandbox_b =
+        insert(:sandbox,
+          parent: sandbox_a,
+          name: "sandbox-b",
+          project_users: [%{user_id: user.id, role: :owner}]
+        )
+
+      workflow = workflow_fixture(project_id: sandbox_b.id)
+
+      conn = log_in_user(conn, user)
+
+      {:ok, _view, html} =
+        live(
+          conn,
+          ~p"/projects/#{sandbox_b.id}/w/#{workflow.id}?v=#{workflow.lock_version}"
+        )
+
+      assert html =~ "You are currently working in the sandbox"
+      assert html =~ "Switch to #{root_project.name}"
+      assert html =~ "/projects/#{root_project.id}/w"
+      refute html =~ sandbox_a.name
+    end
+
+    test "shows banner in job inspector when editing job in sandbox", %{
+      conn: conn
+    } do
+      user = insert(:user)
+      parent_project = insert(:project, name: "Production Project")
+
+      sandbox =
+        insert(:sandbox,
+          parent: parent_project,
+          name: "test-sandbox",
+          project_users: [%{user_id: user.id, role: :owner}]
+        )
+
+      workflow = workflow_fixture(project_id: sandbox.id)
+      job = insert(:job, workflow: workflow, name: "test-job")
+
+      conn = log_in_user(conn, user)
+
+      {:ok, _view, html} =
+        live(
+          conn,
+          ~p"/projects/#{sandbox.id}/w/#{workflow.id}?s=#{job.id}&m=expand&v=#{workflow.lock_version}"
+        )
+
+      assert html =~ "You are currently working in the sandbox"
+      assert html =~ sandbox.name
+      assert html =~ "Switch to #{parent_project.name}"
+      assert html =~ "/projects/#{parent_project.id}/w"
+    end
+
+    test "does not show banner in job inspector when editing job in root project",
+         %{conn: conn} do
+      user = insert(:user)
+
+      project =
+        insert(:project,
+          name: "Production Project",
+          project_users: [%{user_id: user.id, role: :owner}]
+        )
+
+      workflow = workflow_fixture(project_id: project.id)
+      job = insert(:job, workflow: workflow, name: "test-job")
+
+      conn = log_in_user(conn, user)
+
+      {:ok, _view, html} =
+        live(
+          conn,
+          ~p"/projects/#{project.id}/w/#{workflow.id}?s=#{job.id}&m=expand&v=#{workflow.lock_version}"
+        )
+
+      refute html =~ "You are currently working in the sandbox"
+      refute html =~ "Switch to"
+    end
+  end
+
   defp stub_apollo_unavailable(_context) do
     stub(Lightning.MockConfig, :apollo, fn key ->
       case key do
