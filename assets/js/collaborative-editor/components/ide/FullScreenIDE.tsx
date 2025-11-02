d6c8aaf11aead1984c369988469d6be3519dd9cf
Elias W. BA <eliaswalyba@gmail.com>
2025-11-02 21:15:29 +0000

    Add missing useMemo import to FullScreenIDE

--- assets/js/collaborative-editor/components/ide/FullScreenIDE.tsx
+++ assets/js/collaborative-editor/components/ide/FullScreenIDE.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useHotkeys, useHotkeysContext } from "react-hotkeys-hook";
 import {
   type ImperativePanelHandle,
