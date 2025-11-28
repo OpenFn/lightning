59168bfa9c91e7355e58ecfe1d54b88ffa7132a8
Taylor Downs <taylor@openfn.org>
2025-11-28 07:57:04 +0100

    fix existing-view search padding

--- assets/js/manual-run-panel/views/ExistingView.tsx
+++ assets/js/manual-run-panel/views/ExistingView.tsx
@@ -109,7 +109,7 @@ const ExistingView: React.FC<ExistingViewProps> = ({
                   setQuery(e.target.value);
                 }}
                 type="text"
-                className="focus:outline focus:outline-2 focus:-outline-offset-2 focus:ring-0  disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 border-slate-300 focus:border-slate-400 focus:outline-indigo-600 block w-full rounded-l-md border-0 border-r-0 py-1.5 pl-10 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="focus:outline focus:outline-2 focus:-outline-offset-2 focus:ring-0  disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 border-slate-300 focus:border-slate-400 focus:outline-indigo-600 block w-full rounded-l-md border-0 border-r-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Search names or UUID prefixes"
               />
               <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
