9d5291e00f6dd6d54d32583ba001385833b75027
mtuchi <mtuchidev@gmail.com>
2025-12-05 13:53:41 +0300

    add docout, poison and bureaucrat

--- mix.exs
+++ mix.exs
@@ -153,7 +153,10 @@ defmodule Lightning.MixProject do
       {:benchee, "~> 1.5.0", only: :dev},
       {:statistics, "~> 0.6", only: :dev},
       {:y_ex, "~> 0.8.0"},
-      {:chameleon, "~> 2.5"}
+      {:chameleon, "~> 2.5"},
+      {:bureaucrat, "~> 0.2.10"},
+      {:poison, "~> 3.0"},
+      {:docout, github: "tfwright/docout", branch: "main", runtime: false}
     ]
   end
 
