1a9d33e724fd43de97eabd677d46d96560f49c8e
Farhan Yahaya <yahyafarhan48@gmail.com>
2026-01-07 08:58:32 +0000

    feat: add workflow_transform to serializer

--- assets/js/collaborative-editor/hooks/useUnsavedChanges.ts
+++ assets/js/collaborative-editor/hooks/useUnsavedChanges.ts
@@ -3,66 +3,16 @@ import { useWorkflowState } from './useWorkflow';
 
 export function useUnsavedChanges() {
   const { workflow } = useSessionContext();
-  const wf = {
-    jobs: workflow?.jobs.map(j => ({
-      id: j.id,
-      name: j.name,
-      body: j.body || '',
-      adaptor: j.adaptor,
-      project_credential_id: j.project_credential_id,
-      keychain_credential_id: j.keychain_credential_id,
-    })),
-    triggers: workflow?.triggers.map(t => ({
-      cron_expression: t.cron_expression,
-      enabled: t.enabled,
-      id: t.id,
-      type: t.type,
-    })),
-    edges: workflow?.edges.map(e => ({
-      condition_expression: e.condition_expression,
-      condition_label: e.condition_label,
-      condition_type: e.condition_type,
-      enabled: e.enabled,
-      id: e.id,
-      source_job_id: e.source_job_id,
-      source_trigger_id: e.source_trigger_id,
-      target_job_id: e.target_job_id,
-    })),
-    positions: workflow?.positions || {},
-    name: workflow?.name,
-  };
   const storeWorkflow = useWorkflowState(state => ({
-    jobs: state.jobs.map(j => ({
-      id: j.id,
-      name: j.name,
-      body: j.body || '',
-      adaptor: j.adaptor,
-      project_credential_id: j.project_credential_id,
-      keychain_credential_id: j.keychain_credential_id,
-    })),
-    triggers: state.triggers.map(t => ({
-      cron_expression: t.cron_expression,
-      enabled: t.enabled,
-      id: t.id,
-      type: t.type,
-    })),
-    edges: state.edges.map(e => ({
-      condition_expression: e.condition_expression,
-      condition_label: e.condition_label,
-      condition_type: e.condition_type,
-      enabled: e.enabled,
-      id: e.id,
-      source_job_id: e.source_job_id,
-      source_trigger_id: e.source_trigger_id,
-      target_job_id: e.target_job_id,
-    })),
+    jobs: state.jobs,
+    triggers: state.triggers,
+    edges: state.edges,
     positions: state.positions || {},
     name: state.workflow?.name,
   }));
-
   // pick items in the exising and check if the new matches it.
 
-  return { hasChanges: isDiff(wf, storeWorkflow) as boolean };
+  return { hasChanges: isDiff(workflow, storeWorkflow) as boolean };
 }
 
 function isDiff(base: unknown, target: unknown) {
