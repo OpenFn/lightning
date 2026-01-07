a96b1956da9d9153ce99c6b235696a2621f1e6ce
Farhan Yahaya <yahyafarhan48@gmail.com>
2026-01-07 14:36:25 +0000

    feat: transform workflow

--- assets/js/collaborative-editor/hooks/useUnsavedChanges.ts
+++ assets/js/collaborative-editor/hooks/useUnsavedChanges.ts
@@ -1,3 +1,4 @@
+import type { Workflow } from '../types/workflow';
 import { useSessionContext } from './useSessionContext';
 import { useWorkflowState } from './useWorkflow';
 
@@ -12,7 +13,48 @@ export function useUnsavedChanges() {
   }));
   // pick items in the exising and check if the new matches it.
 
-  return { hasChanges: isDiff(workflow, storeWorkflow) as boolean };
+  if (!workflow || !storeWorkflow) return false;
+  return {
+    hasChanges: isDiff(
+      transformWorkflow(workflow || {}),
+      transformWorkflow(storeWorkflow || {})
+    ) as boolean,
+  };
+}
+
+function transformWorkflow(workflow: Workflow) {
+  return {
+    name: workflow.name,
+    jobs: (workflow.jobs || [])
+      .map(job => ({
+        id: job.id,
+        name: job.name,
+        body: job.body,
+        adaptor: job.adaptor,
+        project_credential_id: job.project_credential_id,
+        keychain_credential_id: job.keychain_credential_id,
+      }))
+      .sort((a, b) => a.id.localeCompare(b.id)),
+    edges: (workflow.edges || [])
+      .map(edge => ({
+        id: edge.id,
+        source_job_id: edge.source_job_id,
+        source_trigger_id: edge.source_trigger_id,
+        target_job_id: edge.target_job_id,
+        enabled: edge.enabled || false,
+        condition_type: edge.condition_type,
+        condition_label: edge.condition_label,
+        condition_expression: edge.condition_expression,
+      }))
+      .sort((a, b) => a.id.localeCompare(b.id)),
+    trigger: (workflow.triggers || []).map(trigger => ({
+      id: trigger.id,
+      type: trigger.type,
+      enabled: trigger.enabled,
+      cron_expression: trigger.cron_expression,
+    })),
+    positions: workflow.positions || {},
+  };
 }
 
 function isDiff(base: unknown, target: unknown) {
