1459675c3938c3d940c73b9ddcd84e5984ee4db9
Joe Clark <jclark@openfn.org>
2026-02-05 16:50:46 +0000

    refactor loading spinner

--- assets/js/collaborative-editor/components/CollaborativeMonaco.tsx
+++ assets/js/collaborative-editor/components/CollaborativeMonaco.tsx
@@ -25,38 +25,9 @@ import { useHandleDiffDismissed } from '../contexts/MonacoRefContext';
 import dts_es5 from '../../editor/lib/es5.min.dts';
 import createCompletionProvider from '../../editor/magic-completion';
 
+import { LoadingIndicator } from './common/LoadingIndicator';
 import { Cursors } from './Cursors';
 import { Tooltip } from './Tooltip';
-
-const spinner = (
-  <svg
-    className="inline-block h-5 w-5 animate-spin"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <circle
-      className="opacity-25"
-      cx="12"
-      cy="12"
-      r="10"
-      stroke="currentColor"
-      strokeWidth="4"
-    ></circle>
-    <path
-      className="opacity-75"
-      fill="currentColor"
-      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-    ></path>
-  </svg>
-);
-
-const loadingIndicator = (
-  <div className="inline-block p-2">
-    <span className="mr-2">Loading types</span>
-    {spinner}
-  </div>
-);
 
 type Lib = {
   content: string;
@@ -596,7 +567,7 @@ export const CollaborativeMonaco = forwardRef<
     <div className={cn('relative', className || 'h-full w-full')}>
       {/* Loading indicator */}
       <div className="relative z-10 h-0 overflow-visible text-right text-xs text-white">
-        {loading && loadingIndicator}
+        {loading && <LoadingIndicator text="Loading types" />}
       </div>
       {/* Standard editor container */}
       <div ref={containerRef} className="h-full w-full">
