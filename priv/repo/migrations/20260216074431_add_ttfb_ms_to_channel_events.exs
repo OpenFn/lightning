ad25c9a789ec1ffb08f00156c06a2061c05f4cf6
Stuart Corbishley <corbish@gmail.com>
2026-02-16 13:39:33 +0200

    Squash ttfb_ms column into main channels migration (#4405)
    Since neither migration has shipped, merge the separate
    add_ttfb_ms_to_channel_events migration into create_channels_tables
    to keep migration history clean.

--- priv/repo/migrations/20260216074431_add_ttfb_ms_to_channel_events.exs
+++
@@ -1,9 +0,0 @@
-defmodule Lightning.Repo.Migrations.AddTtfbMsToChannelEvents do
-  use Ecto.Migration
-
-  def change do
-    alter table(:channel_events) do
-      add :ttfb_ms, :integer
-    end
-  end
-end
