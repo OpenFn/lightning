16872600c355ec496ea7f825cb78fd36cea292e5
Elias W. BA <eliaswalyba@gmail.com>
2026-04-14 08:34:08 +0000

    Use explicit up/down in collections migration with irreversible guard

--- priv/repo/migrations/20260410000000_scope_collection_name_uniqueness_to_project.exs
+++ priv/repo/migrations/20260410000000_scope_collection_name_uniqueness_to_project.exs
@@ -1,8 +1,12 @@
 defmodule Lightning.Repo.Migrations.ScopeCollectionNameUniquenessToProject do
   use Ecto.Migration
 
-  def change do
-    drop unique_index(:collections, [:name])
+  def up do
+    drop_if_exists unique_index(:collections, [:name])
     create unique_index(:collections, [:project_id, :name])
+  end
+
+  def down do
+    raise "Cannot reverse: duplicate collection names may exist across projects"
   end
 end
