c06aea6528da78d208cfd565160fa33d83e5c7d7
Farhan Yahaya <yahyafarhan48@gmail.com>
2026-07-08 06:06:23 +0000

    feat: use execute/2

--- priv/repo/migrations/20260629143825_clear_prefer_legacy_editor.exs
+++ priv/repo/migrations/20260629143825_clear_prefer_legacy_editor.exs
@@ -1,9 +1,12 @@
 defmodule Lightning.Repo.Migrations.ClearPreferLegacyEditor do
   use Ecto.Migration
 
-  def up do
-    execute("""
-    UPDATE users SET preferences = preferences - 'prefer_legacy_editor' WHERE preferences ? 'prefer_legacy_editor'
-    """)
+  def change do
+    execute(
+      """
+      UPDATE users SET preferences = preferences - 'prefer_legacy_editor' WHERE preferences ? 'prefer_legacy_editor'
+      """,
+      ""
+    )
   end
 end
